15815c78baac976103ddde959cfacdff745f8554
Nestorferrando <nestor.ferrando@akamon.me>
2014-03-05 21:10:42 +0100

    creation of hexagonal seed in ag/ag growth kmc

--- src/Kinetic_Monte_Carlo/KMC_core/diffusion/Ag_Ag_Growth/Ag_Ag_KMC.java
+++ src/Kinetic_Monte_Carlo/KMC_core/diffusion/Ag_Ag_Growth/Ag_Ag_KMC.java
@@ -7,6 +7,7 @@ package Kinetic_Monte_Carlo.KMC_core.diffusion.Ag_Ag_Growth;
 
 import Kinetic_Monte_Carlo.KMC_core.diffusion.Abstract_2D_diffusion_KMC;
 import Kinetic_Monte_Carlo.KMC_core.diffusion.Round_perimeter;
+import Kinetic_Monte_Carlo.atom.diffusion.Ag_Ag_growth.AgAg_Atom;
 import Kinetic_Monte_Carlo.atom.diffusion.DevitaAccelerator.DevitaAccelerator;
 import Kinetic_Monte_Carlo.atom.diffusion.DevitaAccelerator.Hops_per_step;
 import Kinetic_Monte_Carlo.lattice.diffusion.Ag_Ag_growth.Ag_Ag_Growth_lattice;
@@ -26,7 +27,7 @@ public class Ag_Ag_KMC extends Abstract_2D_diffusion_KMC {
 
         this.lattice = new Ag_Ag_Growth_lattice(sizeX, sizeY, modified_buffer, distance_per_step);
         
-        configureDevitaAccelerator(distance_per_step);
+        //configureDevitaAccelerator(distance_per_step);
     }
 
 
@@ -36,23 +37,33 @@ public class Ag_Ag_KMC extends Abstract_2D_diffusion_KMC {
         
         if (justCentralFlake) {
             this.perimeter = new Round_perimeter("Ag_Ag_growth");
+     
             this.perimeter.setAtomPerimeter(lattice.setInside(perimeter.getCurrentRadius()));
-/*
-            int Xcenter = lattice.getSizeY() / 2;
-            int Ycenter = (lattice.getSizeX() / 2);
-            for (int j = -1; j < 2; j++) {
-                for (int i = -1; i < 1; i++) {
-                    this.depositAtom(Ycenter + i, Xcenter + j);
-                }
-            }*/
+
+            int Xcenter = lattice.getSizeX() / 2;
+            int Ycenter = (lattice.getSizeY() / 2);
+            
+            this.depositAtom(Xcenter , Ycenter);
+            this.depositAtom(Xcenter+1 , Ycenter);
+            
+            this.depositAtom(Xcenter-1 , Ycenter+1);
+            this.depositAtom(Xcenter , Ycenter+1);
+            this.depositAtom(Xcenter+1 , Ycenter+1);
+            
+            this.depositAtom(Xcenter , Ycenter+2);
+            this.depositAtom(Xcenter-1 , Ycenter+2);
+            this.depositAtom(Xcenter-1 , Ycenter+3);
+                        
+            
         } else {
-            /*
+            
             for (int i = 0; i < 3; i++) {
                 int X = (int) (StaticRandom.raw() * lattice.getSizeX());
                 int Y = (int) (StaticRandom.raw() * lattice.getSizeY());
                 depositAtom(X, Y);
-            }*/
+            }
         }
+        System.out.println(this.list.getTotalProbability());       
     }
 
     private void configureDevitaAccelerator(Hops_per_step distance_per_step) {
