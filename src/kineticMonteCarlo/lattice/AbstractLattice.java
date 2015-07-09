08368de15c061d0c1fb2ddf119e05ea8b422fc07
Joseba <joseba.alberdi@ehu.es>
2015-07-09 11:56:53 +0200

    Created a common constructor Created a common method. Thus, deleted duplicated variable and method Also formatting

--- src/kineticMonteCarlo/lattice/AbstractLattice.java
+++ src/kineticMonteCarlo/lattice/AbstractLattice.java
@@ -12,21 +12,19 @@ import kineticMonteCarlo.atom.AbstractAtom;
  */
 public abstract class AbstractLattice {
  
-    
-        protected int  sizeX;
-        protected int  sizeY;
-        protected int  sizeZ; 
+  protected int  sizeX;
+  protected int  sizeY;
+  protected int  sizeZ; 
         
-        protected int unit_cell_size;
+  protected int unit_cell_size;
     
+  public int getSizeX(){return sizeX;}
+  public int getSizeY(){return sizeY;}
+  public int getSizeZ(){return sizeZ;}
+  public int getSizeUC(){return unit_cell_size;}
     
-    public int getSizeX(){return sizeX;}
-    public int getSizeY(){return sizeY;}
-    public int getSizeZ(){return sizeZ;}
-    public int getSizeUC(){return unit_cell_size;}
-    
-    public abstract AbstractAtom getAtom(int X,int Y,int Z,int Unit_cell_pos);
-        
-    public abstract void reset();
-        
+  public abstract AbstractAtom getAtom(int X,int Y,int Z,int Unit_cell_pos);
+
+  public abstract void reset();
+
 }
--- src/kineticMonteCarlo/lattice/diffusion/Abstract2DDiffusionLattice.java
+++ src/kineticMonteCarlo/lattice/diffusion/Abstract2DDiffusionLattice.java
@@ -9,6 +9,8 @@ import kineticMonteCarlo.atom.diffusion.Abstract2DDiffusionAtom;
 import kineticMonteCarlo.lattice.AbstractLattice;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import kineticMonteCarlo.atom.diffusion.ModifiedBuffer;
+import kineticMonteCarlo.atom.diffusion.devitaAccelerator.HopsPerStep;
 import utils.QuickSort;
 
 /**
@@ -17,103 +19,114 @@ import utils.QuickSort;
  */
 public abstract class Abstract2DDiffusionLattice extends AbstractLattice implements IDevitaLattice {
 
-    protected Abstract2DDiffusionAtom[][] atoms;
+  protected Abstract2DDiffusionAtom[][] atoms;
 
-    public abstract Abstract2DDiffusionAtom getNeighbor(int Xpos, int Ypos, int neighbor);
+  private final ModifiedBuffer modified;
 
-    public abstract void configure(double[][] probabilities);
+  public Abstract2DDiffusionLattice(int sizeX, int sizeY, ModifiedBuffer modified, HopsPerStep distance_per_step) {
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    sizeZ = 1;
+    unit_cell_size = 4;
+    this.modified = modified;
+  }
 
-    public abstract Abstract2DDiffusionAtom getAtom(int X, int Y);
+  public abstract Abstract2DDiffusionAtom getNeighbor(int Xpos, int Ypos, int neighbor);
 
-    @Override
-    public AbstractAtom getAtom(int X, int Y, int Z, int Unit_cell_pos) {
-        if (Z != 0 || Unit_cell_pos != 0) {
-            throw new UnsupportedOperationException("Z position or position inside unit cell cannot be different than 0, not supported"); //To change body of generated methods, choose Tools | Templates.
-        }
-        return getAtom(X, Y);
+  public void configure(double[][] probabilities) {
+    for (int i = 0; i < atoms[0].length; i++) { //X
+      for (int j = 0; j < atoms.length; j++) { //Y   
+        atoms[j][i].initialize(this, probabilities, this.getModified());
+      }
     }
+  }
 
-    //obtains the spatial location of certain atom, the distance between atoms is considered as 1
-    public abstract Point2D getSpatialLocation(int X, int Y);
+  public abstract Abstract2DDiffusionAtom getAtom(int X, int Y);
 
-    public abstract Point2D getCentralLatticeLocation();
-    
-    public abstract float getSpatialSizeX();
-    
-    public abstract float getSpatialSizeY();
-
-    @Override
-    public void reset() {
-
-        for (int i = 0; i < atoms[0].length; i++) { //X
-            for (int j = 0; j < atoms.length; j++) { //Y   
-
-                atoms[j][i].clear();
-            }
-        }
+  @Override
+  public AbstractAtom getAtom(int X, int Y, int Z, int Unit_cell_pos) {
+    if (Z != 0 || Unit_cell_pos != 0) {
+      throw new UnsupportedOperationException("Z position or position inside unit cell cannot be different than 0, not supported"); //To change body of generated methods, choose Tools | Templates.
     }
+    return getAtom(X, Y);
+  }
 
-    protected void setAngles() {
+  //obtains the spatial location of certain atom, the distance between atoms is considered as 1
+  public abstract Point2D getSpatialLocation(int X, int Y);
 
-        Point2D middle = getCentralLatticeLocation();
+  public abstract Point2D getCentralLatticeLocation();
 
-        for (int Y = 0; Y < sizeY; Y++) {
-            for (int X = 0; X < sizeX; X++) {
+  public abstract float getSpatialSizeX();
 
-                Point2D position = getSpatialLocation(X, Y);
+  public abstract float getSpatialSizeY();
 
-                double Xdif = position.getX() - middle.getX();
-                double Ydif = position.getY() - middle.getY();
-
-                if (Xdif == 0) {
-                    Xdif = 1e-8;
-                }
-                double angle = Math.atan(Ydif / Xdif);
-
-                if (Xdif < 0) {
-                    angle = Math.PI + angle;
-                }
-                if (Xdif >= 0 && Ydif < 0) {
-                    angle = 2 * Math.PI + angle;
-                }
-                atoms[X][Y].setAngle((float) angle);
-            }
-        }
+  @Override
+  public void reset() {
+    for (int i = 0; i < atoms[0].length; i++) { //X
+      for (int j = 0; j < atoms.length; j++) { //Y   
+        atoms[j][i].clear();
+      }
     }
+  }
 
-    public double getDistanceToCenter(int X, int Y) {
-
-        Point2D middle = getCentralLatticeLocation();
+  protected void setAngles() {
+    Point2D middle = getCentralLatticeLocation();
+    for (int Y = 0; Y < sizeY; Y++) {
+      for (int X = 0; X < sizeX; X++) {
         Point2D position = getSpatialLocation(X, Y);
-
-        return position.distance(middle);
+        double Xdif = position.getX() - middle.getX();
+        double Ydif = position.getY() - middle.getY();
+        if (Xdif == 0) {
+          Xdif = 1e-8;
+        }
+        double angle = Math.atan(Ydif / Xdif);
+        if (Xdif < 0) {
+          angle = Math.PI + angle;
+        }
+        if (Xdif >= 0 && Ydif < 0) {
+          angle = 2 * Math.PI + angle;
+        }
+        atoms[X][Y].setAngle((float) angle);
+      }
     }
+  }
+
+  public double getDistanceToCenter(int X, int Y) {
+
+    Point2D middle = getCentralLatticeLocation();
+    Point2D position = getSpatialLocation(X, Y);
+
+    return position.distance(middle);
+  }
 
     //define como átomos inside a los átomos dentro de dicho rádio
-    //devuelve un array de átomos que es el perimetro de dicha circunferencia.
-    public Abstract2DDiffusionAtom[] setInside(int radius) {
+  //devuelve un array de átomos que es el perimetro de dicha circunferencia.
+  public Abstract2DDiffusionAtom[] setInside(int radius) {
 
-        ArrayList<Abstract2DDiffusionAtom> perimeterList = new ArrayList();
+    ArrayList<Abstract2DDiffusionAtom> perimeterList = new ArrayList();
 
-        for (int Y = 0; Y < sizeY; Y++) {
-            for (int X = 0; X < sizeX; X++) {
-                double distance = getDistanceToCenter(X, Y);
-                if (radius <= distance) {
-                    atoms[X][Y].setOutside(true);
-                } else {
-                    atoms[X][Y].setOutside(false);
-                    if (distance > (radius - 1)) {
-                        perimeterList.add(atoms[X][Y]);
-                    }
-                }
-            }
+    for (int Y = 0; Y < sizeY; Y++) {
+      for (int X = 0; X < sizeX; X++) {
+        double distance = getDistanceToCenter(X, Y);
+        if (radius <= distance) {
+          atoms[X][Y].setOutside(true);
+        } else {
+          atoms[X][Y].setOutside(false);
+          if (distance > (radius - 1)) {
+            perimeterList.add(atoms[X][Y]);
+          }
         }
-
-        Abstract2DDiffusionAtom[] perimeter = perimeterList.toArray(new Abstract2DDiffusionAtom[perimeterList.size()]);
-
-        QuickSort.order_by_angle(perimeter, perimeter.length - 1);
-
-        return perimeter;
+      }
     }
 
+    Abstract2DDiffusionAtom[] perimeter = perimeterList.toArray(new Abstract2DDiffusionAtom[perimeterList.size()]);
+
+    QuickSort.order_by_angle(perimeter, perimeter.length - 1);
+
+    return perimeter;
+  }
+
+  public ModifiedBuffer getModified() {
+    return modified;
+  }
 }
