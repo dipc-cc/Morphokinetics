dbffc54ed59c329cab21c08d37e5e98c1a9fe10f
Joseba <joseba.alberdi@ehu.es>
2015-07-09 16:58:22 +0200

    Included the new Rates library abstract class

---
+++ src/ratesLibrary/basic/RatesCase.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ratesLibrary.basic;
+
+import ratesLibrary.IRatesFactory;
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez
+ */
+public abstract class RatesCase implements IBasicRates {
+
+  private double E0;
+  private double E1;
+  private double E2;
+  private double E3;
+
+  private double P0;
+  private double P1;
+  private double P2;
+
+  private double P3;
+
+  private double[] prefactors = new double[4];
+  private double[] energies = new double[4];
+  
+  public void setRates(double newEnergies[], double newPrefactors[]) {
+    assert(newEnergies.length == 4);
+    assert(newPrefactors.length == 4);
+    for (int i = 0; i < 4; i++) {
+      energies[i] = newEnergies[i];
+      prefactors[i] = newPrefactors[i];
+    }
+  }
+  
+  @Override
+  public double getPrefactor(int i) {
+    return prefactors[i];
+  }
+
+  @Override
+  public double getEnergy(int i) {
+    return energies[i];
+  }
+}
