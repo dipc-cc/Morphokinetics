d8fcb9d84054112cee8709ba079cd582c83e7e12
jalberdi004 <joseba.alberdi@ehu.eus>
2015-07-14 10:56:30 +0200

    Indentation

--- src/kineticMonteCarlo/lattice/diffusion/perimeterStatistics/agAg/AgAgPerimeterStatistics.java
+++ src/kineticMonteCarlo/lattice/diffusion/perimeterStatistics/agAg/AgAgPerimeterStatistics.java
@@ -14,31 +14,27 @@ import java.util.Map;
  */
 public class AgAgPerimeterStatistics extends AbstractPerimeterStatistics {
 
-    public AgAgPerimeterStatistics() {
-        
-        
-  this.totalCount=RawStatisticDataAtomCount1Million.data[0][180];
-  this.atomsCountMap=new HashMap();
-  this.hopsCountMap=new HashMap();
-  int radius=20;
-  
-  for (int i=0;i<RawStatisticDataAtomCount1Million.data.length;i++){
-      Map<Integer,Integer> currentRadiusCountMap=new HashMap();
-      Map<Integer,Integer> currentRadiusHopMap=new HashMap();
-      
+  public AgAgPerimeterStatistics() {
+
+    this.totalCount = RawStatisticDataAtomCount1Million.data[0][180];
+    this.atomsCountMap = new HashMap();
+    this.hopsCountMap = new HashMap();
+    int radius = 20;
+
+    for (int i = 0; i < RawStatisticDataAtomCount1Million.data.length; i++) {
+      Map<Integer, Integer> currentRadiusCountMap = new HashMap();
+      Map<Integer, Integer> currentRadiusHopMap = new HashMap();
+
       this.atomsCountMap.put(radius, currentRadiusCountMap);
       this.hopsCountMap.put(radius, currentRadiusHopMap);
-      
-      for (int j=0;j<180;j++){
-          currentRadiusCountMap.put(j, RawStatisticDataAtomCount1Million.data[i][j]);
-          currentRadiusHopMap.put(j, RawStatisticDataHopsCount1Million.data[i][j]);
+
+      for (int j = 0; j < 180; j++) {
+        currentRadiusCountMap.put(j, RawStatisticDataAtomCount1Million.data[i][j]);
+        currentRadiusHopMap.put(j, RawStatisticDataHopsCount1Million.data[i][j]);
       }
-      radius+=5;
-  }
-        
+      radius += 5;
     }
- 
-    
-    
-    
+
+  }
+
 }
