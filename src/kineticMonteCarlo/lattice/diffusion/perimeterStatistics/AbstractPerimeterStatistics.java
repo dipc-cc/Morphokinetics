98f260a12e4d3834d34bf58380e03de29de40e56
jalberdi004 <joseba.alberdi@ehu.eus>
2015-07-14 11:26:52 +0200

    Indentation

--- src/kineticMonteCarlo/lattice/diffusion/perimeterStatistics/AbstractPerimeterStatistics.java
+++ src/kineticMonteCarlo/lattice/diffusion/perimeterStatistics/AbstractPerimeterStatistics.java
@@ -13,47 +13,47 @@ import java.util.Map;
  */
 public abstract class AbstractPerimeterStatistics {
 
-    protected int totalCount;
-    protected Map<Integer, Map<Integer, Integer>> hopsCountMap;
-    protected Map<Integer, Map<Integer, Integer>> atomsCountMap;
+  protected int totalCount;
+  protected Map<Integer, Map<Integer, Integer>> hopsCountMap;
+  protected Map<Integer, Map<Integer, Integer>> atomsCountMap;
 
-    public int getTotalCount() {
-        return totalCount;
+  public int getTotalCount() {
+    return totalCount;
+  }
+
+  public int getAtomsCount(int radius, int offsetDegree) {
+    return atomsCountMap.get(radius).get(offsetDegree);
+  }
+
+  public int getHopsCount(int radius, int offsetDegree) {
+
+    return hopsCountMap.get(radius).get(offsetDegree);
+  }
+
+  public int getNextRadiusInSize(int radiusSize) {
+
+    Iterator<Integer> it = atomsCountMap.keySet().iterator();
+    int radius = -1;
+    while (it.hasNext()) {
+      int value = it.next();
+      if (value > radiusSize && (value < radius || radius == -1)) {
+        radius = value;
+      }
     }
+    return radius;
+  }
 
-    public int getAtomsCount(int radius, int offsetDegree) {
-        return atomsCountMap.get(radius).get(offsetDegree);
+  public int getMinRadiusInSize() {
+
+    Iterator<Integer> it = atomsCountMap.keySet().iterator();
+    int radius = -1;
+    while (it.hasNext()) {
+      int value = it.next();
+      if ((value < radius || radius == -1)) {
+        radius = value;
+      }
     }
+    return radius;
+  }
 
-    public int getHopsCount(int radius, int offsetDegree) {
-        
-        return hopsCountMap.get(radius).get(offsetDegree);
-    }
-
-    public int getNextRadiusInSize(int radiusSize) {
-
-        Iterator<Integer> it = atomsCountMap.keySet().iterator();
-        int radius = -1;
-        while (it.hasNext()) {
-            int value = it.next();
-            if (value > radiusSize && (value < radius || radius == -1)) {
-                radius = value;
-            }
-        }
-        return radius;
-    }
-    
-    public int getMinRadiusInSize() {
-
-        Iterator<Integer> it = atomsCountMap.keySet().iterator();
-        int radius = -1;
-        while (it.hasNext()) {
-            int value = it.next();
-            if ((value < radius || radius == -1)) {
-                radius = value;
-            }
-        }
-        return radius;
-    }
-    
 }
