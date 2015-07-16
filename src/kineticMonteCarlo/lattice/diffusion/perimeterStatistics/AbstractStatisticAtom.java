41bb6987769adb073f62ed8d6e1d04b33dbb7753
Joseba <joseba.alberdi@ehu.es>
2015-07-16 12:48:57 +0200

    Added abstract classes to properly work with the new approach

---
+++ src/kineticMonteCarlo/lattice/diffusion/perimeterStatistics/AbstractStatisticAtom.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kineticMonteCarlo.lattice.diffusion.perimeterStatistics;
+
+/**
+ *
+ * @author jalberdi004
+ */
+public abstract class AbstractStatisticAtom {
+  
+  private int[][] data;
+  
+  /**
+   * 
+   * @return the value of data[0][180]
+   */
+  public int getTotalCount(){
+    return data[0][180];
+  }
+  
+  /**
+   * 
+   * @return the length of the matrix
+   */
+  public int getLenght(){
+    return data.length;
+  }
+  
+  public int getData(int i, int j){
+    return data[i][j];
+  }
+  
+  /**
+   * To properly initialise in the not abstract constructor
+   * @param rawData 
+   */
+  protected void setData(int[][] rawData){
+    data = rawData;
+  }
+          
+}
---
+++ src/kineticMonteCarlo/lattice/diffusion/perimeterStatistics/AbstractStatisticsHops.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kineticMonteCarlo.lattice.diffusion.perimeterStatistics;
+
+/**
+ *
+ * @author jalberdi004
+ */
+public abstract class AbstractStatisticsHops {
+
+  private int[][] data;
+
+  public int getData(int i, int j) {
+    return data[i][j];
+  }
+
+  /**
+   * To properly initialise in the not abstract constructor
+   *
+   * @param rawData
+   */
+  protected void setData(int[][] rawData) {
+    data = rawData;
+  }
+
+}
