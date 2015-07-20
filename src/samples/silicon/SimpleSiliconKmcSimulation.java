d888f6952e9bbf081e0a5683dc15028e7601e95f
Joseba <joseba.alberdi@ehu.es>
2015-07-20 19:54:50 +0200

    Indentation

--- src/samples/silicon/SimpleSiliconKmcSimulation.java
+++ src/samples/silicon/SimpleSiliconKmcSimulation.java
@@ -16,41 +16,41 @@ import ratesLibrary.SiRatesFactory;
  */
 public class SimpleSiliconKmcSimulation {
 
-    public static void main(String args[]) {
+  public static void main(String args[]) {
 
-        System.out.println("Simple simulation of the Silicon etching KMC");
+    System.out.println("Simple simulation of the Silicon etching KMC");
 
-        SiEtchingKmcConfig config = configKMC();
+    SiEtchingKmcConfig config = configKMC();
 
-        SiEtchingKmc KMC = new SiEtchingKmc(config);
+    SiEtchingKmc KMC = new SiEtchingKmc(config);
 
-        long start = System.nanoTime();
+    long start = System.nanoTime();
 
-        KMC.initializeRates(new SiRatesFactory()
-                .getRates(350));
+    KMC.initializeRates(new SiRatesFactory()
+            .getRates(350));
 
-        KMC.simulate();
+    KMC.simulate();
 
-        System.out.println((System.nanoTime() - start) / 1000000);
+    System.out.println((System.nanoTime() - start) / 1000000);
 
-        new SiliconFrame().drawKMC(KMC);
-    }
+    new SiliconFrame().drawKMC(KMC);
+  }
 
-    private static SiEtchingKmcConfig configKMC() {
-        ListConfiguration listConfig = new ListConfiguration()
-                .setListType(ListConfiguration.BINNED_LIST)
-                .setBinsPerLevel(20)
-                .setExtraLevels(1);
-        
-        SiEtchingKmcConfig config = new SiEtchingKmcConfig()
-                .setMillerX(0)
-                .setMillerY(1)
-                .setMillerZ(1)
-                .setSizeX_UC(96)
-                .setSizeY_UC(96)
-                .setSizeZ_UC(16)
-                .setListConfig(listConfig);
-        return config;
-    }
+  private static SiEtchingKmcConfig configKMC() {
+    ListConfiguration listConfig = new ListConfiguration()
+            .setListType(ListConfiguration.BINNED_LIST)
+            .setBinsPerLevel(20)
+            .setExtraLevels(1);
+
+    SiEtchingKmcConfig config = new SiEtchingKmcConfig()
+            .setMillerX(0)
+            .setMillerY(1)
+            .setMillerZ(1)
+            .setSizeX_UC(96)
+            .setSizeY_UC(96)
+            .setSizeZ_UC(16)
+            .setListConfig(listConfig);
+    return config;
+  }
 
 }
