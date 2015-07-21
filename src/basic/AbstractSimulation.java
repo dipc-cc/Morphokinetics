2fa03a2a3bb4a7014d08025e640c54f384a77708
jalberdi004 <joseba.alberdi@ehu.eus>
2015-07-21 12:24:20 +0200

    Deleted useless comments

--- src/basic/AbstractSimulation.java
+++ src/basic/AbstractSimulation.java
@@ -92,13 +92,11 @@ public abstract class AbstractSimulation {
         psd.addSurfaceSample(sampledSurface);
       }
     }
-    //frame.setVisible(false);
 
     if (currentParser.doPsd()) {
       psd.applySimmetryFold(PsdSignature2D.HORIZONTAL_SIMMETRY);
       psd.applySimmetryFold(PsdSignature2D.VERTICAL_SIMMETRY);
       new Frame2D("PSD analysis").setMesh(MathUtils.avgFilter(psd.getPsd(), 1));
-      //sampledSurface[56][75] = -100;
       new Frame2D("Sampled surface").setMesh(sampledSurface);
     }
   }
