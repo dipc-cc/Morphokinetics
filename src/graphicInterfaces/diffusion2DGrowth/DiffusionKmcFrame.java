24e9681c42f67ffa7623c86f1359255c92c2e4aa
jalberdi004 <joseba.alberdi@ehu.eus>
2015-07-21 18:00:45 +0200

    Deleted accidentally committed line

--- src/graphicInterfaces/diffusion2DGrowth/DiffusionKmcFrame.java
+++ src/graphicInterfaces/diffusion2DGrowth/DiffusionKmcFrame.java
@@ -142,7 +142,6 @@ public class DiffusionKmcFrame extends javax.swing.JFrame {
     private void jSpinner2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner2StateChanged
 
       canvas1.setScale((Integer) jSpinner2.getValue());
-      this.setSize(canvas1.getY()+25, canvas1.getX()+25);
     }//GEN-LAST:event_jSpinner2StateChanged
 
     private void jPanel1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MousePressed
