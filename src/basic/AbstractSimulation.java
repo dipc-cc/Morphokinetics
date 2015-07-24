aeda810ff67c198b64581a7a96c9049120b90b80
Joseba <joseba.alberdi@ehu.eus>
2015-07-24 10:18:26 +0200

    For default the PSD is in log scale and shifted

--- src/basic/AbstractSimulation.java
+++ src/basic/AbstractSimulation.java
@@ -101,7 +101,9 @@ public abstract class AbstractSimulation {
     if (currentParser.doPsd()) {
       psd.applySimmetryFold(PsdSignature2D.HORIZONTAL_SIMMETRY);
       psd.applySimmetryFold(PsdSignature2D.VERTICAL_SIMMETRY);
-      new Frame2D("PSD analysis").setMesh(MathUtils.avgFilter(psd.getPsd(), 1)).setLogScale(true);
+      new Frame2D("PSD analysis").setMesh(MathUtils.avgFilter(psd.getPsd(), 1))
+              .setLogScale(true)
+              .setShift(true);      
       new Frame2D("Sampled surface").setMesh(sampledSurface);
     }
   }
