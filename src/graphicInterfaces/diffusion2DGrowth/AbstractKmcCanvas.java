f7232bf2c2487f1f55553ebbe337d8014c718c9c
Joseba <joseba.alberdi@ehu.eus>
2015-08-05 10:46:35 +0200

    Properly scale the y of the canvas

--- src/graphicInterfaces/diffusion2DGrowth/AbstractKmcCanvas.java
+++ src/graphicInterfaces/diffusion2DGrowth/AbstractKmcCanvas.java
@@ -14,6 +14,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import kineticMonteCarlo.lattice.AgAgLattice;
 
 /**
  *
@@ -46,7 +47,7 @@ public abstract class AbstractKmcCanvas extends Canvas {
   }
 
   public int getSizeY() {
-    return this.lattice.getSizeY() * scale;
+    return (int) (this.lattice.getSizeY() * scale * AgAgLattice.YRatio);
   }
 
   public void setScale(int scale) {
