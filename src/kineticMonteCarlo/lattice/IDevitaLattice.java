994968ac015ea19f2d9a4fd626136fa3849c34e7
Joseba <joseba.alberdi@ehu.eus>
2015-08-05 11:18:38 +0200

    Indentation

--- src/kineticMonteCarlo/lattice/IDevitaLattice.java
+++ src/kineticMonteCarlo/lattice/IDevitaLattice.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package kineticMonteCarlo.lattice;
 
 import kineticMonteCarlo.atom.Abstract2DDiffusionAtom;
@@ -13,9 +12,9 @@ import kineticMonteCarlo.atom.Abstract2DDiffusionAtom;
  * @author Nestor
  */
 public interface IDevitaLattice {
-    
-    
-    public int getAvailableDistance(int atomType,short Xpos, short Ypos,int thresholdDistance);
-    public Abstract2DDiffusionAtom getFarSite(int originType,short Xpos, short Ypos,int distance);
-    
+
+  public int getAvailableDistance(int atomType, short Xpos, short Ypos, int thresholdDistance);
+
+  public Abstract2DDiffusionAtom getFarSite(int originType, short Xpos, short Ypos, int distance);
+
 }
