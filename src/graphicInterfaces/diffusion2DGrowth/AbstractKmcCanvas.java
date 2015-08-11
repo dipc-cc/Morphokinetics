2aa3d7ec1051249252e1273054e7f9dbbb843cf5
Joseba <joseba.alberdi@ehu.eus>
2015-08-11 12:08:33 +0200

    Moved shared "paint" function from the lower classes to the upper AbstractKmcCanvas class.

--- src/graphicInterfaces/diffusion2DGrowth/AbstractKmcCanvas.java
+++ src/graphicInterfaces/diffusion2DGrowth/AbstractKmcCanvas.java
@@ -6,6 +6,7 @@ package graphicInterfaces.diffusion2DGrowth;
 
 import kineticMonteCarlo.lattice.Abstract2DDiffusionLattice;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferStrategy;
@@ -14,6 +15,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import kineticMonteCarlo.atom.AbstractAtom;
 import kineticMonteCarlo.lattice.AgAgLattice;
 
 /**
@@ -113,5 +115,65 @@ public abstract class AbstractKmcCanvas extends Canvas {
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+  
+  @Override
+  public void paint(Graphics g) { //real drawing method
+
+    super.paint(g);
+
+    g.setColor(Color.black);
+    g.fillRect(baseX, baseY, (int) (lattice.getCartSizeX() * scale), (int) (lattice.getCartSizeY() * scale));
+
+    for (int j = 0; j < lattice.getHexaSizeJ(); j++) {          //Y
+      int Y = (int) (Math.round(lattice.getCartY(j) * scale) + baseY);
+      for (int i = 0; i < lattice.getHexaSizeI(); i++) {
+        int X = (int) Math.round(lattice.getCartX(i,j) * scale) + baseX;
+        byte type = lattice.getAtom(i, j).getType();
+        switch (type) {
+          case AbstractAtom.TERRACE:
+            g.setColor(Color.RED);
+            break;
+          case AbstractAtom.CORNER:
+            g.setColor(Color.MAGENTA);
+            break;
+          case AbstractAtom.EDGE:
+            g.setColor(Color.ORANGE);
+            break;
+          case AbstractAtom.KINK:
+            g.setColor(Color.YELLOW);
+            break;
+          case AbstractAtom.BULK:
+            g.setColor(Color.GREEN);
+            break;
+          case 5: // imposible
+            g.setColor(Color.WHITE);
+            break;
+          case 6: // imposible
+            g.setColor(Color.CYAN);
+            break;
+          case 7: // imposible
+            g.setColor(Color.BLUE);
+            break;
+        }
+
+        if (scale < 3) {
+          if (lattice.getAtom(i, j).isOccupied()) {
+            g.fillRect(X, Y, scale, scale);
+          } else if (!lattice.getAtom(i, j).isOutside()) {
+            g.drawRect(X, Y, scale, scale);
+          }
+
+        } else {
+
+          if (lattice.getAtom(i, j).isOccupied()) {
+            g.fillOval(X, Y, scale, scale);
+          } else if (!lattice.getAtom(i, j).isOutside()) {
+            g.drawOval(X, Y, scale, scale);
+          }
+        }
+      }
+    }
+    g.dispose();
   }
 }
--- src/graphicInterfaces/diffusion2DGrowth/agAgGrowth/AgAgKmcCanvas.java
+++ src/graphicInterfaces/diffusion2DGrowth/agAgGrowth/AgAgKmcCanvas.java
@@ -6,74 +6,11 @@ package graphicInterfaces.diffusion2DGrowth.agAgGrowth;
 
 import graphicInterfaces.diffusion2DGrowth.AbstractKmcCanvas;
 import kineticMonteCarlo.lattice.Abstract2DDiffusionLattice;
-import java.awt.Color;
-import java.awt.Graphics;
-import kineticMonteCarlo.atom.AbstractAtom;
 
 public class AgAgKmcCanvas extends AbstractKmcCanvas {
 
   public AgAgKmcCanvas(Abstract2DDiffusionLattice lattice) {
     super(lattice);
-  }
-
-  @Override
-  public void paint(Graphics g) { //real drawing method
-
-    super.paint(g);
-
-    g.setColor(Color.black);
-    g.fillRect(baseX, baseY, (int) (lattice.getCartSizeX() * scale), (int) (lattice.getCartSizeY() * scale));
-
-    for (int j = 0; j < lattice.getHexaSizeJ(); j++) {          //Y
-      int Y = (int) (Math.round(lattice.getCartY(j) * scale) + baseY);
-      for (int i = 0; i < lattice.getHexaSizeI(); i++) {
-        int X = (int) Math.round(lattice.getCartX(i,j) * scale) + baseX;
-        byte type = lattice.getAtom(i, j).getType();
-        switch (type) {
-          case AbstractAtom.TERRACE:
-            g.setColor(Color.RED);
-            break;
-          case AbstractAtom.CORNER:
-            g.setColor(Color.MAGENTA);
-            break;
-          case AbstractAtom.EDGE:
-            g.setColor(Color.ORANGE);
-            break;
-          case AbstractAtom.KINK:
-            g.setColor(Color.YELLOW);
-            break;
-          case AbstractAtom.BULK:
-            g.setColor(Color.GREEN);
-            break;
-          case 5: // imposible
-            g.setColor(Color.WHITE);
-            break;
-          case 6: // imposible
-            g.setColor(Color.CYAN);
-            break;
-          case 7: // imposible
-            g.setColor(Color.BLUE);
-            break;
-        }
-
-        if (scale < 3) {
-          if (lattice.getAtom(i, j).isOccupied()) {
-            g.fillRect(X, Y, scale, scale);
-          } else if (!lattice.getAtom(i, j).isOutside()) {
-            g.drawRect(X, Y, scale, scale);
-          }
-
-        } else {
-
-          if (lattice.getAtom(i, j).isOccupied()) {
-            g.fillOval(X, Y, scale, scale);
-          } else if (!lattice.getAtom(i, j).isOutside()) {
-            g.drawOval(X, Y, scale, scale);
-          }
-        }
-      }
-    }
-    g.dispose();
   }
 
 }
