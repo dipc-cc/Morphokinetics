3e4d382a056655120d824b07175fdafa17cea530
Joseba <joseba.alberdi@ehu.eus>
2015-09-07 12:48:29 +0200

    Formatting of the output

--- test/TestRunner.java
+++ test/TestRunner.java
@@ -13,13 +13,14 @@ import org.junit.runner.notification.Failure;
  * @author J. Alberdi-Rodriguez
  */
 public class TestRunner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(MkTestSuite.class);
-      for (Failure failure : result.getFailures()) {
-        System.out.println("Test number " );
-         System.out.println(failure.toString());
-      }
-      System.out.println(result.wasSuccessful());
-      System.out.println("Has finished");
-   }
-}  	
+
+  public static void main(String[] args) {
+    Result result = JUnitCore.runClasses(MkTestSuite.class);
+    for (Failure failure : result.getFailures()) {
+      System.out.println("###Test number ");
+      System.out.println(failure.toString());
+    }
+    System.out.println(result.wasSuccessful());
+    System.out.println("###Has finished");
+  }
+}
