c11ee2336c19f37dcf4574590e8c1590f6e59dbb
Joseba <joseba.alberdi@ehu.es>
2015-10-14 11:25:25 +0200

    Naming convention

--- src/samples/convergences/agAgGrowth/AgAgKmcConvergence.java
+++ src/samples/convergences/agAgGrowth/AgAgKmcConvergence.java
@@ -24,38 +24,38 @@ public class AgAgKmcConvergence {
   public static void main(String[] args) {
 
     new StaticRandom();
-    float experitental_temp = 135;
-    double deposition_rate = new AgAgRatesFactory().getDepositionRate(experitental_temp);
-    double island_density = new AgAgRatesFactory().getIslandDensity(experitental_temp);
-    double diffusion_rate = new AgAgRatesFactory().getRates(experitental_temp)[0];
+    float experitentalTemp = 135;
+    double depositionRate = new AgAgRatesFactory().getDepositionRate(experitentalTemp);
+    double islandDensity = new AgAgRatesFactory().getIslandDensity(experitentalTemp);
+    double diffusionRate = new AgAgRatesFactory().getRates(experitentalTemp)[0];
 
     GeneticAlgorithmConfiguration geneticConfiguration = new GeneticAlgorithmConfigFactory()
-            .create_Ag_Ag_convergence_configuration(diffusion_rate, island_density, deposition_rate);
+            .create_Ag_Ag_convergence_configuration(diffusionRate, islandDensity, depositionRate);
 
-    GeneticAlgorithm GA = new GeneticAlgorithm(geneticConfiguration);
+    GeneticAlgorithm ga = new GeneticAlgorithm(geneticConfiguration);
 
-    new GaProgressFrame(GA).setVisible(true);
+    new GaProgressFrame(ga).setVisible(true);
     AbstractPsdEvaluation evaluator = geneticConfiguration.mainEvaluator;
 
     //--------------------------------
     evaluator.setRepeats(evaluator.getRepeats() * 5);
-    Individual individual = new Individual(new AgAgRatesFactory().getRates(experitental_temp));
-    float[][] experimentalPSD = evaluator.calculatePsdFromIndividual(individual);
+    Individual individual = new Individual(new AgAgRatesFactory().getRates(experitentalTemp));
+    float[][] experimentalPsd = evaluator.calculatePsdFromIndividual(individual);
     double simulationTime = individual.getSimulationTime();
     evaluator.setRepeats(evaluator.getRepeats() / 5);
          //--------------------------------
 
-    geneticConfiguration.setExperimentalPSD(experimentalPSD);
+    geneticConfiguration.setExperimentalPSD(experimentalPsd);
     geneticConfiguration.expected_simulation_time = simulationTime;
 
-    GA.initialize();
-    GA.iterate(100);
-    printResult(GA);
+    ga.initialize();
+    ga.iterate(100);
+    printResult(ga);
 
   }
 
-  private static void printResult(IGeneticAlgorithm GA) {
-    Individual individual = GA.getIndividual(0);
+  private static void printResult(IGeneticAlgorithm ga) {
+    Individual individual = ga.getIndividual(0);
     System.out.print(individual.getTotalError() + " ");
     for (int gene = 0; gene < individual.getGeneSize(); gene++) {
       System.out.print(individual.getGene(gene) + " ");
