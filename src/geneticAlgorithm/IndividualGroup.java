d50b9c1751818fd950cf41d1f12a3205b1bb1415
Joseba <joseba.alberdi@ehu.es>
2015-10-14 14:19:28 +0200

    Formatting

--- src/geneticAlgorithm/IndividualGroup.java
+++ src/geneticAlgorithm/IndividualGroup.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 @SuppressWarnings("serial")
 public class IndividualGroup extends ArrayList<Individual> {
 
-	public IndividualGroup(int capacity) {		
-		for (int i = 0; i < capacity; i++) {
-			add(null);
-		}
-	}
+  public IndividualGroup(int capacity) {
+    for (int i = 0; i < capacity; i++) {
+      add(null);
+    }
+  }
 }
