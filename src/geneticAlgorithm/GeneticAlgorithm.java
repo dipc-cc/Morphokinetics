3d5cd390cc2375cba1b3993ac2ee0cad7bee43a2
Joseba <joseba.alberdi@ehu.es>
2015-10-14 12:21:10 +0200

    Naming convention and formatting (two space indent...)

--- src/geneticAlgorithm/GeneticAlgorithm.java
+++ src/geneticAlgorithm/GeneticAlgorithm.java
@@ -13,130 +13,125 @@ import graphicInterfaces.gaConvergence.IgaProgressFrame;
  */
 public class GeneticAlgorithm implements IGeneticAlgorithm {
 
-    private Population population;
-    private GeneticAlgorithmConfiguration config;
-    private BasicEvaluator evaluator;
-    private int currentIteration = 0;
-    private int totalIterations = 1;
-    private IgaProgressFrame graphics;
+  private Population population;
+  private GeneticAlgorithmConfiguration config;
+  private BasicEvaluator evaluator;
+  private int currentIteration = 0;
+  private int totalIterations = 1;
+  private IgaProgressFrame graphics;
 
-    public GeneticAlgorithm(GeneticAlgorithmConfiguration configuration) {
-        this.config = configuration;
-        this.evaluator = new BasicEvaluator();
+  public GeneticAlgorithm(GeneticAlgorithmConfiguration configuration) {
+    this.config = configuration;
+    this.evaluator = new BasicEvaluator();
+  }
+
+  public IGeneticAlgorithm initialize() {
+    this.population = config.initialization.createRandomPopulation(config.populationSize);
+    this.config.restriction.apply(this.population);
+    this.evaluator.evaluate_and_order(this.population, this.config.mainEvaluator, this.config.otherEvaluators);
+
+    System.out.println("=============");
+    for (int i = 0; i < this.population.size(); i++) {
+      System.out.print(population.getIndividual(i).getTotalError() + "| \t");
+      for (int a = 0; a < population.getIndividual(i).getGeneSize(); a++) {
+        System.out.print(population.getIndividual(i).getGene(a) + " \t");
+      }
+      System.out.println();
     }
 
-    public IGeneticAlgorithm initialize() {
-        this.population = config.initialization.createRandomPopulation(config.population_size);
-        this.config.restriction.apply(this.population);
-        this.evaluator.evaluate_and_order(this.population, this.config.mainEvaluator, this.config.otherEvaluators);
-        
-         System.out.println("=============");
-         for (int i = 0; i < this.population.size(); i++) {
-        System.out.print(population.getIndividual(i).getTotalError()+"| \t");
-        for (int a=0;a<population.getIndividual(i).getGeneSize();a++) System.out.print(population.getIndividual(i).getGene(a)+" \t");
-         System.out.println();
-         }
-        
-        this.scaleIndividualRates(this.population);
-        
-        if (graphics != null) {
-            graphics.clear();
-        }
-        
-        
+    this.scaleIndividualRates(this.population);
 
-        
-        return this;
-    }
-    
-    
-    
-
-    private void iterateOneStep() {
-        IndividualGroup[] couples = this.config.selection.Select(this.population, this.config.offspring_size);
-        Population offspring = this.config.recombination.recombinate(couples);
-
-        int geneSize = population.getIndividual(0).getGeneSize();
-        this.config.mutation.mutate(offspring, this.config.restriction.getNonFixedGenes(geneSize));
-        this.config.restriction.apply(offspring);
-        this.evaluator.evaluate_and_order(offspring, this.config.mainEvaluator, this.config.otherEvaluators);
-
-        //sometimes it is good to reevaluate the whole population
-        if (currentIteration > 0 && currentIteration % 25 == 0) {
-            this.scaleIndividualRates(this.population);   
-            this.config.restriction.apply(this.population);
-            this.evaluator.evaluate_and_order(this.population, this.config.mainEvaluator, this.config.otherEvaluators);;   
-        }
-              
-        
-       this.config.reinsertion.Reinsert(population, offspring, this.config.population_replacements);
-
+    if (graphics != null) {
+      graphics.clear();
     }
 
-    public void iterate(int steps) {
-        totalIterations = steps;
-        for (int i = 0; i < steps; i++) {
-            currentIteration = i;
-            iterateOneStep();
-            addToGraphics();
+    return this;
+  }
 
-        }
+  private void iterateOneStep() {
+    IndividualGroup[] couples = this.config.selection.Select(this.population, this.config.offspringSize);
+    Population offspring = this.config.recombination.recombinate(couples);
+
+    int geneSize = population.getIndividual(0).getGeneSize();
+    this.config.mutation.mutate(offspring, this.config.restriction.getNonFixedGenes(geneSize));
+    this.config.restriction.apply(offspring);
+    this.evaluator.evaluate_and_order(offspring, this.config.mainEvaluator, this.config.otherEvaluators);
+
+    //sometimes it is good to reevaluate the whole population
+    if (currentIteration > 0 && currentIteration % 25 == 0) {
+      this.scaleIndividualRates(this.population);
+      this.config.restriction.apply(this.population);
+      this.evaluator.evaluate_and_order(this.population, this.config.mainEvaluator, this.config.otherEvaluators);;
     }
 
-    public double getBestError() {
-        return population.getIndividual(0).getTotalError();
+    this.config.reinsertion.Reinsert(population, offspring, this.config.populationReplacements);
+
+  }
+
+  public void iterate(int steps) {
+    totalIterations = steps;
+    for (int i = 0; i < steps; i++) {
+      currentIteration = i;
+      iterateOneStep();
+      addToGraphics();
+
     }
+  }
 
-    public Individual getIndividual(int pos) {
-        return population.getIndividual(pos);
+  public double getBestError() {
+    return population.getIndividual(0).getTotalError();
+  }
+
+  public Individual getIndividual(int pos) {
+    return population.getIndividual(pos);
+  }
+
+  public Individual getBestIndividual() {
+    return population.getIndividual(0);
+  }
+
+  public float[] getProgressPercent() {
+
+    float[] progress = new float[3];
+
+    progress[0] = currentIteration * 100.0f / totalIterations;
+
+    float[] subprogress = evaluator.getProgressPercent();
+    progress[1] = subprogress[0];
+    progress[2] = subprogress[1];
+
+    return progress;
+  }
+
+  @Override
+  public int getCurrentIteration() {
+    return currentIteration;
+  }
+
+  @Override
+  public int getTotalIterations() {
+    return totalIterations;
+  }
+
+  @Override
+  public void setGraphics(IgaProgressFrame graphics) {
+    this.graphics = graphics;
+  }
+
+  private void addToGraphics() {
+    if (graphics != null) {
+      graphics.addNewBestIndividual(getBestIndividual());
     }
+  }
 
-    public Individual getBestIndividual() {
-        return population.getIndividual(0);
+  private void scaleIndividualRates(Population population) {
+
+    for (int i = 0; i < population.size(); i++) {
+      Individual individual = population.getIndividual(i);
+      double factor = config.expectedSimulationTime / individual.getSimulationTime();
+      for (int j = 0; j < individual.getGeneSize(); j++) {
+        individual.setGene(j, individual.getGene(j) / factor);
+      }
     }
-
-    public float[] getProgressPercent() {
-
-        float[] progress = new float[3];
-
-        progress[0] = currentIteration * 100.0f / totalIterations;
-
-        float[] subprogress = evaluator.getProgressPercent();
-        progress[1] = subprogress[0];
-        progress[2] = subprogress[1];
-
-        return progress;
-    }
-
-    @Override
-    public int getCurrentIteration() {
-        return currentIteration;
-    }
-
-    @Override
-    public int getTotalIterations() {
-        return totalIterations;
-    }
-
-    @Override
-    public void setGraphics(IgaProgressFrame graphics) {
-        this.graphics = graphics;
-    }
-
-    private void addToGraphics() {
-        if (graphics != null) {
-            graphics.addNewBestIndividual(getBestIndividual());
-        }
-    }
-
-    private void scaleIndividualRates(Population population) {
-
-        for (int i = 0; i < population.size(); i++) {
-            Individual individual = population.getIndividual(i);
-            double factor = config.expected_simulation_time / individual.getSimulationTime();
-            for (int j = 0; j < individual.getGeneSize(); j++) {
-                individual.setGene(j, individual.getGene(j) / factor);
-            }
-        }
-    }
+  }
 }
--- src/geneticAlgorithm/GeneticAlgorithmDcmaEs.java
+++ src/geneticAlgorithm/GeneticAlgorithmDcmaEs.java
@@ -13,221 +13,219 @@ import geneticAlgorithm.geneticOperators.reinsertion.ElitistAllReinsertion;
 import graphicInterfaces.gaConvergence.IgaProgressFrame;
 
 public class GeneticAlgorithmDcmaEs implements IGeneticAlgorithm {
-	
-	private Population population;
-    private GeneticAlgorithmConfiguration config;
-    private BasicEvaluator evaluator;
-    private int currentIteration = 0;
-    private int totalIterations = 1;
-    private IgaProgressFrame graphics;
-    
-    private DcmaEsConfig dcmaEsConfig;
-    
-    Integer[] offIndex;
-    Integer[] reducedIndex;
-    
-    public GeneticAlgorithmDcmaEs(GeneticAlgorithmConfiguration configuration) {
-    	config = configuration;
-    	evaluator = new BasicEvaluator();
+
+  private Population population;
+  private GeneticAlgorithmConfiguration config;
+  private BasicEvaluator evaluator;
+  private int currentIteration = 0;
+  private int totalIterations = 1;
+  private IgaProgressFrame graphics;
+
+  private DcmaEsConfig dcmaEsConfig;
+
+  Integer[] offIndex;
+  Integer[] reducedIndex;
+
+  public GeneticAlgorithmDcmaEs(GeneticAlgorithmConfiguration configuration) {
+    config = configuration;
+    evaluator = new BasicEvaluator();
+  }
+
+  @Override
+  public IGeneticAlgorithm initialize() {
+    population = config.initialization.createRandomPopulation(config.populationSize);
+
+    // Inicializamos la clase que contiene variables globales del algoritmo.
+    dcmaEsConfig = new DcmaEsConfig(config, population.getIndividual(0).getGeneSize());
+    config.recombination = new DifferentialRecombination(dcmaEsConfig);
+    config.mutation = new CrossoverMutator(dcmaEsConfig);
+    config.reinsertion = new ElitistAllReinsertion(dcmaEsConfig);
+
+    config.restriction.apply(this.population);
+
+    //evaluator.evaluate_and_order(this.population, config.mainEvaluator, config.otherEvaluators);
+    double[] fitness = config.mainEvaluator.evaluate(population);
+    //double[] fitness = myEvaluate(population);
+    for (int i = 0; i < fitness.length; i++) {
+      dcmaEsConfig.offFitness.set(i, fitness[i]);
+      population.getIndividual(i).setError(0, fitness[i]);
+      dcmaEsConfig.counteval++;
     }
 
-	@Override
-	public IGeneticAlgorithm initialize() {
-		population = config.initialization.createRandomPopulation(config.population_size);
-		
-		// Inicializamos la clase que contiene variables globales del algoritmo.
-		dcmaEsConfig = new DcmaEsConfig(config, population.getIndividual(0).getGeneSize());
-		config.recombination = new DifferentialRecombination(dcmaEsConfig);
-		config.mutation = new CrossoverMutator(dcmaEsConfig);
-		config.reinsertion = new ElitistAllReinsertion(dcmaEsConfig);
-        
-		config.restriction.apply(this.population);
-        
-		//evaluator.evaluate_and_order(this.population, config.mainEvaluator, config.otherEvaluators);
-		double[] fitness = config.mainEvaluator.evaluate(population);
-		//double[] fitness = myEvaluate(population);
-		for (int i = 0; i < fitness.length; i++) {
-			dcmaEsConfig.offFitness.set(i, fitness[i]);
-			population.getIndividual(i).setError(0, fitness[i]);
-			dcmaEsConfig.counteval++;
-		}
-		
-		offIndex = dcmaEsConfig.offFitness.sortedIndexes();
-		reducedIndex = Arrays.copyOfRange(offIndex, 0, Double.valueOf(dcmaEsConfig.mu).intValue());
-		dcmaEsConfig.offX = new RichMatrix(population);
-		dcmaEsConfig.xmean = dcmaEsConfig.offX.recombinate(reducedIndex).multiply(dcmaEsConfig.weights);
-		dcmaEsConfig.sigma = dcmaEsConfig.offX.recombinate(reducedIndex).transpose().std().std();
-		
-		Population p= population;
-		p.order();
-        System.out.println("=============");
-        for (int i = 0; i < p.size(); i++) {
-        	System.out.print(p.getIndividual(i).getTotalError()+"| \t");
-        
-        	for (int a=0;a<p.getIndividual(i).getGeneSize();a++) {
-        		System.out.print(p.getIndividual(i).getGene(a)+" \t");
-        	}
-        	
-        	System.out.println();
-        }
-        
-        if (graphics != null) {
-            graphics.clear();
-        }
-        
-        return this;
-	}
-	
-	private void iterateOneStep() {		
-		IndividualGroup[] trios = config.selection.Select(population, config.population_size);
-        Population offspring = config.recombination.recombinate(trios);
+    offIndex = dcmaEsConfig.offFitness.sortedIndexes();
+    reducedIndex = Arrays.copyOfRange(offIndex, 0, Double.valueOf(dcmaEsConfig.mu).intValue());
+    dcmaEsConfig.offX = new RichMatrix(population);
+    dcmaEsConfig.xmean = dcmaEsConfig.offX.recombinate(reducedIndex).multiply(dcmaEsConfig.weights);
+    dcmaEsConfig.sigma = dcmaEsConfig.offX.recombinate(reducedIndex).transpose().std().std();
 
-        config.mutation.mutate(offspring, null);
-        config.restriction.apply(offspring);
-        /*for (int i = 0; i < offspring.size(); i++) {
-        	Individual ind = offspring.getIndividual(i);
-        	for (int j = 0; j < ind.getGeneSize(); j++) {
-        		double val = ind.getGene(j);
-        		ind.setGene(j, Math.max(-500, Math.min(500, val)));
-        	}
-        }*/
-        //this.evaluator.evaluate_and_order(offspring, this.config.mainEvaluator, this.config.otherEvaluators);
-        double[] fitness = config.mainEvaluator.evaluate(offspring);
-        //double[] fitness = myEvaluate(offspring);
-		for (int i = 0; i < fitness.length; i++) {
-			//dcmaEsConfig.offFitness.set(i, fitness[i]);
-			offspring.getIndividual(i).setError(0, fitness[i]);
-			dcmaEsConfig.counteval++;
-		}
+    Population p = population;
+    p.order();
+    System.out.println("=============");
+    for (int i = 0; i < p.size(); i++) {
+      System.out.print(p.getIndividual(i).getTotalError() + "| \t");
+
+      for (int a = 0; a < p.getIndividual(i).getGeneSize(); a++) {
+        System.out.print(p.getIndividual(i).getGene(a) + " \t");
+      }
+
+      System.out.println();
+    }
+
+    if (graphics != null) {
+      graphics.clear();
+    }
+
+    return this;
+  }
+
+  private void iterateOneStep() {
+    IndividualGroup[] trios = config.selection.Select(population, config.populationSize);
+    Population offspring = config.recombination.recombinate(trios);
+
+    config.mutation.mutate(offspring, null);
+    config.restriction.apply(offspring);
+    /*for (int i = 0; i < offspring.size(); i++) {
+     Individual ind = offspring.getIndividual(i);
+     for (int j = 0; j < ind.getGeneSize(); j++) {
+     double val = ind.getGene(j);
+     ind.setGene(j, Math.max(-500, Math.min(500, val)));
+     }
+     }*/
+    //this.evaluator.evaluate_and_order(offspring, this.config.mainEvaluator, this.config.otherEvaluators);
+    double[] fitness = config.mainEvaluator.evaluate(offspring);
+    //double[] fitness = myEvaluate(offspring);
+    for (int i = 0; i < fitness.length; i++) {
+      //dcmaEsConfig.offFitness.set(i, fitness[i]);
+      offspring.getIndividual(i).setError(0, fitness[i]);
+      dcmaEsConfig.counteval++;
+    }
 
         //sometimes it is good to reevaluate the whole population
-       // if (currentIteration > 0 && currentIteration % 25 == 0) {
-            config.restriction.apply(population);
-        	/*for (int i = 0; i < population.size(); i++) {
-            	Individual ind = population.getIndividual(i);
-            	for (int j = 0; j < ind.getGeneSize(); j++) {
-            		double val = ind.getGene(j);
-            		ind.setGene(j, Math.max(-500, Math.min(500, val)));
-            	}
-            }*/
+    // if (currentIteration > 0 && currentIteration % 25 == 0) {
+    config.restriction.apply(population);
+    /*for (int i = 0; i < population.size(); i++) {
+     Individual ind = population.getIndividual(i);
+     for (int j = 0; j < ind.getGeneSize(); j++) {
+     double val = ind.getGene(j);
+     ind.setGene(j, Math.max(-500, Math.min(500, val)));
+     }
+     }*/
             //this.evaluator.evaluate_and_order(this.population, this.config.mainEvaluator, this.config.otherEvaluators);;
-        
+
 		//fitness = config.mainEvaluator.evaluate(population);
-           
 		//fitness = myEvaluate(population);
-           
-	/*	
-		 for (int i = 0; i < fitness.length; i++) {
-    			dcmaEsConfig.offFitness.set(i, fitness[i]);
-    			population.getIndividual(i).setError(0, fitness[i]);
-    			dcmaEsConfig.counteval++;
-    		}
-        }*/
-                   
-       population = config.reinsertion.Reinsert(population, offspring, 0);
-       
-       offIndex = dcmaEsConfig.offFitness.sortedIndexes();
-       dcmaEsConfig.offX = new RichMatrix(population);
-    }
-	
-	@Override
-	public void iterate(int steps) {
-		totalIterations = steps;
-        
-		while (dcmaEsConfig.counteval < dcmaEsConfig.stopEval && currentIteration < steps) {
-			currentIteration++;
-			
-			iterateOneStep();
-			
-			addToGraphics();
-			
-			if (dcmaEsConfig.offFitness.apply(OperationFactory.deduct(dcmaEsConfig.offFitness.min())).allLessOrEqualThan(dcmaEsConfig.stopFitness)
-					|| dcmaEsConfig.D.max() > 1e7 * dcmaEsConfig.D.min()) {
-				break;
-			}
-		     if(this.getBestError()<180)
-		    	 break;
-		}
-		
-		offIndex = dcmaEsConfig.offFitness.sortedIndexes();
-		dcmaEsConfig.offX = dcmaEsConfig.offX.recombinate(offIndex);
-		double fmin = dcmaEsConfig.offFitness.get(0);
-		RichArray xmin = dcmaEsConfig.offX.get(offIndex[0]);
-		
-		System.out.println(dcmaEsConfig.counteval + ": " + fmin);
-		System.out.println(xmin);
-	}
-	
-	private double[] myEvaluate(Population population) {
-		double[] values = new double[population.size()];
-		
-		for (int i = 0; i < population.size(); i++) {
-			values[i] = TestSuite.fschwefel(new RichArray(population.getIndividual(i)));
-		}
-		
-		return values;
-	}
+    /*	
+     for (int i = 0; i < fitness.length; i++) {
+     dcmaEsConfig.offFitness.set(i, fitness[i]);
+     population.getIndividual(i).setError(0, fitness[i]);
+     dcmaEsConfig.counteval++;
+     }
+     }*/
+    population = config.reinsertion.Reinsert(population, offspring, 0);
 
-	@Override
-	public int getCurrentIteration() {
-		return currentIteration;
-	}
+    offIndex = dcmaEsConfig.offFitness.sortedIndexes();
+    dcmaEsConfig.offX = new RichMatrix(population);
+  }
 
-	@Override
-	public int getTotalIterations() {
-		return totalIterations;
-	}
-	
-	@Override
-	public float[] getProgressPercent() {
-		float[] progress = new float[3];
+  @Override
+  public void iterate(int steps) {
+    totalIterations = steps;
 
-        progress[0] = currentIteration * 100.0f / totalIterations;
+    while (dcmaEsConfig.counteval < dcmaEsConfig.stopEval && currentIteration < steps) {
+      currentIteration++;
 
-        float[] subprogress = evaluator.getProgressPercent();
-        progress[1] = subprogress[0];
-        progress[2] = subprogress[1];
+      iterateOneStep();
 
-        return progress;
-	}
+      addToGraphics();
 
-	@Override
-	public void setGraphics(IgaProgressFrame graphics) {
-		this.graphics = graphics;
-	}
-	
-	private void addToGraphics() {
-        if (graphics != null) {
-            graphics.addNewBestIndividual(getBestIndividual());
-        }
-    }
-	
-	public Individual getBestIndividual() {
-		Population p =  new Population(population.getIndividuals());
-		p.order();
-		return p.getIndividual(0);
+      if (dcmaEsConfig.offFitness.apply(OperationFactory.deduct(dcmaEsConfig.offFitness.min())).allLessOrEqualThan(dcmaEsConfig.stopFitness)
+              || dcmaEsConfig.D.max() > 1e7 * dcmaEsConfig.D.min()) {
+        break;
+      }
+      if (this.getBestError() < 180) {
+        break;
+      }
     }
 
-	@Override
-	public double getBestError() {
-		Population p =  new Population(population.getIndividuals());
-		p.order();
-		return p.getIndividual(0).getTotalError();
-	}
+    offIndex = dcmaEsConfig.offFitness.sortedIndexes();
+    dcmaEsConfig.offX = dcmaEsConfig.offX.recombinate(offIndex);
+    double fmin = dcmaEsConfig.offFitness.get(0);
+    RichArray xmin = dcmaEsConfig.offX.get(offIndex[0]);
 
-	@Override
-	public Individual getIndividual(int pos) {
-		return population.getIndividual(pos);
-	}
-	
-	private void scaleIndividualRates(Population population) {
+    System.out.println(dcmaEsConfig.counteval + ": " + fmin);
+    System.out.println(xmin);
+  }
 
-        for (int i = 0; i < population.size(); i++) {
-            Individual individual = population.getIndividual(i);
-            double factor = config.expected_simulation_time / individual.getSimulationTime();
-            for (int j = 0; j < individual.getGeneSize(); j++) {
-                individual.setGene(j, individual.getGene(j) / factor);
-            }
-        }
+  private double[] myEvaluate(Population population) {
+    double[] values = new double[population.size()];
+
+    for (int i = 0; i < population.size(); i++) {
+      values[i] = TestSuite.fschwefel(new RichArray(population.getIndividual(i)));
     }
+
+    return values;
+  }
+
+  @Override
+  public int getCurrentIteration() {
+    return currentIteration;
+  }
+
+  @Override
+  public int getTotalIterations() {
+    return totalIterations;
+  }
+
+  @Override
+  public float[] getProgressPercent() {
+    float[] progress = new float[3];
+
+    progress[0] = currentIteration * 100.0f / totalIterations;
+
+    float[] subprogress = evaluator.getProgressPercent();
+    progress[1] = subprogress[0];
+    progress[2] = subprogress[1];
+
+    return progress;
+  }
+
+  @Override
+  public void setGraphics(IgaProgressFrame graphics) {
+    this.graphics = graphics;
+  }
+
+  private void addToGraphics() {
+    if (graphics != null) {
+      graphics.addNewBestIndividual(getBestIndividual());
+    }
+  }
+
+  public Individual getBestIndividual() {
+    Population p = new Population(population.getIndividuals());
+    p.order();
+    return p.getIndividual(0);
+  }
+
+  @Override
+  public double getBestError() {
+    Population p = new Population(population.getIndividuals());
+    p.order();
+    return p.getIndividual(0).getTotalError();
+  }
+
+  @Override
+  public Individual getIndividual(int pos) {
+    return population.getIndividual(pos);
+  }
+
+  private void scaleIndividualRates(Population population) {
+
+    for (int i = 0; i < population.size(); i++) {
+      Individual individual = population.getIndividual(i);
+      double factor = config.expectedSimulationTime / individual.getSimulationTime();
+      for (int j = 0; j < individual.getGeneSize(); j++) {
+        individual.setGene(j, individual.getGene(j) / factor);
+      }
+    }
+  }
 }
