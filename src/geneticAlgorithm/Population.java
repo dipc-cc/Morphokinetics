06c2788461dd3d2dcf83f9ee700e3e470c7d778a
Joseba <joseba.alberdi@ehu.es>
2015-10-14 13:58:53 +0200

    Formatting, English names and Javadoc from comments

--- src/geneticAlgorithm/Population.java
+++ src/geneticAlgorithm/Population.java
@@ -8,77 +8,86 @@ package geneticAlgorithm;
  *
  * @author Nestor
  *
- * This class just model a GA population, it can be the main population or an
- * offspring
+ * This class just model a Genetic Algorithm population, it can be the main population or an offspring
  *
  *
  */
 public class Population {
 
-    public Individual[] getIndividuals() {
-		return Individuals;
-	}
+  public Individual[] getIndividuals() {
+    return Individuals;
+  }
 
-	private Individual[] Individuals;
+  private Individual[] Individuals;
 
-    public Population(int size) {
-        Individuals = new Individual[size];
-    }
-    public Population(Individual[] p) {
-        Individuals = new Individual[p.length];
-        for ( int i =0; i< p.length; i++)
-        	Individuals[i]= p[i];
-        
-    }
-    public Individual getIndividual(int pos) {
-        return Individuals[pos];
+  public Population(int size) {
+    Individuals = new Individual[size];
+  }
+
+  public Population(Individual[] p) {
+    Individuals = new Individual[p.length];
+    for (int i = 0; i < p.length; i++) {
+      Individuals[i] = p[i];
     }
 
-    public void setIndividual(Individual ind, int pos) {
-        Individuals[pos] = ind;
+  }
+
+  public Individual getIndividual(int pos) {
+    return Individuals[pos];
+  }
+
+  public void setIndividual(Individual ind, int pos) {
+    Individuals[pos] = ind;
+  }
+
+  public int size() {
+    return Individuals.length;
+  }
+
+  /**
+   * Orders the population, from the least to the more error.
+   */
+  public void order() {
+
+    quicksort(Individuals, 0, Individuals.length - 1);
+  }
+
+  /**
+   * Quicksort-based ordering algorithm.
+   * @param a
+   * @param left
+   * @param right 
+   */
+  private static void quicksort(Individual[] a, int left, int right) {
+    int i = left;
+    int j = right;
+    Individual aux;
+
+    double pivote = a[(left + right) / 2].getTotalError();
+
+    do {
+      while (a[i].getTotalError() < pivote) {
+
+        i++;
+      }
+      while (a[j].getTotalError() > pivote) {
+        j--;
+
+      }
+      if (i <= j) {
+
+        aux = a[i];
+        a[i] = a[j];
+        a[j] = aux;
+        i++;
+        j--;
+      }
+    } while (i <= j);
+    if (left < j) {
+      quicksort(a, left, j);
     }
-
-    public int size() {
-        return Individuals.length;
+    if (i < right) {
+      quicksort(a, i, right);
     }
-
-//orders the population, from the least to the more error.
-    public void order() {
-
-        quicksort(Individuals, 0, Individuals.length - 1);
-    }
-
-//quicksort-based ordering algorithm
-    private static void quicksort(Individual[] a, int izq, int der) {
-        int i = izq;
-        int j = der;
-        Individual aux;
-
-        double pivote = a[ (izq + der) / 2].getTotalError();
-
-        do {
-            while (a[i].getTotalError() < pivote) {
-
-                i++;
-            }
-            while (a[j].getTotalError() > pivote) {
-                j--;
-
-            }
-            if (i <= j) {
-
-                aux = a[i];
-                a[i] = a[j];
-                a[j] = aux;
-                i++;
-                j--;
-            }
-        } while (i <= j);
-        if (izq < j) {
-            quicksort(a, izq, j);
-        }
-        if (i < der) {
-            quicksort(a, i, der);
-        }
-    }
+  }
 }
