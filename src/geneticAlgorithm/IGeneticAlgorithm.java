94ce14deadd6cfe5095e3268f423e1ff0b33aa81
Joseba <joseba.alberdi@ehu.eus>
2015-10-15 15:53:54 +0200

    Formatting

--- src/geneticAlgorithm/IGeneticAlgorithm.java
+++ src/geneticAlgorithm/IGeneticAlgorithm.java
@@ -11,14 +11,19 @@ import graphicInterfaces.gaConvergence.IgaProgressFrame;
  * @author Nestor
  */
 public interface IGeneticAlgorithm extends IProgressable {
-    
-    public IGeneticAlgorithm initialize();
-    public int getCurrentIteration();
-    public int getTotalIterations();
-    public void setGraphics(IgaProgressFrame graphics);
-    public double getBestError();
-    public Individual getIndividual(int pos);
-    public void iterate(int steps);
-    
-    
+
+  public IGeneticAlgorithm initialize();
+
+  public int getCurrentIteration();
+
+  public int getTotalIterations();
+
+  public void setGraphics(IgaProgressFrame graphics);
+
+  public double getBestError();
+
+  public Individual getIndividual(int pos);
+
+  public void iterate(int steps);
+
 }
