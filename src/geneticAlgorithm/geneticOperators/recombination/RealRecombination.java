b2ddae569d48c67300954698e4d8840ee40cbda2
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:23:45 +0200

    Formatting and naming convention (variables should start with lowercase)

--- src/geneticAlgorithm/geneticOperators/recombination/RealRecombination.java
+++ src/geneticAlgorithm/geneticOperators/recombination/RealRecombination.java
@@ -14,28 +14,25 @@ import geneticAlgorithm.Population;
  */
 public class RealRecombination implements IRecombination {
 
-    private float OutBounds = 0.1f; // each gene of the child will be between the father and mother gene value +-15%
+  private float outBounds = 0.1f; // each gene of the child will be between the father and mother gene value +-15%
 
-    @Override
-    public Population recombinate(IndividualGroup[] groups) {
+  @Override
+  public Population recombinate(IndividualGroup[] groups) {
+    Population offspring = new Population(groups.length);
 
-        Population offspring = new Population(groups.length);
+    for (int i = 0; i < (offspring.size()); i++) {
+      Individual child = new Individual(groups[0].get(0).getGeneSize(),
+              groups[0].get(0).getErrorsSize());
 
+      for (int a = 0; a < child.getGeneSize(); a++) {
 
-        for (int i = 0; i < (offspring.size()); i++) {
-            Individual child = new Individual(groups[0].get(0).getGeneSize(),
-                    groups[0].get(0).getErrorsSize());
+        double e = utils.StaticRandom.raw() * (1 + 2 * outBounds) - outBounds;
+        child.setGene(a, Math.max(0.0, groups[i].get(0).getGene(a) * e + groups[i].get(1).getGene(a) * (1 - e)));
 
-            for (int a = 0; a < child.getGeneSize(); a++) {
-
-                double e = utils.StaticRandom.raw() * (1 + 2 * OutBounds) - OutBounds;
-                child.setGene(a, Math.max(0.0, groups[i].get(0).getGene(a) * e + groups[i].get(1).getGene(a) * (1 - e)));
-
-            }
-         offspring.setIndividual(child,i);   
-        }
-
-        return offspring;
-
+      }
+      offspring.setIndividual(child, i);
     }
+
+    return offspring;
+  }
 }
