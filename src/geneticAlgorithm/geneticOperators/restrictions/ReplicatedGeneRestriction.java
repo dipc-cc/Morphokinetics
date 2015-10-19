9a6edeec8fc360bb1d567916705c32de093d2e21
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:26:06 +0200

    Formatting

--- src/geneticAlgorithm/geneticOperators/restrictions/ReplicatedGeneRestriction.java
+++ src/geneticAlgorithm/geneticOperators/restrictions/ReplicatedGeneRestriction.java
@@ -12,24 +12,24 @@ import geneticAlgorithm.Individual;
  */
 public class ReplicatedGeneRestriction extends GeneRestriction {
 
-    private int genePositionToReplicate;
+  private int genePositionToReplicate;
 
-    public ReplicatedGeneRestriction(int genePositionToReplicate, int genePosition) {
-        super(genePosition);
-        this.genePositionToReplicate = genePositionToReplicate;
-    }
+  public ReplicatedGeneRestriction(int genePositionToReplicate, int genePosition) {
+    super(genePosition);
+    this.genePositionToReplicate = genePositionToReplicate;
+  }
 
-    @Override
-    public void restrictGene(Individual i) {
-        i.setGene(genePosition, i.getGene(genePositionToReplicate));
-    }
+  @Override
+  public void restrictGene(Individual i) {
+    i.setGene(genePosition, i.getGene(genePositionToReplicate));
+  }
 
-    @Override
-    public int getRestrictionType() {
-        if (genePosition == genePositionToReplicate) {
-            return GeneRestriction.NO_RESTRICTION;
-        } else {
-            return GeneRestriction.REPLICATE_GENE;
-        }
+  @Override
+  public int getRestrictionType() {
+    if (genePosition == genePositionToReplicate) {
+      return GeneRestriction.NO_RESTRICTION;
+    } else {
+      return GeneRestriction.REPLICATE_GENE;
     }
+  }
 }
