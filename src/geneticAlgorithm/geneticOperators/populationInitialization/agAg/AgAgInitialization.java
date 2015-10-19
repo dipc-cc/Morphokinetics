71e9cd1465601b8d894308256bcedc42594cd66f
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:17:55 +0200

    Formatting and Javadoc from comment

--- src/geneticAlgorithm/geneticOperators/populationInitialization/agAg/AgAgInitialization.java
+++ src/geneticAlgorithm/geneticOperators/populationInitialization/agAg/AgAgInitialization.java
@@ -15,35 +15,41 @@ import utils.StaticRandom;
  */
 public class AgAgInitialization implements IInitializator {
 
-    //robust initialization methods, it uses a logarithmic distribution of process rates, more similar to what is expected from a real system.
-    @Override
-    public Population createRandomPopulation(int populationSize) {
+  /**
+   * Robust initialisation methods, it uses a logarithmic distribution of process rates, more
+   * similar to what is expected from a real system.
+   *
+   * @param populationSize
+   * @return
+   */
+  @Override
+  public Population createRandomPopulation(int populationSize) {
 
-        Population p = new Population(populationSize);
-        for (int ind = 0; ind < p.size(); ind++) {
-            Individual i = new Individual(49, 4);
+    Population p = new Population(populationSize);
+    for (int ind = 0; ind < p.size(); ind++) {
+      Individual i = new Individual(49, 4);
 
-            for (int a = 0; a < 7; a++) {
-                for (int j = 0; j < 7; j++) {
-                    if (a == 1) {
-                        i.setGene(a * 7 + j, Math.max(0.1, 1000 * Math.pow(150000, StaticRandom.raw())));
-                    } else {
-                        i.setGene(a * 7 + j, Math.max(0.1, 0.1 * Math.pow(10000000, StaticRandom.raw())));
-                    }
-                }
-            }
-
-            p.setIndividual(i, ind);
-        	
-        	/*Individual i = new Individual(2, 4);
-        	
-        	for (int a = 0; a < 2; a++) {
-        		//i.setGene(a, Math.max(0.1, 1000 * Math.pow(150000, StaticRandom.raw())));
-        		i.setGene(a, -500 + 1000 * StaticRandom.raw());
-        	}
-        	
-        	p.setIndividual(i, ind);*/
+      for (int a = 0; a < 7; a++) {
+        for (int j = 0; j < 7; j++) {
+          if (a == 1) {
+            i.setGene(a * 7 + j, Math.max(0.1, 1000 * Math.pow(150000, StaticRandom.raw())));
+          } else {
+            i.setGene(a * 7 + j, Math.max(0.1, 0.1 * Math.pow(10000000, StaticRandom.raw())));
+          }
         }
-        return p;
+      }
+
+      p.setIndividual(i, ind);
+
+      /*Individual i = new Individual(2, 4);
+        	
+       for (int a = 0; a < 2; a++) {
+       //i.setGene(a, Math.max(0.1, 1000 * Math.pow(150000, StaticRandom.raw())));
+       i.setGene(a, -500 + 1000 * StaticRandom.raw());
+       }
+        	
+       p.setIndividual(i, ind);*/
     }
+    return p;
+  }
 }
