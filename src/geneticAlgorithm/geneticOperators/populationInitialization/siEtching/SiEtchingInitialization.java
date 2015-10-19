e466fa9403db12054a8793fedced053fa743d20d
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:18:56 +0200

    Formatting, Javadoc from comment and orthography corrections

--- src/geneticAlgorithm/geneticOperators/populationInitialization/siEtching/SiEtchingInitialization.java
+++ src/geneticAlgorithm/geneticOperators/populationInitialization/siEtching/SiEtchingInitialization.java
@@ -15,26 +15,32 @@ import utils.StaticRandom;
  */
 public class SiEtchingInitialization implements IInitializator {
 
-    //simplest way of initialization, a pure random value. Ag_ag initialization has a more robuts initialization method, recommended.
-    @Override
-    public Population createRandomPopulation(int populationSize) {
+  /**
+   * Simplest way of initialisation, a pure random value. AgAg initialisation has a more robust
+   * initialisation method, recommended.
+   *
+   * @param populationSize
+   * @return
+   */
+  @Override
+  public Population createRandomPopulation(int populationSize) {
 
-        Population p = new Population(populationSize);
+    Population p = new Population(populationSize);
 
-        for (int ind = 0; ind < p.size(); ind++) {
+    for (int ind = 0; ind < p.size(); ind++) {
 
-            Individual i = new Individual(16*4, 4);
-            for (int gene = 0; gene < i.getGeneSize(); gene++) {
+      Individual i = new Individual(16 * 4, 4);
+      for (int gene = 0; gene < i.getGeneSize(); gene++) {
 
-                i.setGene(gene, Math.max(1e-7*Math.pow(1e7, StaticRandom.raw()), 1e-8));
+        i.setGene(gene, Math.max(1e-7 * Math.pow(1e7, StaticRandom.raw()), 1e-8));
                 //i.setGene(gene,Math.max(StaticRandom.raw()*0.1,1e-8));
-                //System.out.println(Math.max(1e-6*Math.pow(1e6, StaticRandom.raw()), 1e-8));
-                // i.setGene(gene, Math.max(StaticRandom.raw()*0.1, 1e-8));
-                //i.setProbabilidad(Math.max(rand.raw()*0.1,1e-8), a);
-                
-            }
-            p.setIndividual(i, ind);
-        }
-        return p;
+        //System.out.println(Math.max(1e-6*Math.pow(1e6, StaticRandom.raw()), 1e-8));
+        // i.setGene(gene, Math.max(StaticRandom.raw()*0.1, 1e-8));
+        //i.setProbabilidad(Math.max(rand.raw()*0.1,1e-8), a);
+
+      }
+      p.setIndividual(i, ind);
     }
+    return p;
+  }
 }
