9a5ef4082a6602094ad120509f77accab599e2e8
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:25:16 +0200

    Formatting and Javadoc

--- src/geneticAlgorithm/geneticOperators/reinsertion/IReinsertion.java
+++ src/geneticAlgorithm/geneticOperators/reinsertion/IReinsertion.java
@@ -8,15 +8,13 @@ import geneticAlgorithm.Population;
 
 /**
  *
+ * Performs the reinsertion of one population into another. The populations are supposed to be
+ * ordered from best (less error) to worse (more error).
+ *
  * @author Nestor
- * 
- * performs the reinsertion of one population into another. 
- * The populations are supposed to be ordered from best (less error) to worse (more error).
- * 
  */
 public interface IReinsertion {
-    
-    
-    public Population Reinsert(Population origin, Population offpring,int substitutions);
-    
+
+  public Population Reinsert(Population origin, Population offpring, int substitutions);
+
 }
