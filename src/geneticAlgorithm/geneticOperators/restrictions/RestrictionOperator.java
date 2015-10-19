753a51a3dd86e701c04cd2a3b8ea75d856fea486
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:26:16 +0200

    Formatting

--- src/geneticAlgorithm/geneticOperators/restrictions/RestrictionOperator.java
+++ src/geneticAlgorithm/geneticOperators/restrictions/RestrictionOperator.java
@@ -15,40 +15,38 @@ import java.util.List;
  */
 public abstract class RestrictionOperator {
 
-    protected List<GeneRestriction> genesRestriction= new ArrayList();
+  protected List<GeneRestriction> genesRestriction = new ArrayList();
 
+  public abstract void initialize();
 
-    
-    public abstract void initialize();
+  public void apply(Population p) {
+    for (int ind = 0; ind < p.size(); ind++) {
 
-    public void apply(Population p) {
-        for (int ind = 0; ind < p.size(); ind++) {
+      Individual individual = p.getIndividual(ind);
 
-            Individual individual = p.getIndividual(ind);
-
-            for (int i = 0; i < genesRestriction.size(); i++) {
-                    genesRestriction.get(i).restrictGene(individual);
-            }
-        }
+      for (int i = 0; i < genesRestriction.size(); i++) {
+        genesRestriction.get(i).restrictGene(individual);
+      }
     }
+  }
 
-    public List getNonFixedGenes(int geneSize) {
-        List<Integer> result = new ArrayList();
+  public List getNonFixedGenes(int geneSize) {
+    List<Integer> result = new ArrayList();
 
-        for (int i = 0; i < geneSize; i++) {
-            boolean restricted = false;
-            for (int j = 0; j < genesRestriction.size(); j++) {
+    for (int i = 0; i < geneSize; i++) {
+      boolean restricted = false;
+      for (int j = 0; j < genesRestriction.size(); j++) {
 
-                if (genesRestriction.get(j).getGenePosition() == i && genesRestriction.get(j).getRestrictionType() > GeneRestriction.BOUNDED_VALUES) {
-                    restricted = true;
-                    break;
-                }
-            }
-            if (!restricted) {
-                result.add(i);
-            }
+        if (genesRestriction.get(j).getGenePosition() == i && genesRestriction.get(j).getRestrictionType() > GeneRestriction.BOUNDED_VALUES) {
+          restricted = true;
+          break;
         }
-        return result;
+      }
+      if (!restricted) {
+        result.add(i);
+      }
     }
+    return result;
+  }
 
 }
