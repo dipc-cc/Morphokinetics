d51a33d2aacff60286ffa0df78626bf63487ed15
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:24:26 +0200

    Formatting

--- src/geneticAlgorithm/geneticOperators/reinsertion/ElitistAllReinsertion.java
+++ src/geneticAlgorithm/geneticOperators/reinsertion/ElitistAllReinsertion.java
@@ -5,29 +5,27 @@ import geneticAlgorithm.Individual;
 import geneticAlgorithm.Population;
 
 public class ElitistAllReinsertion implements IReinsertion {
-	
-	private DcmaEsConfig config;
-	
-	public ElitistAllReinsertion(DcmaEsConfig config) {
-		this.config = config;
-	}
 
-	@Override
-	public Population Reinsert(Population origin, Population offpring,
-			int substitutions) {
-		
-		for (int k = 0; k < config.getOffSize(); k++) {
-			
-			Individual original = origin.getIndividual(k);
-			Individual candidate = offpring.getIndividual(k);
-			
-			if (candidate.getTotalError() <= original.getTotalError()) {
-				origin.setIndividual(candidate, k);
-				config.getOffFitness().set(k, candidate.getTotalError());
-			}
-		}
-		
-		return origin;
-	}
+  private DcmaEsConfig config;
+
+  public ElitistAllReinsertion(DcmaEsConfig config) {
+    this.config = config;
+  }
+
+  @Override
+  public Population Reinsert(Population origin, Population offpring, int substitutions) {
+    for (int k = 0; k < config.getOffSize(); k++) {
+
+      Individual original = origin.getIndividual(k);
+      Individual candidate = offpring.getIndividual(k);
+
+      if (candidate.getTotalError() <= original.getTotalError()) {
+        origin.setIndividual(candidate, k);
+        config.getOffFitness().set(k, candidate.getTotalError());
+      }
+    }
+
+    return origin;
+  }
 
 }
