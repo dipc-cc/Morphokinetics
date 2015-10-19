b4108e88a66c4f579ed1e24785f4103a0208eb84
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:28:45 +0200

    Formatting

--- src/geneticAlgorithm/geneticOperators/selection/RankingSelection.java
+++ src/geneticAlgorithm/geneticOperators/selection/RankingSelection.java
@@ -13,46 +13,47 @@ import geneticAlgorithm.Population;
  */
 public class RankingSelection implements ISelection {
 
-	private float SP = 2f; // selective pressure, choose in interval [1, 2]
+  private float SP = 2f; // selective pressure, choose in interval [1, 2]
 
-	public IndividualGroup[] Select(Population p, int groupCount) {
+  public IndividualGroup[] Select(Population p, int groupCount) {
 
-		float[] Fitness = new float[p.size()];
-		for (int i = 0; i < p.size(); i++) {
-			Fitness[p.size() - i - 1] = 2.0f - SP + 2.0f * (SP - 1.0f) * (i)
-					/ (p.size() - 1.0f);
-		}
+    float[] Fitness = new float[p.size()];
+    for (int i = 0; i < p.size(); i++) {
+      Fitness[p.size() - i - 1] = 2.0f - SP + 2.0f * (SP - 1.0f) * (i)
+              / (p.size() - 1.0f);
+    }
 
-		IndividualGroup[] groups = new IndividualGroup[groupCount];
+    IndividualGroup[] groups = new IndividualGroup[groupCount];
 
-		for (int i = 0; i < groupCount; i++) {
-			groups[i] = new IndividualGroup(2);
-			groups[i].set(0, p.getIndividual(linearSearch(Fitness)));
+    for (int i = 0; i < groupCount; i++) {
+      groups[i] = new IndividualGroup(2);
+      groups[i].set(0, p.getIndividual(linearSearch(Fitness)));
 
-			do {
-				groups[i].set(1, p.getIndividual(linearSearch(Fitness)));
-			} while (groups[i].get(0) == groups[i].get(1));
-		}
-		return groups;
-	}
+      do {
+        groups[i].set(1, p.getIndividual(linearSearch(Fitness)));
+      } while (groups[i].get(0) == groups[i].get(1));
+    }
+    return groups;
+  }
 
-	private int linearSearch(float[] probs) {
+  private int linearSearch(float[] probs) {
 
-		float total = 0;
-		for (int i = 0; i < probs.length; i++) {
-			total += probs[i];
-		}
+    float total = 0;
+    for (int i = 0; i < probs.length; i++) {
+      total += probs[i];
+    }
 
-		float selected = ((float) utils.StaticRandom.raw()) * total;
+    float selected = ((float) utils.StaticRandom.raw()) * total;
 
-		float acc = 0;
-		int i;
-		for (i = 0; i < probs.length; i++) {
-			acc += probs[i];
-			if (acc > selected)
-				return i;
-		}
-		return i;
-	}
+    float acc = 0;
+    int i;
+    for (i = 0; i < probs.length; i++) {
+      acc += probs[i];
+      if (acc > selected) {
+        return i;
+      }
+    }
+    return i;
+  }
 
 }
