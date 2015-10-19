9acf6920255245863b6ca20607734d1b21e1cb60
Joseba <joseba.alberdi@ehu.es>
2015-10-19 11:24:38 +0200

    Formatting

--- src/geneticAlgorithm/geneticOperators/reinsertion/ElitistReinsertion.java
+++ src/geneticAlgorithm/geneticOperators/reinsertion/ElitistReinsertion.java
@@ -12,18 +12,18 @@ import geneticAlgorithm.Population;
  */
 public class ElitistReinsertion implements IReinsertion {
 
-    public Population Reinsert(Population origin, Population offpring, int substitutions) {
+  public Population Reinsert(Population origin, Population offpring, int substitutions) {
 
-        //we order, just in case    
-        origin.order();
-        offpring.order();
+    //we order, just in case    
+    origin.order();
+    offpring.order();
 
-        for (int i = 0; i < substitutions; i++) {
-            origin.setIndividual(offpring.getIndividual(i), origin.size() - 1 - i);
-        }
-
-        //we reorder
-        origin.order();
-        return origin;
+    for (int i = 0; i < substitutions; i++) {
+      origin.setIndividual(offpring.getIndividual(i), origin.size() - 1 - i);
     }
+
+    //we reorder
+    origin.order();
+    return origin;
+  }
 }
