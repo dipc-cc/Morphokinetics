1e42d50bb5e58d430fa9ee951028ac15bcf6ae41
Joseba <joseba.alberdi@ehu.eus>
2015-10-20 10:55:32 +0200

    Naming convention

--- src/geneticAlgorithm/geneticOperators/evaluationFunctions/AgHierarchyEvaluator.java
+++ src/geneticAlgorithm/geneticOperators/evaluationFunctions/AgHierarchyEvaluator.java
@@ -28,25 +28,25 @@ public class AgHierarchyEvaluator extends AbstractEvaluator {
 
       Individual individual = p.getIndividual(i);
 
-      double step_A = Math.log10(individual.getGene(2 * 7 + 2));
-      double step_B = Math.log10(individual.getGene(5 * 7 + 5));
+      double stepA = Math.log10(individual.getGene(2 * 7 + 2));
+      double stepB = Math.log10(individual.getGene(5 * 7 + 5));
 
-      errors[i] += Math.max(0, 0.75 - (step_A - Math.log10(individual.getGene(2 * 7 + 3))));
-      errors[i] += Math.max(0, 0.75 - (step_A - Math.log10(individual.getGene(2 * 7 + 4))));
-      errors[i] += Math.max(0, 0.75 - (step_A - Math.log10(individual.getGene(2 * 7 + 5))));
-      errors[i] += Math.max(0, 0.75 - (step_A - Math.log10(individual.getGene(2 * 7 + 6))));
+      errors[i] += Math.max(0, 0.75 - (stepA - Math.log10(individual.getGene(2 * 7 + 3))));
+      errors[i] += Math.max(0, 0.75 - (stepA - Math.log10(individual.getGene(2 * 7 + 4))));
+      errors[i] += Math.max(0, 0.75 - (stepA - Math.log10(individual.getGene(2 * 7 + 5))));
+      errors[i] += Math.max(0, 0.75 - (stepA - Math.log10(individual.getGene(2 * 7 + 6))));
 
-      errors[i] += Math.max(0, 0.75 - (step_B - Math.log10(individual.getGene(5 * 7 + 2))));
-      errors[i] += Math.max(0, 0.75 - (step_B - Math.log10(individual.getGene(5 * 7 + 3))));
-      errors[i] += Math.max(0, 0.75 - (step_B - Math.log10(individual.getGene(5 * 7 + 4))));
-      errors[i] += Math.max(0, 0.75 - (step_B - Math.log10(individual.getGene(5 * 7 + 6))));
+      errors[i] += Math.max(0, 0.75 - (stepB - Math.log10(individual.getGene(5 * 7 + 2))));
+      errors[i] += Math.max(0, 0.75 - (stepB - Math.log10(individual.getGene(5 * 7 + 3))));
+      errors[i] += Math.max(0, 0.75 - (stepB - Math.log10(individual.getGene(5 * 7 + 4))));
+      errors[i] += Math.max(0, 0.75 - (stepB - Math.log10(individual.getGene(5 * 7 + 6))));
 
-      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 1)) - Math.max(step_A, step_B)));
-      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 2)) - Math.max(step_A, step_B)));
-      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 3)) - Math.max(step_A, step_B)));
-      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 4)) - Math.max(step_A, step_B)));
-      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 5)) - Math.max(step_A, step_B)));
-      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 6)) - Math.max(step_A, step_B)));
+      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 1)) - Math.max(stepA, stepB)));
+      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 2)) - Math.max(stepA, stepB)));
+      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 3)) - Math.max(stepA, stepB)));
+      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 4)) - Math.max(stepA, stepB)));
+      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 5)) - Math.max(stepA, stepB)));
+      errors[i] += Math.max(0, 0.75 - (Math.log10(individual.getGene(1 * 7 + 6)) - Math.max(stepA, stepB)));
 
     }
     return errors;
