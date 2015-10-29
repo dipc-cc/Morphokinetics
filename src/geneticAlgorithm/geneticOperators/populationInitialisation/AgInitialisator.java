d369e3654f9e8288bd15eda403c0f06df1f83a7e
Joseba <joseba.alberdi@ehu.eus>
2015-10-29 13:08:04 +0100

    Deleted commented lines and whitespaces

--- src/geneticAlgorithm/geneticOperators/populationInitialisation/AgInitialisator.java
+++ src/geneticAlgorithm/geneticOperators/populationInitialisation/AgInitialisator.java
@@ -23,11 +23,9 @@ public class AgInitialisator implements IInitialisator {
    */
   @Override
   public Population createRandomPopulation(int populationSize) {
-
     Population p = new Population(populationSize);
     for (int ind = 0; ind < p.size(); ind++) {
       Individual i = new Individual(49, 4);
-
       for (int a = 0; a < 7; a++) {
         for (int j = 0; j < 7; j++) {
           if (a == 1) {
@@ -37,17 +35,7 @@ public class AgInitialisator implements IInitialisator {
           }
         }
       }
-
       p.setIndividual(i, ind);
-
-      /*Individual i = new Individual(2, 4);
-        	
-       for (int a = 0; a < 2; a++) {
-       //i.setGene(a, Math.max(0.1, 1000 * Math.pow(150000, StaticRandom.raw())));
-       i.setGene(a, -500 + 1000 * StaticRandom.raw());
-       }
-        	
-       p.setIndividual(i, ind);*/
     }
     return p;
   }
