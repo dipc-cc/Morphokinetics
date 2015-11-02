afe72d9715ca03cfa917994798ffd55596e8ec10
Joseba <joseba.alberdi@ehu.eus>
2015-11-02 12:34:54 +0100

    Deleted commented lines and whitespaces

--- src/geneticAlgorithm/geneticOperators/restrictions/AgRestriction.java
+++ src/geneticAlgorithm/geneticOperators/restrictions/AgRestriction.java
@@ -54,61 +54,6 @@ public class AgRestriction extends RestrictionOperator {
 
   }
 
-  /*
-   @Override
-   public void apply(Population p) {
-        
-            
-   for (int ind=0;ind<p.size();ind++){
-   
-   Individual i=p.getIndividual(ind);
- 
-   //negative values are not valid
-   for (int a=0;a<i.getGeneSize();a++)   if (i.getGene(a)<0.0) i.setGene(a,0.0);
-
-   //Diffusion rate
-   i.setGene(0*7+0,diffusion_rate);
-   i.setGene(0*7+1,diffusion_rate);
-   i.setGene(0*7+2,diffusion_rate);
-   i.setGene(0*7+3,diffusion_rate);
-   i.setGene(0*7+4,diffusion_rate);
-   i.setGene(0*7+5,diffusion_rate);
-   i.setGene(0*7+6,diffusion_rate);     
-
-
-   //non-mobile dimers
-   i.setGene(1*7+0,0.0);
-
-
-   i.setGene(2*7+0,0.0);
-   i.setGene(2*7+1,0.0);
-
-   i.setGene(5*7+0,0.0);
-   i.setGene(5*7+1,0.0);
-
-   for (int j=0;j<7;j++){  i.setGene(3*7+j,0.0);
-   i.setGene(4*7+j,0.0);
-   i.setGene(6*7+j,0.0);}
-
-
-   //We set the following atomistic configurations to the same rate (according to the Ag/Ag diffuion paper):
-   //(2,3)=(2,4)=(2,5)=(2,6)=(5,2)=(5,3)=(5,4)=(5,6)
-
-   double value=i.getGene(2*7+3);
-
-   i.setGene(2*7+4,value);
-   i.setGene(2*7+5,value);
-   i.setGene(2*7+6,value);
-
-
-   i.setGene(5*7+2,value);
-   i.setGene(5*7+3,value);
-   i.setGene(5*7+4,value);
-   i.setGene(5*7+6,value);
-   }
-        
-   }
-   */
   @Override
   public void initialize() {
 
