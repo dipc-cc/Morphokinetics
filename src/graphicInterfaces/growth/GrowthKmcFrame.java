fe8444af0712a1af690b054198c66d07ce0c61a3
Joseba <joseba.alberdi@ehu.eus>
2015-11-04 14:20:02 +0100

    Deleted unneeded layout

--- src/graphicInterfaces/growth/GrowthKmcFrame.java
+++ src/graphicInterfaces/growth/GrowthKmcFrame.java
@@ -107,17 +107,6 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
       }
     });
 
-    javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-    jPanel1.setLayout(jPanel1Layout);
-    jPanel1Layout.setHorizontalGroup(
-      jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 0, Short.MAX_VALUE)
-    );
-    jPanel1Layout.setVerticalGroup(
-      jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addGap(0, 0, Short.MAX_VALUE)
-    );
-
     javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
     getContentPane().setLayout(layout);
     layout.setHorizontalGroup(
