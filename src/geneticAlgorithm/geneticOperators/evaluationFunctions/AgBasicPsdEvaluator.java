1f9b3e2adee80a8ff5b5ccb9ddf56321e0df3ef1
Joseba <joseba.alberdi@ehu.eus>
2015-11-06 12:24:04 +0100

    Removing interal frames. Removed the growth frame and PSD frame. Those are now printed to the main frame.

--- src/geneticAlgorithm/geneticOperators/evaluationFunctions/AgBasicPsdEvaluator.java
+++ src/geneticAlgorithm/geneticOperators/evaluationFunctions/AgBasicPsdEvaluator.java
@@ -6,11 +6,8 @@ package geneticAlgorithm.geneticOperators.evaluationFunctions;
 
 import basic.io.Restart;
 import geneticAlgorithm.Individual;
-import graphicInterfaces.growth.GrowthKmcFrame;
-import graphicInterfaces.growth.KmcCanvas;
 import graphicInterfaces.surfaceViewer2D.Frame2D;
 import kineticMonteCarlo.kmcCore.diffusion.AgKmc;
-import kineticMonteCarlo.lattice.AbstractGrowthLattice;
 import utils.psdAnalysis.PsdSignature2D;
 
 /**
@@ -32,14 +29,6 @@ public class AgBasicPsdEvaluator extends AbstractPsdEvaluator {
     this.kmc = kmc;
     psd = new PsdSignature2D(getPsdSizeY(), getPsdSizeX());
     difference = new float[getPsdSizeY()][getPsdSizeX()];
-
-    GrowthKmcFrame frame = new GrowthKmcFrame(new KmcCanvas((AbstractGrowthLattice) kmc.getLattice()));
-    frame.setVisible(true);
-    psdFrame =  new Frame2D("Calculated PSD analysis")
-            .setLogScale(true)
-            .setShift(true)
-            .setMin(2.02634)
-            .setMax(19.34551);
   }
 
   @Override
@@ -49,14 +38,14 @@ public class AgBasicPsdEvaluator extends AbstractPsdEvaluator {
     String folderName = "gaResults/population"+currentPopulation.getIterationNumber()+"/individual"+currentSimulation/repeats;
     int sizes[] = new int[2];
     Restart restart = new Restart(folderName);
-    psd.setRestart(restart);
+    psd.setRestart(restart);      
     kmc.initialiseRates(ind.getGenes());
     for (int i = 0; i < repeats; i++) {
       kmc.reset();
       kmc.depositSeed();
       while (true) {
         kmc.simulate(measureInterval);
-        sampledSurface = kmc.getSampledSurface(getPsdSizeY(), getPsdSizeX());
+        sampledSurface = kmc.getSampledSurface(getPsdSizeY(), getPsdSizeX());   
         psd.addSurfaceSample(sampledSurface);
         sizes[0] = sampledSurface.length;
         sizes[1] = sampledSurface[0].length;
@@ -68,11 +57,11 @@ public class AgBasicPsdEvaluator extends AbstractPsdEvaluator {
       }
       currentSimulation++;
     }
+ 
     ind.setSimulationTime(time / repeats);
     psd.printAvgToFile();
     psd.applySimmetryFold(PsdSignature2D.HORIZONTAL_SIMMETRY);
     psd.applySimmetryFold(PsdSignature2D.VERTICAL_SIMMETRY);
-    psdFrame.setMesh(psd.getPsd());
     return psd.getPsd();
   }
 
