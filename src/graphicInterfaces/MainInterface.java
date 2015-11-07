98337f8254f446af2ddd7801e81a505f6fa70b76
Joseba <joseba.alberdi@ehu.es>
2015-11-07 12:12:00 +0100

    Unused import

--- src/graphicInterfaces/MainInterface.java
+++ src/graphicInterfaces/MainInterface.java
@@ -22,7 +22,6 @@ import static java.awt.BorderLayout.EAST;
 import static java.awt.BorderLayout.NORTH;
 import static java.awt.BorderLayout.WEST;
 import java.awt.Container;
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
