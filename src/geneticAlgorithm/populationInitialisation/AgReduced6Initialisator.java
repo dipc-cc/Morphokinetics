638ce21422278387a077c0677493e909ab256741
Joseba <joseba.alberdi@ehu.eus>
2015-11-16 12:45:16 +0100

    Added new classes to work in the genetic algorithm with only 10 or 6 genes, instead of having 49 and restricting them. Not working yet

---
+++ src/geneticAlgorithm/populationInitialisation/AgReduced6Initialisator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geneticAlgorithm.populationInitialisation;
+
+import geneticAlgorithm.Individual;
+import geneticAlgorithm.Population;
+import utils.StaticRandom;
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez
+ */
+public class AgReduced6Initialisator implements IInitialisator{
+  
+  @Override
+  public Population createRandomPopulation(int populationSize) {
+    Population p = new Population(populationSize);
+    for (int ind = 0; ind < p.size(); ind++) {
+      System.out.println("Individual " + ind);
+      Individual i = new Individual(6, 4);
+      for (int j = 0; j < 6; j++) {
+        i.setGene(j, Math.max(0.1, 1000 * Math.pow(150000, StaticRandom.raw())));
+        //System.out.println("Individual"+ind+" random gene "+a+" "+j+" "+ i.getGene(a*7+j));
+      }
+      p.setIndividual(i, ind);
+    }
+    return p;
+  }
+}
---
+++ src/geneticAlgorithm/restrictions/AgReduced6Restriction.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geneticAlgorithm.restrictions;
+
+/**
+ * We have only 6 genes, which correspond to all different rates we can find in an Ag/Ag
+ * simulation.
+ *
+ * @author J. Alberdi-Rodriguez
+ */
+public class AgReduced6Restriction extends RestrictionOperator {
+
+  public void initialize(double temperature) {
+
+  }
+
+  public AgReduced6Restriction() {
+    //Negative and 0 values are not valid
+    for (int currentGene = 0; currentGene < 6; currentGene++) {
+      genesRestriction.add(new BoundedGeneRestriction(1e-8, 1e20, currentGene));
+    }
+
+    // For deposition rate a minimum value has to be defined
+    genesRestriction.add(new BoundedGeneRestriction(100, 1e20, 0));
+  }
+
+  @Override
+  public void initialize() {
+
+  }
+
+}
