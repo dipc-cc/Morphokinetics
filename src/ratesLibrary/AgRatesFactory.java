faab405d74ce30095b1bac5294071d414ea9685a
Joseba <joseba.alberdi@ehu.eus>
2015-11-17 12:07:18 +0100

    Added a method to only obtain 6 different rates

--- src/ratesLibrary/AgRatesFactory.java
+++ src/ratesLibrary/AgRatesFactory.java
@@ -29,6 +29,29 @@ public class AgRatesFactory implements IRatesFactory {
     return rates;
   }
 
+  /**
+   * This is tuned to work with only 6 "genes".
+   * Gene 0 from Ed   (0,j)
+   * Gene 1 from Ec   (1,1)(1,2)(1,6)
+   * Gene 2 from Ee   (1,3)(1,5)
+   * Gene 3 from Ef   (2,3)(5,2)(5,3)(5,4)(5,6)
+   * Gene 4 from Ea   (2,2)
+   * Gene 5 from Eb   (5,5)
+   * @param temperature
+   * @return 
+   */
+  public double[] getReduced6Rates(int temperature) {
+    double[] rates = new double[6];
+    rates[0] = experiments.getRate(0, 0, temperature);
+    rates[1] = experiments.getRate(1, 1, temperature);
+    rates[2] = experiments.getRate(1, 3, temperature);
+    rates[3] = experiments.getRate(2, 3, temperature);
+    rates[4] = experiments.getRate(2, 2, temperature);
+    rates[5] = experiments.getRate(5, 5, temperature);
+            
+    return rates;
+  }
+  
   @Override
   public double getDepositionRate(double temperature) {
     return experiments.getDepositionRate();
