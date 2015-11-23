e69cd79079a6f7ce2cf9e0fbe4557b383147d600
Joseba <joseba.alberdi@ehu.eus>
2015-11-23 12:47:29 +0100

    Formatting

--- src/utils/Wait.java
+++ src/utils/Wait.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package utils;
 
 /**
@@ -10,30 +9,29 @@ package utils;
  * @author Néstor
  */
 public class Wait {
-  public static void oneSec() {
-     try {
-       Thread.currentThread().sleep(1000);
-       }
-     catch (InterruptedException e) {
-       e.printStackTrace();
-       }
-     }
-  public static void manySec(long s) {
-     try {
-       Thread.currentThread().sleep(s * 1000);
-       }
-     catch (InterruptedException e) {
-       e.printStackTrace();
-       }
-     }
 
-    public static void manymsec(long s) {
-     try {
-       Thread.currentThread().sleep(s);
-       }
-     catch (InterruptedException e) {
-       e.printStackTrace();
-       }
-     }
+  public static void oneSec() {
+    try {
+      Thread.currentThread().sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void manySec(long s) {
+    try {
+      Thread.currentThread().sleep(s * 1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static void manymsec(long s) {
+    try {
+      Thread.currentThread().sleep(s);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
 
 }
