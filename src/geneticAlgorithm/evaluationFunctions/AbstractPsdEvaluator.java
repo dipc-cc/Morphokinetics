a4507dd2357b7becbbdde821ac80763754eeaf05
Joseba <joseba.alberdi@ehu.eus>
2015-11-24 12:16:10 +0100

    Printing also the hierarchy error

--- src/geneticAlgorithm/evaluationFunctions/AbstractPsdEvaluator.java
+++ src/geneticAlgorithm/evaluationFunctions/AbstractPsdEvaluator.java
@@ -149,10 +149,10 @@ public abstract class AbstractPsdEvaluator extends AbstractEvaluator {
     if (kmcError == -1) error = 1000; // If the KMC execution did not finish properly, set huge error
 
     // Print to standard output, file and GUI
-    System.out.println("  errors: "+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+error);
+    System.out.println("  errors: "+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+calculateHierarchyError(ind)+"\t"+error);
     System.out.println("  simul. time: "+ind.getSimulationTime()+"\t("+expectedSimulationTime+")");
-    String errors = " Errors: FrobeniusNormMatrix\ttimeError\tError\n";
-    errors = errors+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+error;
+    String errors = " Errors: FrobeniusNormMatrix\ttimeError\tHierarchy\tError\n";
+    errors = errors+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+calculateHierarchyError(ind)+"\t"+error;
     String folderName = "gaResults/population"+currentPopulation.getIterationNumber()+"/individual"+((currentSimulation-repeats)/repeats);
     Restart restart = new Restart(folderName);
     String fileName = "errors.txt";
