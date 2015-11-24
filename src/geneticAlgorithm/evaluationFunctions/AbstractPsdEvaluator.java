80fd35a49ab20424b2c38fb06bfa4c16fb510974
Joseba <joseba.alberdi@ehu.eus>
2015-11-24 15:11:41 +0100

    Using hierarchy evaluator (with reference)

--- src/geneticAlgorithm/evaluationFunctions/AbstractPsdEvaluator.java
+++ src/geneticAlgorithm/evaluationFunctions/AbstractPsdEvaluator.java
@@ -88,6 +88,8 @@ public abstract class AbstractPsdEvaluator extends AbstractEvaluator {
   
   protected abstract double calculateHierarchyError(Individual i);
   
+  protected abstract double calculateHierarchyErrorFromReference(Individual i);
+  
   protected double calculateFrobeniusNormErrorMatrix(PsdSignature2D psd) {
     double error;
     double sum = 0.0f;
@@ -145,14 +147,14 @@ public abstract class AbstractPsdEvaluator extends AbstractEvaluator {
     double error = calculateFrobeniusNormErrorMatrix(psd); // Calculate corresponding error with the reference
     double timeError = Math.pow(Math.log(ind.getSimulationTime()) - expectedSimulationTime, 2) / expectedSimulationTime; // Calculate simulated time error with the reference
     error += timeError; // update error with the Frobenius error of the log of the time
-    error += calculateHierarchyError(ind);
+    error += calculateHierarchyErrorFromReference(ind);
     if (kmcError == -1) error = 1000; // If the KMC execution did not finish properly, set huge error
 
     // Print to standard output, file and GUI
-    System.out.println("  errors: "+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+calculateHierarchyError(ind)+"\t"+error);
+    System.out.println("  errors: "+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+calculateHierarchyErrorFromReference(ind)+"\t"+error);
     System.out.println("  simul. time: "+ind.getSimulationTime()+"\t("+expectedSimulationTime+")");
     String errors = " Errors: FrobeniusNormMatrix\ttimeError\tHierarchy\tError\n";
-    errors = errors+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+calculateHierarchyError(ind)+"\t"+error;
+    errors = errors+"\t"+calculateFrobeniusNormErrorMatrix(psd)+"\t"+timeError+"\t"+calculateHierarchyErrorFromReference(ind)+"\t"+error;
     String folderName = "gaResults/population"+currentPopulation.getIterationNumber()+"/individual"+((currentSimulation-repeats)/repeats);
     Restart restart = new Restart(folderName);
     String fileName = "errors.txt";
