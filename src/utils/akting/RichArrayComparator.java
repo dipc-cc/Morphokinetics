85765944b73bacfddb04b52d4b90919e6e3771a3
Joseba <joseba.alberdi@ehu.eus>
2015-11-26 16:06:27 +0100

    Proper indentation

--- src/utils/akting/RichArrayComparator.java
+++ src/utils/akting/RichArrayComparator.java
@@ -3,26 +3,26 @@ package utils.akting;
 import java.util.Comparator;
 
 public class RichArrayComparator implements Comparator<Integer> {
-	
-	private final RichArray richArray;
-	
-	public RichArrayComparator(RichArray richArray) {
-		this.richArray = richArray;
-	}
-	
-	public Integer[] createIndexArray() {
-		int size = richArray.size();
-		Integer[] indexes = new Integer[size];
-		
-		for (int i = 0; i < size; i++) {
-			indexes[i] = i;
-		}
-		
-		return indexes;
-	}
 
-	public int compare(Integer o1, Integer o2) {
-		return richArray.get(o1).compareTo(richArray.get(o2));
-	}
+  private final RichArray richArray;
+
+  public RichArrayComparator(RichArray richArray) {
+    this.richArray = richArray;
+  }
+
+  public Integer[] createIndexArray() {
+    int size = richArray.size();
+    Integer[] indexes = new Integer[size];
+
+    for (int i = 0; i < size; i++) {
+      indexes[i] = i;
+    }
+
+    return indexes;
+  }
+
+  public int compare(Integer o1, Integer o2) {
+    return richArray.get(o1).compareTo(richArray.get(o2));
+  }
 
 }
