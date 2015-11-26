4143358de0e75ae579bee40f74a8198bd318c237
Joseba <joseba.alberdi@ehu.eus>
2015-11-26 16:11:11 +0100

    Proper indentation

--- src/utils/akting/operations/DivideOperation.java
+++ src/utils/akting/operations/DivideOperation.java
@@ -1,15 +1,15 @@
 package utils.akting.operations;
 
 public class DivideOperation implements Operation {
-	
-	private final double factor;
-	
-	public DivideOperation(double factor) {
-		this.factor = factor;
-	}
 
-	public double apply(double value) {
-		return value / factor;
-	}
+  private final double factor;
+
+  public DivideOperation(double factor) {
+    this.factor = factor;
+  }
+
+  public double apply(double value) {
+    return value / factor;
+  }
 
 }
