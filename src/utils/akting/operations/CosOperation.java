ced98c4028825bc5aafde6e85935c7d8d20a80b7
Joseba <joseba.alberdi@ehu.eus>
2015-11-26 16:10:49 +0100

    Proper indentation

--- src/utils/akting/operations/CosOperation.java
+++ src/utils/akting/operations/CosOperation.java
@@ -2,8 +2,8 @@ package utils.akting.operations;
 
 public class CosOperation implements Operation {
 
-	public double apply(double value) {
-		return Math.cos(value);
-	}
+  public double apply(double value) {
+    return Math.cos(value);
+  }
 
 }
