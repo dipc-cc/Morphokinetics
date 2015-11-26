045cf2f63a39c32ba23f5b1fec21e76305ce2b61
Joseba <joseba.alberdi@ehu.eus>
2015-11-26 16:12:47 +0100

    Added override

--- src/utils/akting/operations/SqrtOperation.java
+++ src/utils/akting/operations/SqrtOperation.java
@@ -5,7 +5,10 @@ public class SqrtOperation implements Operation {
   /**
    * In the environment of the project we assume that we won't need to calculate negative sqrt.
    * Hence we take the absolute value.
+   * @param value
+   * @return 
    */
+  @Override
   public double apply(double value) {
     return Math.sqrt(Math.abs(value));
   }
