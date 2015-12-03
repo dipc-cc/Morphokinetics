70741ac2dbf544786a1c2ae9ce88f5e3a335265c
Joseba <joseba.alberdi@ehu.eus>
2015-12-03 17:11:43 +0100

    Added overloaded constructor to be able to choose the range of the search from outside

--- src/geneticAlgorithm/restrictions/AgReduced6Restriction.java
+++ src/geneticAlgorithm/restrictions/AgReduced6Restriction.java
@@ -23,7 +23,29 @@ public class AgReduced6Restriction extends RestrictionOperator {
       genesRestriction.add(new BoundedGeneRestriction(1e-8, 1e20, currentGene));
     }
   }
+  
+  /**
+   *
+   * @param dimensions number of genes (or variables)
+   * @param min the minimum value
+   * @param max the maximum value
+   */
+  public AgReduced6Restriction(int dimensions, double min, double max) {
+    //Negative and 0 values are not valid
+    if (min > max) {
+      System.out.println("Minimum has to be less than maximum");
+      throw new IllegalArgumentException("Minimum has to be less than maximum");
+    }
+    if (min <= 0) {
+      System.out.println("Minimum has to be greater than 0");
+      throw new IllegalArgumentException("Minimum has to be greater than 0");
+    }
 
+    for (int currentGene = 0; currentGene < dimensions; currentGene++) {
+      genesRestriction.add(new BoundedGeneRestriction(min, max, currentGene));
+    }
+  }
+  
   @Override
   public void initialize() {
 
