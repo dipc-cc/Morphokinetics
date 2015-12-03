27f44275790b062a458a6d9325b1872394402b0e
Joseba <joseba.alberdi@ehu.eus>
2015-12-03 16:47:49 +0100

    Using the general initialisator instead of previous one.

--- src/geneticAlgorithm/populationInitialisation/SiInitialisator.java
+++ src/geneticAlgorithm/populationInitialisation/SiInitialisator.java
@@ -4,15 +4,13 @@
  */
 package geneticAlgorithm.populationInitialisation;
 
-import geneticAlgorithm.Individual;
 import geneticAlgorithm.Population;
-import utils.StaticRandom;
 
 /**
  *
  * @author Nestor
  */
-public class SiInitialisator implements IInitialisator {
+public class SiInitialisator extends GeneralInitialisator implements IInitialisator {
 
   /**
    * Simplest way of initialisation, a pure random value. AgAg initialisation has a more robust
@@ -23,14 +21,6 @@ public class SiInitialisator implements IInitialisator {
    */
   @Override
   public Population createRandomPopulation(int populationSize) {
-    Population p = new Population(populationSize);
-    for (int ind = 0; ind < p.size(); ind++) {
-      Individual i = new Individual(16 * 4, 4);
-      for (int gene = 0; gene < i.getGeneSize(); gene++) {
-        i.setGene(gene, Math.max(1e-7 * Math.pow(1e7, StaticRandom.raw()), 1e-8));
-      }
-      p.setIndividual(i, ind);
-    }
-    return p;
+    return createRandomPopulation(populationSize, 16*4, 1e-8, 1e7,true);
   }
 }
