d9d08dc4586312c32ff74515ffd41f234b34d586
Joseba <joseba.alberdi@ehu.eus>
2015-12-03 17:10:18 +0100

    Reverting the restrictions to a large range

--- src/geneticAlgorithm/restrictions/AgReduced6Restriction.java
+++ src/geneticAlgorithm/restrictions/AgReduced6Restriction.java
@@ -22,13 +22,6 @@ public class AgReduced6Restriction extends RestrictionOperator {
     for (int currentGene = 0; currentGene < 6; currentGene++) {
       genesRestriction.add(new BoundedGeneRestriction(1e-8, 1e20, currentGene));
     }
-    
-    genesRestriction.add(new BoundedGeneRestriction(1e6, 1e10, 0));
-    genesRestriction.add(new BoundedGeneRestriction(1e9, 1e13, 1));
-    genesRestriction.add(new BoundedGeneRestriction(1e6, 1e10, 2));
-    genesRestriction.add(new BoundedGeneRestriction(1e0, 1e4, 3));
-    genesRestriction.add(new BoundedGeneRestriction(1e1, 1e5, 4));
-    genesRestriction.add(new BoundedGeneRestriction(1e0, 1e4, 5));
   }
 
   @Override
