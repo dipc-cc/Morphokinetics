4d3a9ae2cb16d5b3923126ecdbd38ef6e32dc9ba
Joseba <joseba.alberdi@ehu.eus>
2015-12-04 17:10:10 +0100

    Added Javadoc description and formatting

--- src/geneticAlgorithm/evaluationFunctions/EvaluatorType.java
+++ src/geneticAlgorithm/evaluationFunctions/EvaluatorType.java
@@ -9,11 +9,15 @@ import java.util.EnumSet;
 import java.util.Set;
 
 /**
+ * Evaluator type enumerator. Based on this class we can choose between different evaluators. Main
+ * idea taken from:
+ * http://stackoverflow.com/questions/5346477/implementing-a-bitfield-using-java-enums and
+ * http://eddmann.com/posts/using-bit-flags-and-enumsets-in-java/
  *
  * @author J. Alberdi-Rodriguez
  */
 public class EvaluatorType {
-  
+
   public enum evaluatorFlag {
     PSD(1 << 0), TIME(1 << 1), HIERARCHY(1 << 2);
 
@@ -28,7 +32,7 @@ public class EvaluatorType {
       return statusFlagValue;
     }
   }
-  
+
   /**
    * Translates a numeric status code into a Set of StatusFlag enums
    *
@@ -37,7 +41,7 @@ public class EvaluatorType {
    */
   public EnumSet<evaluatorFlag> getStatusFlags(long statusValue) {
     EnumSet statusFlags = EnumSet.noneOf(evaluatorFlag.class);
-        
+
     // Iterate all possible flag values and add them if the power of two is equal
     for (evaluatorFlag myFlag : evaluatorFlag.values()) {
       if ((myFlag.getStatusFlagValue() & statusValue) == myFlag.getStatusFlagValue()) {
@@ -47,25 +51,25 @@ public class EvaluatorType {
 
     return statusFlags;
   }
+
   /**
    * Translates a set of StatusFlag enums into a numeric status code
+   *
    * @param flags if statusFlags
-   * @return numeric representation of the document status 
+   * @return numeric representation of the document status
    */
-  public long getStatusValue(Set<evaluatorFlag> flags)
-  {
-      long value=0;
-      for (evaluatorFlag myFlag : flags) {
-        value |= myFlag.getStatusFlagValue();
-      }
-      return value;
+  public long getStatusValue(Set<evaluatorFlag> flags) {
+    long value = 0;
+    for (evaluatorFlag myFlag : flags) {
+      value |= myFlag.getStatusFlagValue();
+    }
+    return value;
   }
-  
-  
+
   public enum hierarchyFlag {
     GENERAL, STEP, FROM_REFERENCE_FROBENIUS, FROM_REFERENCE;
 
     public static final EnumSet<hierarchyFlag> ALL_OPTS = EnumSet.allOf(hierarchyFlag.class);
   }
-  
+
 }
