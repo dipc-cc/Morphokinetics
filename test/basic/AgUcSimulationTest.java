ae59f7754afafa10ebbffbd32e1c95aac046c4f7
Joseba <joseba.alberdi@ehu.eus>
2016-03-17 14:56:19 +0100

    Added a test for Ag run with unit celss (AgUc). It uses the same reference as the Ag without unit cell and the error are inside the permitted tolerance.

---
+++ test/basic/AgUcSimulationTest.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basic;
+
+import basic.io.Restart;
+import geneticAlgorithm.evaluationFunctions.AgBasicPsdEvaluator;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import kineticMonteCarlo.kmcCore.growth.RoundPerimeter;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez
+ */
+public class AgUcSimulationTest {
+  
+  private float[][] currentSurface;
+  private float[][] currentPsd;
+
+  public AgUcSimulationTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+  
+  @Test
+  public void testAgUcPsd() {
+    AbstractSimulation.printHeader("AgUc PSD test");
+    Parser parser = new Parser();
+    parser.readFile(TestHelper.getBaseDir() + "/test/input/AgUcPsdParameters");
+    parser.print();
+    
+    doAgTest(parser);
+    //TODO check that PSDs are equivalent
+    Restart restart = new Restart(TestHelper.getBaseDir() + "/test/references/");
+    int[] sizes = {parser.getCartSizeX() / 2, parser.getCartSizeY() / 2};
+    float[][] ref = null;
+    try {
+      ref = restart.readPsdText2D(2, sizes, "AgPsdAvgRaw.txt");
+    } catch (FileNotFoundException ex) {
+      Logger.getLogger(AgSimulationTest.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    AgBasicPsdEvaluator evaluator = new AgBasicPsdEvaluator(null,
+            0, 0,
+            parser.getCartSizeX() / 2, parser.getCartSizeY() / 2,
+            null, "Frobenius", null, parser.getTemperature());
+    evaluator.setPsd(ref);
+    double FrobeniusError = evaluator.calculateFrobeniusNormErrorMatrix(currentPsd);
+    System.out.println("Frobenius error is " + FrobeniusError);
+    List<Double> results = new ArrayList();
+    results.add(FrobeniusError);
+    //0.031!!!
+    results.add(0.032); // the error must be lower than 0.032
+    results.sort((a, b) -> b.compareTo(a));
+    assertEquals(0.032, results.get(0), 0.0); // ensure that the first value is 0.032, and therefore, the current error is lower
+ 
+  }
+
+  private void doAgTest(Parser parser) {
+    AbstractSimulation simulation = new AgUcSimulation(parser);
+
+    simulation.initialiseKmc();
+    simulation.createFrame();
+    simulation.doSimulation();
+    simulation.finishSimulation();
+
+    currentSurface = simulation.getKmc().getSampledSurface(parser.getCartSizeX() / 2, parser.getCartSizeY() / 2);
+    currentPsd = simulation.getPsd().getPsd();
+  }
+   
+}
