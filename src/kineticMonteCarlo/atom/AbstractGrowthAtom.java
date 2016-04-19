1ce5a927fb51735e080f0a7fdeb9f3a41b6deeb2
Joseba <joseba.alberdi@ehu.eus>
2016-04-19 11:26:56 +0200

    The hash have to be based in a unique value; this is not anymore true with the old code, because hash was based on i,j coordinates. Now, hash is based on the Id, which has to be given in the constructor and have to be unique for each atom.

--- src/kineticMonteCarlo/atom/AbstractGrowthAtom.java
+++ src/kineticMonteCarlo/atom/AbstractGrowthAtom.java
@@ -76,13 +76,14 @@ public abstract class AbstractGrowthAtom extends AbstractAtom {
     setNumberOfNeighbours(numberOfNeighbours);
   }
   
+  /**
+   * Every atom will have an unique Id. So, we can use it as hash.
+   *
+   * @return hash value, based on Id
+   */
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + iHexa;
-    result = prime * result + jHexa;
-    return result;
+    return id;
   }
 
   /**
