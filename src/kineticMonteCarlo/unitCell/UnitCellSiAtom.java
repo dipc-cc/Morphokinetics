1947f010a2e9e1c7a0a1009c30d6e84c5cb7eaa8
Joseba <joseba.alberdi@ehu.eus>
2016-06-15 10:50:32 +0200

    Simplifying code: removed unused attributes and methods.

--- src/kineticMonteCarlo/unitCell/UnitCellSiAtom.java
+++ src/kineticMonteCarlo/unitCell/UnitCellSiAtom.java
@@ -12,7 +12,6 @@ public class UnitCellSiAtom {
   private float posX_space, posY_space, posZ_space;
   private static float limitX, limitY, limitZ;
   private static float desp_x_y, desp_x_z, desp_y_z;
-  private static float offset_X, offset_Y;
 
   public static float getLimitX() {
     return limitX;
@@ -24,26 +23,6 @@ public class UnitCellSiAtom {
 
   public static float getLimitZ() {
     return limitZ;
-  }
-
-  public static float getOffsetX() {
-    return offset_X;
-  }
-
-  public static float getOffsetY() {
-    return offset_Y;
-  }
-
-  public static float getDesp_x_y() {
-    return desp_x_y;
-  }
-
-  public static float getDesp_x_z() {
-    return desp_x_z;
-  }
-
-  public static float getDesp_y_z() {
-    return desp_y_z;
   }
 
   //Inicializa las variables de posicionamiento
@@ -60,7 +39,6 @@ public class UnitCellSiAtom {
     posX_space = (float) posX;
     posY_space = (float) posY;
     posZ_space = (float) posZ;
-
   }
 
   public float getPosX(int uc_x, int uc_y, int uc_z) {
@@ -75,16 +53,6 @@ public class UnitCellSiAtom {
 
   public float getPosZ(int uc_x, int uc_y, int uc_z) {
     return posZ_space - uc_z * limitZ;
-  }
-
-  public float getPosXPure(int uc_x, int uc_y, int uc_z) {
-    float temp = posX_space + uc_x * limitX + desp_x_y * uc_y - uc_z * desp_x_z;
-    return temp;
-  }
-
-  public float getPosYPure(int uc_x, int uc_y, int uc_z) {
-    float temp = posY_space + uc_y * limitY - desp_y_z * uc_z;
-    return temp;
   }
     
   /**
