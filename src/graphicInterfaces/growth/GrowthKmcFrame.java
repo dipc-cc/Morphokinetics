bd62abce027df1fa479e9a713a4b1ab4585e11b4
Joseba <joseba.alberdi@ehu.eus>
2016-06-20 17:24:28 +0200

    Variable name changed.

--- src/graphicInterfaces/growth/GrowthKmcFrame.java
+++ src/graphicInterfaces/growth/GrowthKmcFrame.java
@@ -31,7 +31,7 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
     canvas1.setSize(canvas1.getSizeX(), canvas1.getSizeY());
     jPanel1.add(canvas1);
     canvas1.initialise();
-    jSpinner2.setValue(((KmcCanvas) canvas1).getScale());
+    jSpinnerScale.setValue(((KmcCanvas) canvas1).getScale());
     setResizable(true);
     setSize(canvas1.getSizeX() + 50, canvas1.getSizeY() + 100);
 
@@ -83,8 +83,8 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
   // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
   private void initComponents() {
 
-    jLabel2 = new javax.swing.JLabel();
-    jSpinner2 = new javax.swing.JSpinner();
+    jLabelScale = new javax.swing.JLabel();
+    jSpinnerScale = new javax.swing.JSpinner();
     jPanel1 = new javax.swing.JPanel();
 
     JScrollPane scrollPane = new javax.swing.JScrollPane(jPanel1);
@@ -92,14 +92,14 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
     setTitle("Morphoniketics");
     setResizable(false);
 
-    jLabel2.setText("Scale");
+    jLabelScale.setText("Scale");
 
-    jSpinner2.setModel(new javax.swing.SpinnerNumberModel(Integer.valueOf(1), Integer.valueOf(1), null, Integer.valueOf(1)));
-    jSpinner2.setFocusCycleRoot(true);
-    jSpinner2.setFocusable(false);
-    jSpinner2.addChangeListener(new javax.swing.event.ChangeListener() {
+    jSpinnerScale.setModel(new javax.swing.SpinnerNumberModel(Integer.valueOf(1), Integer.valueOf(1), null, Integer.valueOf(1)));
+    jSpinnerScale.setFocusCycleRoot(true);
+    jSpinnerScale.setFocusable(false);
+    jSpinnerScale.addChangeListener(new javax.swing.event.ChangeListener() {
       public void stateChanged(javax.swing.event.ChangeEvent evt) {
-        jSpinner2StateChanged(evt);
+        jSpinnerScaleStateChanged(evt);
       }
     });
 
@@ -131,9 +131,9 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
           .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
           .addGroup(layout.createSequentialGroup()
-            .addComponent(jLabel2)
+            .addComponent(jLabelScale)
             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-            .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jSpinnerScale, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addGap(0, 0, Short.MAX_VALUE)))
         .addContainerGap())
     );
@@ -141,8 +141,8 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
       layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-          .addComponent(jLabel2)
-          .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+          .addComponent(jLabelScale)
+          .addComponent(jSpinnerScale, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
         .addComponent(scrollPane, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         .addContainerGap())
@@ -151,12 +151,12 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
     pack();
   }// </editor-fold>//GEN-END:initComponents
 
-    private void jSpinner2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner2StateChanged
+    private void jSpinnerScaleStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinnerScaleStateChanged
 
-      canvas1.setScale((Integer) jSpinner2.getValue());
+      canvas1.setScale((Integer) jSpinnerScale.getValue());
       canvas1.setSize(canvas1.getSizeX(), canvas1.getSizeY());
       setSize(canvas1.getSizeX() + 50, canvas1.getSizeY() + 100);
-    }//GEN-LAST:event_jSpinner2StateChanged
+    }//GEN-LAST:event_jSpinnerScaleStateChanged
 
     private void jPanel1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MousePressed
       startMouseX = evt.getX();
@@ -175,7 +175,7 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
 
   private void jPanel1MouseWheelMoved(java.awt.event.MouseWheelEvent evt) {//GEN-FIRST:event_jPanel1MouseWheelMoved
 
-    int zoom = (Integer) jSpinner2.getValue();
+    int zoom = (Integer) jSpinnerScale.getValue();
     if ((Integer) evt.getWheelRotation() == -1) {
       zoom *=2;
     } else {
@@ -187,15 +187,15 @@ public class GrowthKmcFrame extends javax.swing.JFrame {
     if (zoom >= 32) {
       zoom = 32;
     }
-    jSpinner2.setValue(zoom);
+    jSpinnerScale.setValue(zoom);
     canvas1.setScale(zoom);
     canvas1.setSize(canvas1.getSizeX(), canvas1.getSizeY());
     setSize(canvas1.getSizeX() + 50, canvas1.getSizeY() + 100);
   }//GEN-LAST:event_jPanel1MouseWheelMoved
 
   // Variables declaration - do not modify//GEN-BEGIN:variables
-  private javax.swing.JLabel jLabel2;
+  private javax.swing.JLabel jLabelScale;
   private javax.swing.JPanel jPanel1;
-  private javax.swing.JSpinner jSpinner2;
+  private javax.swing.JSpinner jSpinnerScale;
   // End of variables declaration//GEN-END:variables
 }
