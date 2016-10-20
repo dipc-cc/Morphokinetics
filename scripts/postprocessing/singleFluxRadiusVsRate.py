b03019e1900eb035292b33f768965f94cdbbdbe7
Joseba <joseba.alberdi@ehu.eus>
2016-10-20 16:32:14 +0200

    Adapted to the new interface

--- scripts/postprocessing/singleFluxRadiusVsRate.py
+++ scripts/postprocessing/singleFluxRadiusVsRate.py
@@ -11,12 +11,12 @@ import morphokinetics as mk
 
 def radiusVsRate(folder="."):
     
+    temperatures = list(range(120,221,5))
     plt.title("Average island growth")
     label = r'Average island radius growth rate  $ \sqrt{{ \dot{{r}} }}$'
     plt.ylabel(label)
     label = r'Time-averaged total rate $< R >_t }}$'
     plt.xlabel(label)
-    plt.legend(loc='upper left', prop={'size':6})
     plt.grid(True)
 
     results = []
@@ -26,20 +26,17 @@ def radiusVsRate(folder="."):
     print(folder)
     try:
         os.chdir(folder)
-        oneResult = mk.getIslandDistribution(sqrt, interval)
+        oneResult = mk.getIslandDistribution(temperatures, sqrt, interval)
         results.append(oneResult)
     except OSError:
         print ("error changing to {}".format(folder))
         a = 0 #do nothing
-    print(results)
     plt.loglog(np.array(results[-1][1]), np.array(results[-1][0]),  label="growth"+folder)
     plt.loglog(np.array(results[-1][1]), np.array(results[-1][2]), label="gyradius")
     plt.loglog(np.array(results[-1][1]), 1000/np.array(results[-1][4]), label="perimeter")
     plt.legend(loc='upper left', prop={'size':6})
 
 
-if __name__ == '__main__':
-    print("Why am I here?")
 workingPath = os.getcwd()
 flux = 3.5e-3
 radiusVsRate()
