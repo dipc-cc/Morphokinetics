2a8c530f042555b21a5eca8b9661e3e1a4ae1753
Joseba <joseba.alberdi@ehu.eus>
2016-10-20 15:17:01 +0200

    Number of islands is proportional to the growth of islands. Gyradius is somehow similar to them.

--- scripts/postprocessing/radiusVsRateGlobalAe.py
+++ scripts/postprocessing/radiusVsRateGlobalAe.py
@@ -21,10 +21,8 @@ ax1.grid(True)
 
 workingPath = os.getcwd()
 results = []
-temperatures = list(range(120,221,5))
-Rtt = ([16909147819.3565 ,21827140898.7456 ,27627597919.6124 ,34364354949.7052 ,42082771314.0787 ,50819661607.7332 ,60603453969.7332 ,71454522157.9662 ,83385644639.2549 ,96402550521.0296 ,110504518908.249 ,125685004681.771 ,141932269487.491 ,159230001774.093 ,177557913993.664 ,196892308616.069 ,217206607470.615 ,238471841198.484 ,260657097361.911 ,283729927093.541 ,307656711157.28])
+temperatures = list(range(120,321,5))
 
-Rstep = ([85941.5666677768, 185709.051983031, 378203.30334162, 730700.474287996, 1346871.73342121, 2380119.6620485, 4049377.66119581, 6657170.49755918, 10609590.0115993, 16437717.6779536, 24819933.7510902, 36604495.7633867, 52831748.04825, 74755336.5257832, 103861844.747233, 141888332.294831, 190837338.622147, 252989007.881088, 330910087.306819, 427459648.223626, 545791470.564676])
 kb = 8.6173324e-5
 for i in range(-3,-2):
     folder = "flux3.5e"+str(i)
@@ -37,15 +35,25 @@ for i in range(-3,-2):
         print ("error changing to {}".format(folder))
         a = 0 #do nothing
     os.chdir(workingPath)
+    while(len(temperatures) > len(results[-1][0])):
+        results[-1][0].append(np.nan)
+        results[-1][1].append(np.nan)
+        results[-1][2].append(np.nan)
+        results[-1][3].append(np.nan)
+        
     axes = plt.gca()
-    lng3 = ax2.semilogy(1/(kb*np.array(temperatures)), np.array(results[-1][3]),  "-r", label="islands"+folder)
-    lng1 = ax1.semilogy(1/(kb*np.array(temperatures)), 1e5*np.array(results[-1][0]),  "x-", label="growth "+folder)
+    v = 0.7*flux*400*400/(np.array(results[-1][3]))#+np.array(results[-1][5]))
+    v0 = np.array(results[-1][0])
+    lng3 = ax1.semilogy(1/(kb*np.array(temperatures)), v,  "-r", label="islands "+folder)
+    lng1 = ax1.semilogy(1/(kb*np.array(temperatures)), v0,  "x-", label="growth "+folder)
     lng2 = ax1.semilogy(1/(kb*np.array(temperatures)), np.array(results[-1][1]),  "s-", label="ratio "+folder)
-    lng = lng1 + lng2 + lng3
+    lng4 = ax1.semilogy(1/(kb*np.array(temperatures)), 1e2*np.array(results[-1][2]), '--', label="gyradius "+folder)
+    lng5 = ax2.plot(1/(kb*np.array(temperatures)), np.array(v0/v), 'g-', label="islands*growth "+folder)
+    lng = lng1 + lng2 + lng3 + lng4 + lng5
     labs = [l.get_label() for l in lng]
     plt.legend(lng, labs, loc='upper right', prop={'size':6})
     
-    plt.savefig("radiusVsRate.png")
+    plt.savefig("radiusVsRateGlobalAe.png")
     
 plt.close()
 
