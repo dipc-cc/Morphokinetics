ce667d493b93a63e73cab95b67bc6ec5b8fa41a2
Joseba <joseba.alberdi@ehu.eus>
2016-11-08 17:07:44 +0100

    Code cleaning

--- src/main/MainActivity.java
+++ src/main/MainActivity.java
@@ -5,10 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,10 +16,6 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Enumeration;
 import java.util.Properties;
 
@@ -36,263 +29,176 @@ import basic.SiSimulation;
 import eus.ehu.dipc.morphokinetics.R;
 import kineticMonteCarlo.atom.AbstractAtom;
 import kineticMonteCarlo.atom.AbstractGrowthAtom;
-import kineticMonteCarlo.atom.BasicAtom;
 import kineticMonteCarlo.lattice.AbstractGrowthLattice;
 import kineticMonteCarlo.unitCell.AbstractGrowthUc;
-import kineticMonteCarlo.unitCell.IUc;
-
-import static android.R.color.darker_gray;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "ebookfrenzy.StateChange";
+  private int count = 0;
+  private AbstractSimulation simulation = null;
+  private Parser parser;
 
-    private int count = 0;
-    private AbstractSimulation simulation = null;
-    private Parser parser;
-    Paint mTextPaint;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    //private GoogleApiClient client;
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    try {
+      parser = new Parser();
+    } catch (Exception e) {
+      System.out.println("ERROR!");
+    }
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-        try {
-            parser = new Parser();
-        } catch (Exception e){
-            System.out.println("ERROR!");
-        }
+  public void runSimulation(View v) {
+    Properties p = System.getProperties();
+    Enumeration keys = p.keys();
+    while (keys.hasMoreElements()) {
+      String key = (String) keys.nextElement();
+      String value = (String) p.get(key);
+      System.out.println(key + " >>>> " + value);
     }
 
-    public void runSimulation(View v) {
-      Properties p = System.getProperties();
-      Enumeration keys = p.keys();
-      while (keys.hasMoreElements()) {
-        String key = (String) keys.nextElement();
-        String value = (String) p.get(key);
-        System.out.println(key + " >>>> " + value);
-      }
-
-      System.out.println("kkk "+System.getProperty("java.vm.name"));
-      String className;
-      if (System.getProperty("java.vm.name").equals("Dalvik")) {
-        className = "main.AndroidConfigurator";
-      } else {
-        className = "main.PcConfigurator";
-      }
-      System.out.println(className);
-        TextView tv = (TextView) findViewById(R.id.outputText);
-        tv.setText("Starting simulation " + count);
-        System.out.println("hello " + count + " times ");
-        count++;
+    String className;
+    if (System.getProperty("java.vm.name").equals("Dalvik")) {
+      className = "main.AndroidConfigurator";
+    } else {
+      className = "main.PcConfigurator";
+    }
+    TextView tv = (TextView) findViewById(R.id.outputText);
+    tv.setText("Starting simulation " + count);
+    count++;
 
 
-        if (((RadioButton) findViewById(R.id.radioButtonBasic)).isChecked()) {
-            parser.setCalculationMode("basic");
-        }
-        if (((RadioButton) findViewById(R.id.radioButtonAg)).isChecked()) {
-            parser.setCalculationMode("Ag");
-        }
-        if (((RadioButton) findViewById(R.id.radioButtonGraphene)).isChecked()) {
-            parser.setCalculationMode("graphene");
-        }
-        //EditText editText = (EditText) findViewById(R.id.editTextTemperature);
-        //int kk = Integer.parseInt(editText.getText());
-        parser.setTemperature(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextTemperature)).getText())));
-        parser.setCartSizeX(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextSize)).getText())));
-        parser.setCartSizeY(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextSize)).getText())));
-        parser.setNumberOfSimulations(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextRepetitions)).getText())));
-        try {
-            parser.print();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        switch (parser.getCalculationMode()) {
-            case "Ag":
-                simulation = new AgSimulation(parser);
-                break;
-            case "AgUc":
-                simulation = new AgUcSimulation(parser);
-                break;
-            case "graphene":
-                simulation = new GrapheneSimulation(parser);
-                break;
-            case "Si":
-                simulation = new SiSimulation(parser);
-                break;
-            case "basic":
-                simulation = new BasicGrowthSimulation(parser);
-                break;
-            default:
-                System.err.println("Error: Default case calculation mode. This simulation mode is not implemented!");
-                throw new IllegalArgumentException("This simulation mode is not implemented");
-        }
-
-      // Dirty way to read a resource file
-      InputStream inputStream = getResources().openRawResource(R.raw.test);
-      InputStreamReader inputreader = new InputStreamReader(inputStream);
-      BufferedReader buffreader = new BufferedReader(inputreader);
-      String line;
-      StringBuilder text = new StringBuilder();
-
-      try {
-        while (( line = buffreader.readLine()) != null) {
-          System.out.println(line);
-          text.append(line);
-          text.append('\n');
-        }
-      } catch (IOException e) {
-        System.out.println("Error");
-      }
-
-      System.out.println("is this right?"+getApplicationInfo().sourceDir);
-
-        System.out.println("Init");
-        simulation.initialiseKmc();
-        System.out.println("Frame");
-        simulation.createFrame();
-        System.out.println("Simulation");
-        simulation.doSimulation();
-        System.out.println("Finish");
-        simulation.finishSimulation();
-        TextView tv0 = (TextView) findViewById(R.id.textViewResults);
-        if (tv0 != null) {
-            tv0.setText(simulation.printFooter());
-        }
-        //SurfaceView sv = (SurfaceView) findViewById(R.id.surfaceView);
-        //sv.set.
-        ImageView iv = (ImageView) findViewById(R.id.imageView);
-        Canvas canvas = new Canvas();
-        Paint paint = new Paint();
-        Bitmap bm = paint((AbstractGrowthLattice) simulation.getKmc().getLattice());
-        iv.setImageBitmap(bm);
+    if (((RadioButton) findViewById(R.id.radioButtonBasic)).isChecked()) {
+      parser.setCalculationMode("basic");
+    }
+    if (((RadioButton) findViewById(R.id.radioButtonAg)).isChecked()) {
+      parser.setCalculationMode("Ag");
+    }
+    if (((RadioButton) findViewById(R.id.radioButtonGraphene)).isChecked()) {
+      parser.setCalculationMode("graphene");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    parser.setTemperature(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextTemperature)).getText())));
+    parser.setCartSizeX(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextSize)).getText())));
+    parser.setCartSizeY(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextSize)).getText())));
+    parser.setNumberOfSimulations(Integer.parseInt(String.valueOf(((EditText) findViewById(R.id.editTextRepetitions)).getText())));
+    try {
+      parser.print();
+    } catch (JSONException e) {
+      e.printStackTrace();
+    }
+    switch (parser.getCalculationMode()) {
+      case "Ag":
+        simulation = new AgSimulation(parser);
+        break;
+      case "AgUc":
+        simulation = new AgUcSimulation(parser);
+        break;
+      case "graphene":
+        simulation = new GrapheneSimulation(parser);
+        break;
+      case "Si":
+        simulation = new SiSimulation(parser);
+        break;
+      case "basic":
+        simulation = new BasicGrowthSimulation(parser);
+        break;
+      default:
+        System.err.println("Error: Default case calculation mode. This simulation mode is not implemented!");
+        throw new IllegalArgumentException("This simulation mode is not implemented");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    simulation.initialiseKmc();
+    simulation.createFrame();
+    simulation.doSimulation();
+    simulation.finishSimulation();
+    TextView tv0 = (TextView) findViewById(R.id.textViewResults);
+    if (tv0 != null) {
+      tv0.setText(simulation.printFooter());
+    }
+    ImageView iv = (ImageView) findViewById(R.id.imageView);
+    Bitmap bm = paint((AbstractGrowthLattice) simulation.getKmc().getLattice());
+    iv.setImageBitmap(bm);
+  }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  public Bitmap paint(AbstractGrowthLattice lattice) { //real drawing method
+
+    Paint p;
+
+    int scale = 2;
+    int baseX = 0;
+    int baseY = 0;
+    Bitmap g = Bitmap.createBitmap((int) (lattice.getCartSizeX() * scale), (int) (lattice.getCartSizeY() * scale), Bitmap.Config.RGB_565);
+
+    for (int i = 0; i < lattice.size(); i++) {
+      AbstractGrowthUc uc = lattice.getUc(i);
+      for (int j = 0; j < uc.size(); j++) {
+        AbstractGrowthAtom atom = uc.getAtom(j);
+        int Y = (int) Math.round((atom.getPos().getY() + uc.getPos().getY()) * scale) + baseY;
+        int X = (int) Math.round((atom.getPos().getX() + uc.getPos().getX()) * scale) + baseX;
+        String atomColor = "white";
+        switch (atom.getType()) { // the cases are for graphene
+          case AbstractAtom.TERRACE:
+            atomColor = "gray";
+            break;
+          case AbstractAtom.CORNER:
+            atomColor = "red";
+            break;
+          case AbstractAtom.EDGE:
+            atomColor = "magenta";
+            break;
+          case AbstractAtom.ARMCHAIR_EDGE: // == Ag KINK
+            atomColor = "white";
+            break;
+          case AbstractAtom.ZIGZAG_WITH_EXTRA: // == Ag ISLAND
+            atomColor = "cyan";
+            break;
+          case AbstractAtom.SICK:
+            atomColor = "blue";
+            break;
+          case AbstractAtom.KINK:
+            atomColor = "yellow";
+            break;
+          case AbstractAtom.BULK:
+            atomColor = "green";
+            break;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    public Bitmap paint(AbstractGrowthLattice lattice) { //real drawing method
-
-        Paint p;
-        //Canvas g;
-
-        //super.paint(g);
-        int scale = 2;
-        int baseX = 0;
-        int baseY = 0;
-        //g.setColor(GRAY);
-        Bitmap g = Bitmap.createBitmap((int) (lattice.getCartSizeX() * scale), (int) (lattice.getCartSizeY() * scale), Bitmap.Config.RGB_565);
-        //g.fillRect(baseX, baseY, (int) (lattice.getCartSizeX() * scale), (int) (lattice.getCartSizeY() * scale));
-
-        System.out.println("Created a bitmap of "+(int) (lattice.getCartSizeX() * scale) +"x"+ (int) (lattice.getCartSizeY() * scale));
-        for (int i = 0; i < lattice.size(); i++) {
-            AbstractGrowthUc uc = lattice.getUc(i);
-            for (int j = 0; j < uc.size(); j++) {
-                AbstractGrowthAtom atom = uc.getAtom(j);
-                int Y = (int) Math.round((atom.getPos().getY() + uc.getPos().getY()) * scale) + baseY;
-                int X = (int) Math.round((atom.getPos().getX() + uc.getPos().getX()) * scale) + baseX;
-                String atomColor = "white";
-                switch (atom.getType()) { // the cases are for graphene
-                    case AbstractAtom.TERRACE:
-                        //g.setColor(WHITE_GRAY);
-                        atomColor = "gray";
-                        break;
-                    case AbstractAtom.CORNER:
-                        atomColor = "red";
-                        //g.setColor(RED);
-                        break;
-                    case AbstractAtom.EDGE:
-                        atomColor = "magenta";
-                        //g.setColor(LILAC);
-                        break;
-                    case AbstractAtom.ARMCHAIR_EDGE: // == Ag KINK
-                        atomColor = "white";
-                        //g.setColor(Color.WHITE);
-                        break;
-                    case AbstractAtom.ZIGZAG_WITH_EXTRA: // == Ag ISLAND
-                        atomColor = "cyan";
-                        //g.setColor(Color.CYAN);
-                        break;
-                    case AbstractAtom.SICK:
-                        atomColor = "blue";
-                        //g.setColor(Color.BLUE);
-                        break;
-                    case AbstractAtom.KINK:
-                        atomColor = "yellow";
-                        //g.setColor(BANANA);
-                        break;
-                    case AbstractAtom.BULK:
-                        atomColor = "green";
-                        //g.setColor(GREEN);
-                        break;
-                }
-
-                System.out.println("Trying to print pixel "+X+" "+Y+" color "+atomColor+" type "+atom.getType());
-                int x = X;
-                int y = Y;
-                for (int iterX = 0; iterX < scale; iterX++) {
-                    x += iterX;
-                    for (int iterY = 0; iterY < scale; iterY++) {
-                        y += iterY;
-                        System.out.println("printing pixel "+x+" "+y+" color "+atomColor+" type "+atom.getType());
-                        if (x < g.getWidth() && y < g.getHeight()) {
-                            g.setPixel(x, y, Color.parseColor(atomColor));
-
-                        }
-                    }
-                }
-                /*if (X < g.getWidth() && Y < g.getHeight()) {
-                    p = new Paint();
-                    p.setColor(Color.parseColor(atomColor));
-                    //g.setPixel(X, Y, Color.parseColor(atomColor));
-                    g.drawCircle(X, Y, scale, p);
-
-                }*/
-
-                /*if (scale < 3) {
-                    if (atom.isOccupied()) {
-                        g.fillRect(X, Y, scale, scale);
-                    } else if (!atom.isOutside()) {
-                        g.drawRect(X, Y, scale, scale);
-                    }
-
-                } else if (atom.isOccupied()) {
-                    g.fillOval(X, Y, scale, scale);
-                    if (scale > 8) {
-                        g.drawString(Integer.toString(atom.getId()), X, Y);
-                    }
-                } else if (!atom.isOutside()) {
-                    g.drawOval(X, Y, scale, scale);
-                }*/
+        int x = X;
+        int y = Y;
+        for (int iterX = 0; iterX < scale; iterX++) {
+          x += iterX;
+          for (int iterY = 0; iterY < scale; iterY++) {
+            y += iterY;
+            if (x < g.getWidth() && y < g.getHeight()) {
+              g.setPixel(x, y, Color.parseColor(atomColor));
             }
+          }
         }
-        //g.dispose();
-        //g.
-        return g;
+      }
     }
+    return g;
+  }
 }
