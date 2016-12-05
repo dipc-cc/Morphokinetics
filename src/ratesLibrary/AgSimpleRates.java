9b8a59bef5c6a71e4daa427a0b1e4c5331532ce5
Joseba <joseba.alberdi@ehu.eus>
2016-12-05 11:35:40 +0100

    Removed unused variables.

--- src/ratesLibrary/AgSimpleRates.java
+++ src/ratesLibrary/AgSimpleRates.java
@@ -14,7 +14,6 @@ import static ratesLibrary.IRates.kB;
 public class AgSimpleRates implements IRates {
   
   private final double[][] energies;
-  private final double[][] fraction;
   private double diffusionMl;
   
   private final double prefactor;
@@ -27,9 +26,6 @@ public class AgSimpleRates implements IRates {
     double e2 = 0.33;
     double e3 = 0.42;
     double eInf = 9999999;
-    double e3p = eInf;
-    double e4 = eInf;
-    double e5 = eInf;
     
     prefactor = 1e13;
     
@@ -42,7 +38,7 @@ public class AgSimpleRates implements IRates {
     energies[0][5] = eInf;
     energies[0][6] = eInf;
     
-    energies[1][0] = e3p;
+    energies[1][0] = eInf;
     energies[1][1] = e1;
     energies[1][2] = e1;
     energies[1][3] = e1;
@@ -51,7 +47,7 @@ public class AgSimpleRates implements IRates {
     energies[1][6] = eInf;
     
     energies[2][0] = eInf;
-    energies[2][1] = e2; //e3
+    energies[2][1] = e2;
     energies[2][2] = e2;
     energies[2][3] = e2;
     energies[2][4] = e2;
@@ -60,7 +56,7 @@ public class AgSimpleRates implements IRates {
     
     energies[3][0] = eInf;
     energies[3][1] = eInf;
-    energies[3][2] = eInf; //e3p;
+    energies[3][2] = eInf;
     energies[3][3] = e3;
     energies[3][4] = e3;
     energies[3][5] = e3;
@@ -70,7 +66,7 @@ public class AgSimpleRates implements IRates {
     energies[4][1] = eInf;
     energies[4][2] = eInf;
     energies[4][3] = eInf;
-    energies[4][4] = e4;
+    energies[4][4] = eInf;
     energies[4][5] = eInf;
     energies[4][6] = eInf;
     
@@ -79,7 +75,7 @@ public class AgSimpleRates implements IRates {
     energies[5][2] = eInf;
     energies[5][3] = eInf;
     energies[5][4] = eInf;
-    energies[5][5] = e5;
+    energies[5][5] = eInf;
     energies[5][6] = eInf;
     
     energies[6][0] = eInf;
@@ -90,71 +86,10 @@ public class AgSimpleRates implements IRates {
     energies[6][5] = eInf;
     energies[6][6] = eInf;
     
-    fraction = new double[7][7];
-    fraction[0][0] = 1;
-    fraction[0][1] = 2.5;
-    fraction[0][2] = 5;
-    fraction[0][3] = 7.5;
-    fraction[0][4] = 0;
-    fraction[0][5] = 0;
-    fraction[0][6] = 0;
-    
-    fraction[1][0] = 0.001;
-    fraction[1][1] = 1;
-    fraction[1][2] = 2.5;
-    fraction[1][3] = 5;
-    fraction[1][4] = 7.5;
-    fraction[1][5] = 0;
-    fraction[1][6] = 0;
-    
-    fraction[2][0] = 0.001;
-    fraction[2][1] = 1;
-    fraction[2][2] = 1;
-    fraction[2][3] = 2.5;
-    fraction[2][4] = 5;
-    fraction[2][5] = 7.5;
-    fraction[2][6] = 0;
-    
-    fraction[3][0] = 0;
-    fraction[3][1] = 0;
-    fraction[3][2] = 0.001;
-    fraction[3][3] = 1;
-    fraction[3][4] = 2.5;
-    fraction[3][5] = 5;
-    fraction[3][6] = 0;
-    
-    fraction[4][0] = 0;
-    fraction[4][1] = 0;
-    fraction[4][2] = 0;
-    fraction[4][3] = 0;
-    fraction[4][4] = 1;
-    fraction[4][5] = 0;
-    fraction[4][6] = 0;
-    
-    fraction[5][0] = 0;
-    fraction[5][1] = 0;
-    fraction[5][2] = 0;
-    fraction[5][3] = 0;
-    fraction[5][4] = 0;
-    fraction[5][5] = 1;
-    fraction[5][6] = 0;
-    
-    fraction[6][0] = 0;
-    fraction[6][1] = 0;
-    fraction[6][2] = 0;
-    fraction[6][3] = 0;
-    fraction[6][4] = 0;
-    fraction[6][5] = 0;
-    fraction[6][6] = 0;
-    for (int i = 0; i < 7; i++) {
-      for (int j = 0; j < 7; j++) {
-        fraction[i][j] = 1;
-      }
-    }//*/
   }
   
   private double getRate(int sourceType, int destinationType, double temperature) {
-    return fraction[sourceType][destinationType]*prefactor * Math.exp(-energies[sourceType][destinationType] / (kB * temperature));
+    return prefactor * Math.exp(-energies[sourceType][destinationType] / (kB * temperature));
   }
 
   /**
