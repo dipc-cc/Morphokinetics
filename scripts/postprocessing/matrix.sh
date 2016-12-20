aa1c015851e9874a3aa08b73a2f6a915c39c2fea
Joseba <joseba.alberdi@ehu.eus>
2016-12-20 11:30:28 +0100

    Ensure that the format is the expected one.

--- scripts/postprocessing/matrix.sh
+++ scripts/postprocessing/matrix.sh
@@ -20,6 +20,8 @@ do
 	    grep -v histo dataEvery1percentAndNucleation.txt > matrix.txt;
 	    sed -i -e "s/\[/\#\[/" matrix.txt; # comment second line
 	    sed -i '3d' matrix.txt # remove 3rd line
+	    sed -i -e "s/\ /\t/g" matrix.txt # replace spaces with tabs
+	    sed -i -e "s/\t$/\n/" matrix.txt # remove last tab
 	    cd ../..;
 	fi
     done;
