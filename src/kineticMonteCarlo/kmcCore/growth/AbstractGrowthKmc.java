9fd07e00d1b69b467a0063711a1daba90adf86fa
Joseba <joseba.alberdi@ehu.es>
2016-12-28 14:10:13 +0100

    Some few times it is not possible to diffuse without forming a dimer. So returning originAtom itself to make possible to another originAtom to be choosen.

--- src/kineticMonteCarlo/kmcCore/growth/AbstractGrowthKmc.java
+++ src/kineticMonteCarlo/kmcCore/growth/AbstractGrowthKmc.java
@@ -458,7 +458,11 @@ public abstract class AbstractGrowthKmc extends AbstractKmc {
 
     } else {
       do {
-        destinationAtom = chooseRandomHop(originAtom);
+        destinationAtom = chooseRandomHop(originAtom, 0);
+        if (destinationAtom.equals(originAtom)) {
+          destinationAtom.equals(originAtom);
+          break;
+        }
       } while (!diffuseAtom(originAtom, destinationAtom));
     }
 
@@ -546,9 +550,10 @@ public abstract class AbstractGrowthKmc extends AbstractKmc {
    * chosen. With Devita accelerator many steps far away atom can be chosen.
    *
    * @param originAtom atom that has to be moved.
+   * @param times how many times it has been called recursively
    * @return destinationAtom.
    */
-  private AbstractGrowthAtom chooseRandomHop(AbstractGrowthAtom originAtom) {
+  private AbstractGrowthAtom chooseRandomHop(AbstractGrowthAtom originAtom, int times) {
     AbstractGrowthAtom destinationAtom;
     if (accelerator != null) {
       destinationAtom = accelerator.chooseRandomHop(originAtom);
@@ -556,8 +561,12 @@ public abstract class AbstractGrowthKmc extends AbstractKmc {
       destinationAtom =  originAtom.chooseRandomHop();
     }
 
-    if (destinationAtom.areTwoTerracesTogether()) {
-      return chooseRandomHop(originAtom);
+    if (justCentralFlake && destinationAtom.areTwoTerracesTogether()) {
+      if (times > originAtom.getNumberOfNeighbours()*100) { // it is not possible to diffuse without forming a dimer. So returning originAtom itself to make possible to another originAtom to be choosen.
+        return originAtom;
+      }
+        
+      return chooseRandomHop(originAtom, times+1);
     }
     if (destinationAtom.isOutside()) {
       do {
