6dd49b9126a17fe0e1935fdecfc841272878509e
karmele <karmele@10.110.225.162>
2017-02-10 12:36:35 +0100

    Catalysis Atom is an abstract growth atom

--- src/kineticMonteCarlo/atom/CatalysisAtom.java
+++ src/kineticMonteCarlo/atom/CatalysisAtom.java
@@ -10,6 +10,64 @@ package kineticMonteCarlo.atom;
  *
  * @author karmele
  */
-public class CatalysisAtom {
-    
+public class CatalysisAtom extends AbstractGrowthAtom {
+
+    public CatalysisAtom(int id, int numberOfNeighbours) {
+        super(id, numberOfNeighbours);
+    }
+
+    @Override
+    public byte getTypeWithoutNeighbour(int posNeighbour) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean areTwoTerracesTogether() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractGrowthAtom chooseRandomHop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getOrientation() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void obtainRateFromNeighbours() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double probJumpToNeighbour(int originType, int position) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setNeighbour(AbstractGrowthAtom a, int pos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public AbstractGrowthAtom getNeighbour(int pos) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double updateOneBound(int bond) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isPartOfImmobilSubstrate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEligible() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
