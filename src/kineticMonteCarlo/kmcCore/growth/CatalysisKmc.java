078280660151878f05d13a795235a59c4c24aa5a
KARMELE\karmele <kamele_94@hotmail.com>
2017-02-16 10:48:56 +0100

    Catalysis kmc copied from basic growth kmc

--- src/kineticMonteCarlo/kmcCore/growth/CatalysisKmc.java
+++ src/kineticMonteCarlo/kmcCore/growth/CatalysisKmc.java
@@ -5,6 +5,12 @@
 package kineticMonteCarlo.kmcCore.growth;
 
 import basic.Parser;
+import static kineticMonteCarlo.atom.CatalysisAtom.EDGE;
+import static kineticMonteCarlo.atom.CatalysisAtom.TERRACE;
+import kineticMonteCarlo.kmcCore.growth.devitaAccelerator.DevitaAccelerator;
+import kineticMonteCarlo.kmcCore.growth.devitaAccelerator.DevitaHopsConfig;
+import kineticMonteCarlo.kmcCore.growth.devitaAccelerator.HopsPerStep;
+import kineticMonteCarlo.lattice.CatalysisLattice;
 
 /**
  *
@@ -14,5 +20,41 @@ public abstract class CatalysisKmc extends AbstractGrowthKmc {
   
   public CatalysisKmc(Parser parser) {
     super(parser);
-  }   
+    
+    HopsPerStep distancePerStep = new HopsPerStep();
+    CatalysisLattice basicLattice = new CatalysisLattice(parser.getHexaSizeI(), parser.getHexaSizeJ(), getModifiedBuffer());
+    basicLattice.init();
+    setLattice(basicLattice); 
+    if (parser.justCentralFlake()) {
+      setPerimeter(new RoundPerimeter("Ag"));
+    }
+    if (parser.useDevita()) {
+      configureDevitaAccelerator(distancePerStep);
+    }
+  }
+
+  @Override
+  public float[][] getHexagonalPeriodicSurface(int binX, int binY) {
+    return getSampledSurface(binX, binY);
+  }
+  
+  private void configureDevitaAccelerator(HopsPerStep distancePerStep) {
+    setAccelerator(new DevitaAccelerator(getLattice(), distancePerStep));
+
+    if (getAccelerator() != null) {
+      getAccelerator().tryToSpeedUp(TERRACE,
+              new DevitaHopsConfig()
+              .setMinAccumulatedSteps(100)
+              .setMaxAccumulatedSteps(200)
+              .setMinDistanceHops(1)
+              .setMaxDistanceHops(8));
+
+      getAccelerator().tryToSpeedUp(EDGE,
+              new DevitaHopsConfig()
+              .setMinAccumulatedSteps(30)
+              .setMaxAccumulatedSteps(100)
+              .setMinDistanceHops(1)
+              .setMaxDistanceHops(5));
+    }
+  }
 }
