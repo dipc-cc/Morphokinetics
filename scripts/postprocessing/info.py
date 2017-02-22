21ed2e090badd3433db261281314de87d757bb9d
Joseba <joseba.alberdi@ehu.eus>
2017-02-22 11:45:16 +0100

    Added also last coverage

--- scripts/postprocessing/info.py
+++ scripts/postprocessing/info.py
@@ -16,12 +16,12 @@ def getTemperatures():
 
     
 def getInputParameters():
-    r_tt, temp, flux, calcType, sizI, sizJ = getInformationFromFile()
+    r_tt, temp, flux, calcType, sizI, sizJ, maxC = getInformationFromFile()
     maxN = 3
     if re.match("Ag", calcType): # Adjust J in hexagonal lattices
         sizJ = round(sizJ / math.sin(math.radians(60)))
         maxN = 6
-    return r_tt, temp, flux, sizI, sizJ, maxN
+    return r_tt, temp, flux, sizI, sizJ, maxN, maxC
 
 
 def getInformationFromFile():
@@ -39,9 +39,11 @@ def getInformationFromFile():
             temp = float(list(filter(None,re.split(" |,",line)))[1])
         if re.search("depositionFlux", line):
             flux = float(list(filter(None,re.split(" |,",line)))[1])
+        if re.search("coverage", line):
+            maxC = int(float(list(filter(None,re.split(" |,",line)))[1]))
         if hit:
             r_tt = float(re.split(' ', line)[0])
-            return r_tt, temp, flux, calc, sizX, sizY
+            return r_tt, temp, flux, calc, sizX, sizY, maxC
         if re.match("These", line):
             hit = True
 
