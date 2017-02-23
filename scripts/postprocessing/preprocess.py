ab2df8ec24e468c56ceeb235b7e5f9ac5f086cda
jalberdi004 <jalberdi004@pc-345>
2017-02-23 12:39:38 +0100

    Added a file to preprocess all the output of morphokinetics

---
+++ scripts/postprocessing/preprocess.py
@@ -0,0 +1,24 @@
+import os
+import info as i
+
+
+workingPath = os.getcwd()
+fluxes = i.getFluxes()
+for f in fluxes:
+    firstCollisionTime = []
+    temperaturesPlot = []
+    print(f)
+    os.chdir(f)
+    fPath = os.getcwd()
+    for t in i.getTemperatures():
+        try:
+            os.chdir(str(t)+"/results")
+            print("\t",t)
+            i.splitDataFiles()
+            i.splitAeFiles()
+        except FileNotFoundError:
+            pass
+        os.chdir(fPath)
+    kb = 8.6173324e-5
+    os.chdir(workingPath)
+
