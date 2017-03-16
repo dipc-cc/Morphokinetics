98c5dbd8bbd4b17a2b8f8bad38369da1a9fe4908
jalberdi004 <jalberdi004@pc-345>
2017-03-16 10:47:12 +0100

    Reading averages from info file

--- scripts/postprocessing/diffusivityIntegrated.py
+++ scripts/postprocessing/diffusivityIntegrated.py
@@ -13,14 +13,10 @@ import itertools
 
 def diffusivityDistance(index):
     p = inf.getInputParameters()
-    allData = []
-
-    filesN = glob.glob("data[0-9]*.txt")
-    for i in range(0,len(filesN)):
-        fileName = "data"+str(i)+".txt"
-        allData.append(np.loadtxt(fname=fileName, delimiter="\t"))
-
+    d = inf.readAverages()
+    
     # read all histograms
+    filesN = glob.glob("histogram[0-9]*.txt")
     islandDistribution = []
     for i in range(0,len(filesN)):
         fileName = "histogram"+str(i)+".txt"
@@ -28,81 +24,55 @@ def diffusivityDistance(index):
 
     islB3 = np.mean([[np.count_nonzero(h[1:]>2) for h in run] for run in islandDistribution],axis=0)
 
-    cove = np.mean([i[:,0]  for i in allData], axis=0)
-    time = np.mean([i[:,1]  for i in allData], axis=0)
-    isld = np.mean([i[:,3]  for i in allData], axis=0)
-    Ra   = np.mean([i[:,4]  for i in allData], axis=0)
-    Rh   = np.mean([i[:,5]  for i in allData], axis=0)
-    even = np.mean([i[:,7]  for i in allData], axis=0)
-    hops = np.mean([i[:,15] for i in allData], axis=0)
-    diff = np.mean([i[:,12] for i in allData], axis=0)
-    neg0 = np.mean([i[:,16] for i in allData], axis=0)
-    neg1 = np.mean([i[:,17] for i in allData], axis=0)
-    neg2 = np.mean([i[:,18] for i in allData], axis=0)
-    neg3 = np.mean([i[:,19] for i in allData], axis=0)
-    if p.maxN == 6:
-        neg4 = np.mean([i[:,20] for i in allData], axis=0)
-        neg5 = np.mean([i[:,21] for i in allData], axis=0)
-        neg6 = np.mean([i[:,22] for i in allData], axis=0)
+    cove = d.getRecomputedCoverage()/p.sizI/p.sizJ
 
-    neg = []
-    neg.append(neg0)
-    neg.append(neg1)
-    neg.append(neg2)
-    neg.append(neg3)
-    neg.append(neg4)
-    neg.append(neg5)
-    neg.append(neg6)
-    cove = np.sum(neg[:],axis=0)/p.sizI/p.sizJ
+    ratios = inf.getRatios(p)
 
-    ratios = inf.getRatio(p.temp, inf.getHexagonalEnergies())
-    Na = cove * p.sizI * p.sizJ
-
-    isld = fun.getOnlyAscending(isld)
+    isld = fun.getOnlyAscending(d.isld)
 
     #Correct the simulated time:
-    tp=np.zeros(len(time))
-    tp[0]=even[0]/(Rh[0]+Ra[0])
-    for j in range(1,len(Rh)):
-        tp[j] = tp[j-1]+(even[j]-even[j-1])/(Rh[j]+Ra[j]);
+    tp=np.zeros(len(d.time))
+    tp[0]=d.even[0]/(d.prob[0]+d.depo[0])
+    for j in range(1,len(d.prob)):
+        tp[j] = tp[j-1]+(d.even[j]-d.even[j-1])/(d.prob[j]+d.depo[j]);
 
     # Plot 2
     plt.clf()
-    x = list(range(0,len(time)))
+    x = list(range(0,len(d.time)))
     x = cove
-    plt.loglog(x, time, "s", label="time")
+    plt.loglog(x, d.time, "s", label="time")
     plt.loglog(x, tp, "x", label="corrected time")
     #time = tp
     cm = plt.get_cmap("Accent")
-    plt.loglog(x, diff, label=r"$R^2 (\times \frac{1}{N_1 N_2})$",
+    plt.loglog(x, d.diff, label=r"$R^2 (\times \frac{1}{N_1 N_2})$",
                marker="s", ls="", mew=0, markerfacecolor=cm(0/8), alpha=0.8)
-    plt.loglog(x, hops, label=r"$N_h l^2 (\times \frac{1}{N_1 N_2})$",
+    plt.loglog(x, d.hops, label=r"$N_h l^2 (\times \frac{1}{N_1 N_2})$",
                marker="p", ls="", mew=0, markerfacecolor=cm(7/8), alpha=0.8)
-    plt.loglog(x, fun.timeAverage(Rh,time)*time, label=r"$\overline{R_{h}} \;\; l^2 t$")
+    plt.loglog(x, fun.timeAverage(d.hops,d.time)*d.time, label=r"$\overline{R_{h}} \;\; l^2 t$")
                
     #coverages
     cm = plt.get_cmap("Set1")
   
     aneg = []
     for k in range(0,7):
-        aneg.append(fun.timeAverage(neg[k], time))
+        aneg.append(fun.timeAverage(d.negs[k], d.time))
         if k < 4:
             plt.loglog(x, aneg[k]/p.sizI/p.sizJ, label="aneg{} ".format(k))
     plt.loglog(x, (aneg[4]+aneg[5]+aneg[6])/p.sizI/p.sizJ, label="aneg{} ".format(k))
  
-    acov = fun.timeAverage(cove, time)
+    acov = fun.timeAverage(cove, d.time)
     plt.loglog(x, acov, "o", label="acov")
 
-    hopsCalc0 = (time * 6 * aneg[0] *ratios[0])
+    hopsCalc0 = (d.time * 6 * aneg[0] *ratios[0])
     plt.loglog(x, hopsCalc0, label="hops calc0")
-    hopsCalc = time * (6 * aneg[0]*ratios[0]+2*aneg[1]*ratios[8]+2*aneg[2]*ratios[15]+0.1*aneg[3]*ratios[24])
+    hopsCalc = d.time * (6 * aneg[0]*ratios[0]+2*aneg[1]*ratios[8]+2*aneg[2]*ratios[15]+0.1*aneg[3]*ratios[24])
     plt.loglog(x, hopsCalc, label="hops calc")
 
     ratios = inf.getRatio(p.temp, inf.getHexagonalEnergies())
 
     cm = plt.get_cmap("Set3")
-    plt.loglog(x, fun.thetaAverage(time, p.flux), label=r"$1-\frac{1-e^{-Ft}}{Ft}$", color=cm(0))
-    plt.loglog(x, fun.timeAverage(islB3, time)/p.sizI/p.sizJ, ".", color=cm(3/12), label=r"$N_{isl}$", markerfacecolor="None")
+    plt.loglog(x, fun.thetaAverage(d.time, p.flux), label=r"$1-\frac{1-e^{-Ft}}{Ft}$", color=cm(0))
+    plt.loglog(x, fun.timeAverage(islB3, d.time)/p.sizI/p.sizJ, ".", color=cm(3/12), label=r"$N_{isl}$", markerfacecolor="None")
 
     plt.subplots_adjust(left=0.12, bottom=0.1, right=0.7, top=0.9, wspace=0.2, hspace=0.2)
 
@@ -111,7 +81,6 @@ def diffusivityDistance(index):
     plt.title("flux: {:.1e} temperature: {:d}".format(p.flux, int(p.temp)))
     plt.savefig("../../../plot"+str(p.flux)+str(p.temp)+".png")
 
-    return time, neg1
 
 ##########################################################
 ##########           Main function   #####################
@@ -130,7 +99,7 @@ for f in fluxes:
             os.chdir(str(t)+"/results")
             print("\t",t)
             inf.splitDataFiles()
-            time, neg1 = diffusivityDistance(i)
+            diffusivityDistance(i)
         except FileNotFoundError:
             pass
         os.chdir(fPath)
