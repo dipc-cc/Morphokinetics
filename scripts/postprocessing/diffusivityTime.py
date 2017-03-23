6989c09f61744345ae28531e1887432279f9d31b
jalberdi004 <jalberdi004@pc-345>
2017-03-23 10:03:27 +0100

    Properly showing legend

--- scripts/postprocessing/diffusivityTime.py
+++ scripts/postprocessing/diffusivityTime.py
@@ -12,12 +12,12 @@ def read():
 def diffusivityTime(p, d, coverage):
     try:
         divisor = d.time[100-coverage]*p.flux**0.7
-        r = p.flux, d.cove[100-coverage]/divisor, d.diff[100-coverage]/divisor, d.hops[100-coverage]/divisor, d.even[100-coverage]/divisor
+        r = p.flux, d.cove[100-coverage]/divisor, d.diff[100-coverage]/divisor, d.hops[100-coverage]/divisor, np.max(d.isld)
     except IndexError:
         r = p.flux, None, None, None, None
     return r
 
-def plot(ax, data, i, showLegend):
+def plot(ax, data, i):
     cm = plt.get_cmap("Accent")
     alpha = 0.5
     mew = 0
@@ -28,8 +28,7 @@ def plot(ax, data, i, showLegend):
             marker="o", ls="", mew=mew, ms=8, alpha=alpha, markerfacecolor=cm(i/8))
     lg2, = ax.plot(x, data[:,4], label=r"$N_h$ {:1.0E}".format(flux),
             marker="+", ls="", mew=1, markeredgecolor=cm(i/8), ms=7, alpha=1)
-    if showLegend:
-        ax.legend(handles=[lg1, lg2], loc=1, numpoints=1, prop={'size':8}, markerscale=1.5)
+    return lg1, lg2
 
 workingPath = os.getcwd()
 fluxes = inf.getFluxes()
@@ -44,6 +43,7 @@ ax.set_yscale("log")
 ax.set_title("{} % coverage".format(coverage1))
 ax.set_xlabel(r"$1/k_BT + ln(F^{1.5})$")
 ax.set_ylabel(r"$R^2/(t F^{0.7})$,  $N_h/(t F^{0.7})$")
+legends = []
 for i,f in enumerate(fluxes):
     temperaturesPlot = []
     print(f)
@@ -57,18 +57,21 @@ for i,f in enumerate(fluxes):
             os.chdir(str(t)+"/results")
             print("\t",t)
             p, d = read()
-            flux, cove, diff, hops, even = diffusivityTime(p, d, coverage1)
-            data1.append([flux, t, cove, diff, hops, even])
-            flux, cove, diff, hops, even = diffusivityTime(p, d, coverage2)
-            data2.append([flux, t, cove, diff, hops, even])
-            flux, cove, diff, hops, even = diffusivityTime(p, d, coverage3)
-            data3.append([flux, t, cove, diff, hops, even])
+            flux, cove, diff, hops, isld = diffusivityTime(p, d, coverage1)
+            data1.append([flux, t, cove, diff, hops, isld])
+            flux, cove, diff, hops, isld = diffusivityTime(p, d, coverage2)
+            data2.append([flux, t, cove, diff, hops, isld])
+            flux, cove, diff, hops, isld = diffusivityTime(p, d, coverage3)
+            data3.append([flux, t, cove, diff, hops, isld])
         except FileNotFoundError:
             pass
         os.chdir(fPath)
     ### Plot
-    plot(ax, data1, i, True)
-    plot(ax, data2, i, False)
-    plot(ax, data3, i, False)
+    lg1, lg2 = plot(ax, data1, i)
+    legends.append(lg1)
+    legends.append(lg2)
+    plot(ax, data2, i)
+    plot(ax, data3, i)
+    ax.legend(handles=legends, loc=1, numpoints=1, prop={'size':8}, markerscale=1.5)
     fig.savefig("../diffusivityTime.pdf", bbox_inches='tight')
     os.chdir(workingPath)
