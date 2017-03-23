9fb0a750a3b53e54a734f8e421d5bb7c60350288
jalberdi004 <jalberdi004@pc-345>
2017-03-23 10:14:54 +0100

    Added plot of number of islands at 50 % of coverage

--- scripts/postprocessing/diffusivityTime.py
+++ scripts/postprocessing/diffusivityTime.py
@@ -12,18 +12,20 @@ def read():
 def diffusivityTime(p, d, coverage):
     try:
         divisor = d.time[100-coverage]*p.flux**0.7
-        r = p.flux, d.cove[100-coverage]/divisor, d.diff[100-coverage]/divisor, d.hops[100-coverage]/divisor, np.max(d.isld)
+        r = p.flux, d.cove[100-coverage]/divisor, d.diff[100-coverage]/divisor, d.hops[100-coverage]/divisor, d.isld[100-coverage]/(p.flux**0.29)#np.max(d.isld)/(p.flux**0.29)
     except IndexError:
         r = p.flux, None, None, None, None
     return r
 
-def plot(ax, data, i):
+def plot(ax, ax2, data, i):
     cm = plt.get_cmap("Accent")
     alpha = 0.5
     mew = 0
     data = np.array(data)
     flux = data[0,0]
     x = 1/kb/data[:,1]+np.log(flux**1.5)
+    if ax2 != None:
+        ax2.plot(x, data[:,5])
     lg1, = ax.plot(x, data[:,3], label=r"$R^2$ {:1.0E}".format(flux),
             marker="o", ls="", mew=mew, ms=8, alpha=alpha, markerfacecolor=cm(i/8))
     lg2, = ax.plot(x, data[:,4], label=r"$N_h$ {:1.0E}".format(flux),
@@ -39,7 +41,9 @@ coverage3 = 70
 kb = 8.617332e-5
 fig = plt.figure(num=None, figsize=(6,5))
 ax = fig.gca()
+ax2 = ax.twinx()
 ax.set_yscale("log")
+ax2.set_yscale("log")
 ax.set_title("{} % coverage".format(coverage1))
 ax.set_xlabel(r"$1/k_BT + ln(F^{1.5})$")
 ax.set_ylabel(r"$R^2/(t F^{0.7})$,  $N_h/(t F^{0.7})$")
@@ -67,11 +71,11 @@ for i,f in enumerate(fluxes):
             pass
         os.chdir(fPath)
     ### Plot
-    lg1, lg2 = plot(ax, data1, i)
+    lg1, lg2 = plot(ax, None, data1, i)
     legends.append(lg1)
     legends.append(lg2)
-    plot(ax, data2, i)
-    plot(ax, data3, i)
+    plot(ax, ax2, data2, i)
+    plot(ax, None, data3, i)
     ax.legend(handles=legends, loc=1, numpoints=1, prop={'size':8}, markerscale=1.5)
     fig.savefig("../diffusivityTime.pdf", bbox_inches='tight')
     os.chdir(workingPath)
