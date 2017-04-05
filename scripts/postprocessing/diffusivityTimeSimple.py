dc6a97df294189e29fd9afcd61d76024ef7cb0d5
jalberdi004 <jalberdi004@pc-345>
2017-04-05 15:06:26 +0200

    Added a subplot for basic mode. Moved common labels etc. to a function.

--- scripts/postprocessing/diffusivityTimeSimple.py
+++ scripts/postprocessing/diffusivityTimeSimple.py
@@ -14,8 +14,63 @@ def read():
     p = inf.getInputParameters()
     d = inf.readAverages()
     return p, d
-    
 
+
+def annotate(ax):
+    ax.set_yscale("log")
+    ax.set_xlabel(r"$1/k_BT + ln(F^{1.5})$")
+    ax.set_xlim(0,250)
+    ax.set_ylim(1e-3,1e4)
+    bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=1)
+    label = r"$\theta=0.30$"
+    ax.annotate(label, xy=(0.075,0.4), xycoords="axes fraction",
+                bbox=bbox_props, zorder=+100)
+    ax.annotate("", xy=(45,1e-3), xycoords='data', xytext=(45, 1e4),
+                arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
+    ax.annotate("", xy=(94,1e-3), xycoords='data', xytext=(94, 1e4),
+                arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
+    y = 2e-3
+    ax.text(120, y, "$I$", color="gray")
+    ax.text(60, y, "$II$", color="gray")
+    ax.text(25, y, "$III$", color="gray")
+    legends = []
+    rLg = mlines.Line2D([], [], color='black', ls="-",
+                        markersize=10, label=r"$g\; \frac{\langle R^2\rangle}{t F^{0.7}}$")
+    nLg = mlines.Line2D([], [], color='black', ls="-.",
+                        markersize=8, label=r"$gl^2\; \frac{\langle N_h\rangle}{t F^{0.7}}$")
+    iLg = mlines.Line2D([], [], color='black', ls='--',
+                        markersize=8, label=r"$N_{isl}$")
+    legends.append(rLg)
+    legends.append(nLg)
+    return legends
+
+    
+def annotateBasic(ax):
+    #ax.set_xlim(33,102)
+    #ax.set_ylim(1e-1,1e3)
+    ax.set_yscale("log")
+    ax.set_xlabel(r"$1/k_BT + ln(F^{1.78})$", size=8)
+    inf.smallerFont(ax, 8)
+    ax.annotate("", xy=(45,1e-2), xycoords='data', xytext=(45, 1e3),
+                arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
+    ax.annotate("", xy=(59,1e-2), xycoords='data', xytext=(59, 1e3),
+                arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
+    y = 2e-2
+    ax.text(70, y, "$I$", color="gray", size=8)
+    ax.text(50, y, "$II$", color="gray", size=8)
+    ax.text(35, y, "$III$", color="gray", size=8)
+    legends = []
+    rLg = mlines.Line2D([], [], color='black', ls="-",
+                        markersize=10, label=r"$g\; \frac{\langle R^2\rangle}{t F^{0.8}}$")
+    nLg = mlines.Line2D([], [], color='black', ls="-.",
+                        markersize=8, label=r"$gl^2\; \frac{\langle N_h\rangle}{t F^{0.8}}$")
+    iLg = mlines.Line2D([], [], color='black', ls='--',
+                        markersize=8, label=r"$N_{isl}$")
+    legends.append(rLg)
+    legends.append(nLg)
+    return legends
+
+    
 def plot(ax, data, i):
     marker = ["o", "s","H","D","^","d","h","p","o"]
     cm = plt.get_cmap("Accent")
@@ -26,52 +81,56 @@ def plot(ax, data, i):
         flux = data[0,0]
     except IndexError:
         raise
+    factor = data[0,6]
 
-    x = 1/kb/data[:,1]+np.log(flux**1.5)
+    x = 1/kb/data[:,1]+np.log(flux**factor)
     lw = 3
     lg1, = ax.plot(x, data[:,3], label=r"$F=$"+fun.base10(flux),
                    lw=lw, marker="", ls="-", mew=mew, ms=12, alpha=alpha, color=cm(i/8))
     ax.plot(x, data[:,4], label=r"$N_h$"+fun.base10(flux),
             lw=lw+1, ls="-.", mew=1, color=cm(i/8), ms=7, alpha=1)
-    #ax.text(x[4], data[:,4][4], fun.base10(flux), color=cm(i/8), size=10)
     arrow = dict(arrowstyle="->", connectionstyle="arc3", ls="-", color=cm(i/8))
-    ax.annotate(fun.base10(flux), xy=(x[2], data[:,4][2]), color=cm(i/8),
-                xytext=(0.85,0.6-0.05*i), textcoords="axes fraction", arrowprops=arrow)
-    if i == 3:
-        bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=0.3)
-        label = r"$\theta=0.30$"
-        ax.annotate(label, xy=(0.65,0.9), xycoords="axes fraction",
-                    bbox=bbox_props)
-        ax.annotate("", xy=(44,1e-3), xycoords='data', xytext=(44, 1e4),
-                    arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
-        ax.annotate("", xy=(86,1e-3), xycoords='data', xytext=(86, 1e4),
-                    arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
+    if str(flux)[0] == "5":
+        ax.annotate(fun.base10(flux), xy=(x[2], data[:,4][2]), color=cm(i/8),
+                    xytext=(0.9,0.52-0.05*i), textcoords="axes fraction", arrowprops=arrow)
+    if flux == 5e4:
         #Fit
         rngt = inf.defineRanges("AgUc","simple", data[:,1])
-        for i in range(0,len(rngt)-1):
+        for i in range(len(rngt)-2,-1,-1):
             y = data[:,4]
             a, b = fun.linearFit(x, y, rngt[i], rngt[i+1])
             ax.semilogy(x[rngt[i]:rngt[i+1]+1], np.exp(fun.linear(x[rngt[i]:rngt[i+1]+1], a, b)), ls="-", color="black", zorder=+200)
             xHalf = (x[rngt[i]]+x[rngt[i+1]]+1)/2
             text = "{:03.3f}".format(-b)
-            yHalf = np.exp(fun.linear(xHalf, a, b))*5
+            yHalf = np.exp(fun.linear(xHalf, a, b))*10
             text = r"$E_a^{Arrh}="+text+r"$"
             bbox_props = dict(boxstyle="round", fc="w", ec="1", alpha=0.6)
-            ax.text(xHalf,yHalf, text, color="black", bbox=bbox_props, ha="center", va="center", size=10)
+            ax.text(xHalf,yHalf, text, color="black", bbox=bbox_props, ha="center", va="center", size=10, zorder=+400)
+        i = 0 # print "interpolation" to the rest of lower x values
+        ax.semilogy(x[0:rngt[i+1]+1], np.exp(fun.linear(x[0:rngt[i+1]+1], a, b)), ls=":", color="black", zorder=+300)
+    if flux == 3.5e0:
+        rngt = inf.defineRanges("basic","simple", data[:,1])
+        for i in range(0,len(rngt)-1):
+            y = data[:,4]
+            a, b = fun.linearFit(x, y, rngt[i], rngt[i+1])
+            ax.semilogy(x[rngt[i]:rngt[i+1]+1], np.exp(fun.linear(x[rngt[i]:rngt[i+1]+1], a, b)), ls="-", color="black", zorder=+200)
+        
     return lg1
 
 workingPath = os.getcwd()
 fluxes = inf.getFluxes()
 i = 0
-coverage1 = 30
+coverage = 30
 kb = 8.617332e-5
 fig = plt.figure(num=None, figsize=(6,4))
 ax = fig.gca()
-ax.set_yscale("log")
-ax.set_xlabel(r"$1/k_BT + ln(F^{1.5})$")
-ax.set_xlim(0,250)
-ax.set_ylim(1e-3,1e4)
-legends = []
+legends = annotate(ax)
+# ax2 = fig.add_axes(ax.get_position())
+# ax2.patch.set_visible(False)
+# ax2.yaxis.set_label_position('right')
+# ax2.yaxis.set_ticks_position('right')
+# ax2.xaxis.set_label_position('top')
+# ax2.xaxis.set_ticks_position('top')
 for i,f in enumerate(fluxes):
     if i<-1:
         continue
@@ -79,37 +138,58 @@ for i,f in enumerate(fluxes):
     print(f)
     os.chdir(f)
     fPath = os.getcwd()
-    data1 = []
+    data = []
     for t in inf.getTemperatures():
         try:
             os.chdir(str(t)+"/results")
             print("\t",t)
             p, d = read()
-            flux, cove, diff, hops, isld = inf.getAvgDataCoverage(p, d, coverage1)
-            data1.append([flux, t, cove, diff, hops, isld])
+            flux, cove, diff, hops, isld, fact = inf.getAvgDataCoverage(p, d, coverage)
+            data.append([flux, t, cove, diff, hops, isld, fact])
         except (TypeError, FileNotFoundError):
             pass
         os.chdir(fPath)
     ### Plot
-    if i == 0:
-        rLg = mlines.Line2D([], [], color='black', ls="-",
-                            markersize=10, label=r"$g\; \frac{\langle R^2\rangle}{t F^{0.7}}$")
-        nLg = mlines.Line2D([], [], color='black', ls="-.",
-                            markersize=8, label=r"$gl^2\; \frac{\langle N_h\rangle}{t F^{0.7}}$")
-        iLg = mlines.Line2D([], [], color='black', ls='--',
-                            markersize=8, label=r"$N_{isl}$")
-        legends.append(rLg)
-        legends.append(nLg)
-        y = 1e-2
-        ax.text(120, y, "$I$", color="gray")
-        ax.text(60, y, "$II$", color="gray")
-        ax.text(25, y, "$III$", color="gray")
     try:
-        plot(ax, data1, i)
+        plot(ax, data, i)
     except IndexError:
         os.chdir(workingPath)
         continue
-    ax.legend(handles=legends, loc="best", #bbox_to_anchor=(0.75, 0.32),
+    ax.legend(handles=legends, loc=(0.05,0.15),#bbox_to_anchor=(0.75, 0.32),
               bbox_transform=plt.gcf().transFigure, numpoints=1, ncol=1, prop={'size':8}, markerscale=1)
     fig.savefig("../diffusivityTimeSimple.pdf", bbox_inches='tight')
     os.chdir(workingPath)
+
+workingPath = "/home/jalberdi004/mk_test/activationEnergy/basic/400/23.-newDiffusivity"
+os.chdir(workingPath)
+fluxes = inf.getFluxes()
+ax2 = fig.add_axes([0.6,0.6,0.35,0.35])
+legends = annotateBasic(ax2)
+for i,f in enumerate(fluxes):
+    os.chdir(workingPath)
+    print(os.system("pwd"))
+    if i<-1:
+        continue
+    temperaturesPlot = []
+    print(f)
+    os.chdir(f)
+    fPath = os.getcwd()
+    data = []
+    for t in inf.getTemperatures():
+        try:
+            os.chdir(str(t)+"/results")
+            print("\t",t)
+            p, d = read()
+            flux, cove, diff, hops, isld, fact = inf.getAvgDataCoverage(p, d, coverage)
+            data.append([flux, t, cove, diff, hops, isld, fact])
+        except (TypeError, FileNotFoundError):
+            pass
+        os.chdir(fPath)
+    ### Plot
+    try:
+        plot(ax2, data, i)
+    except IndexError:
+        continue
+    ax2.legend(handles=legends, loc="best", #bbox_to_anchor=(0.75, 0.32),
+              bbox_transform=plt.gcf().transFigure, numpoints=1, ncol=1, prop={'size':8}, markerscale=1)
+    fig.savefig("../diffusivityTimeSimple.pdf", bbox_inches='tight')
