50c9bdab982f994c4aec56eebbdd0a1f8a54a937
jalberdi004 <jalberdi004@pc-345>
2017-04-06 12:09:58 +0200

    Fit lines for basic, with light gray color. Improved text

--- scripts/postprocessing/diffusivityTimeSimple.py
+++ scripts/postprocessing/diffusivityTimeSimple.py
@@ -23,7 +23,7 @@ def annotate(ax):
     ax.set_ylim(1e-3,1e4)
     bbox_props = dict(boxstyle="round", fc="w", ec="0.5", alpha=1)
     label = r"$\theta=0.30$"
-    ax.annotate(label, xy=(0.075,0.4), xycoords="axes fraction",
+    ax.annotate(label, xy=(0.075,0.45), xycoords="axes fraction",
                 bbox=bbox_props, zorder=+100)
     ax.annotate("", xy=(45,1e-3), xycoords='data', xytext=(45, 1e4),
                 arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
@@ -34,40 +34,46 @@ def annotate(ax):
     ax.text(60, y, "$II$", color="gray")
     ax.text(25, y, "$III$", color="gray")
     legends = []
-    rLg = mlines.Line2D([], [], color='black', ls="-",
+    rLg = mlines.Line2D([], [], color='black', ls="-", lw=4,
                         markersize=10, label=r"$g\; \frac{\langle R^2\rangle}{t F^{0.7}}$")
-    nLg = mlines.Line2D([], [], color='black', ls="-.",
+    nLg = mlines.Line2D([], [], color='black', ls="-.", lw=3,
                         markersize=8, label=r"$gl^2\; \frac{\langle N_h\rangle}{t F^{0.7}}$")
     iLg = mlines.Line2D([], [], color='black', ls='--',
                         markersize=8, label=r"$N_{isl}$")
+    fLg = mlines.Line2D([], [], color="lightgray", ls='-',
+                        markersize=8, label=r"$Fit$")
     legends.append(rLg)
     legends.append(nLg)
+    legends.append(fLg)
     return legends
 
     
 def annotateBasic(ax):
-    #ax.set_xlim(33,102)
-    #ax.set_ylim(1e-1,1e3)
+    ax.set_xlim(30,120)
+    ax.set_ylim(1e-2,1e3)
     ax.set_yscale("log")
     ax.set_xlabel(r"$1/k_BT + ln(F^{1.78})$", size=8)
     inf.smallerFont(ax, 8)
-    ax.annotate("", xy=(45,1e-2), xycoords='data', xytext=(45, 1e3),
+    ax.annotate("", xy=(45,1e-2), xycoords='data', xytext=(45, 1e3), 
                 arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
-    ax.annotate("", xy=(59,1e-2), xycoords='data', xytext=(59, 1e3),
+    ax.annotate("", xy=(63,1e-2), xycoords='data', xytext=(63, 1e3),
                 arrowprops=dict(arrowstyle="-", connectionstyle="arc3", ls="--", color="gray"))
     y = 2e-2
     ax.text(70, y, "$I$", color="gray", size=8)
     ax.text(50, y, "$II$", color="gray", size=8)
     ax.text(35, y, "$III$", color="gray", size=8)
     legends = []
-    rLg = mlines.Line2D([], [], color='black', ls="-",
+    rLg = mlines.Line2D([], [], color='black', ls="-", lw=4,
                         markersize=10, label=r"$g\; \frac{\langle R^2\rangle}{t F^{0.8}}$")
-    nLg = mlines.Line2D([], [], color='black', ls="-.",
+    nLg = mlines.Line2D([], [], color='black', ls="-.", lw=3,
                         markersize=8, label=r"$gl^2\; \frac{\langle N_h\rangle}{t F^{0.8}}$")
     iLg = mlines.Line2D([], [], color='black', ls='--',
                         markersize=8, label=r"$N_{isl}$")
+    fLg = mlines.Line2D([], [], color='lightgray', ls='-',
+                        markersize=8, label=r"$Fit$")
     legends.append(rLg)
     legends.append(nLg)
+    legends.append(fLg)
     return legends
 
     
@@ -99,21 +105,27 @@ def plot(ax, data, i):
         for i in range(len(rngt)-2,-1,-1):
             y = data[:,4]
             a, b = fun.linearFit(x, y, rngt[i], rngt[i+1])
-            ax.semilogy(x[rngt[i]:rngt[i+1]+1], np.exp(fun.linear(x[rngt[i]:rngt[i+1]+1], a, b)), ls="-", color="black", zorder=+200)
+            ax.semilogy(x[rngt[i]:rngt[i+1]+1], np.exp(fun.linear(x[rngt[i]:rngt[i+1]+1], a, b)), ls="-", color="lightgray", zorder=+200)
             xHalf = (x[rngt[i]]+x[rngt[i+1]]+1)/2
             text = "{:03.3f}".format(-b)
             yHalf = np.exp(fun.linear(xHalf, a, b))*10
             text = r"$E_a^{Arrh}="+text+r"$"
-            bbox_props = dict(boxstyle="round", fc="w", ec="1", alpha=0.6)
-            ax.text(xHalf,yHalf, text, color="black", bbox=bbox_props, ha="center", va="center", size=10, zorder=+400)
+            ax.text(xHalf,yHalf, text, color="black", ha="center", va="center", size=10, zorder=+400)
         i = 0 # print "interpolation" to the rest of lower x values
-        ax.semilogy(x[0:rngt[i+1]+1], np.exp(fun.linear(x[0:rngt[i+1]+1], a, b)), ls=":", color="black", zorder=+300)
+        ax.semilogy(x[0:rngt[i+1]+1], np.exp(fun.linear(x[0:rngt[i+1]+1], a, b)), ls=":", color="lightgray", zorder=+300)
     if flux == 3.5e0:
         rngt = inf.defineRanges("basic","simple", data[:,1])
-        for i in range(0,len(rngt)-1):
+        for i in range(len(rngt)-2,-1,-1): #reverse iteration
             y = data[:,4]
             a, b = fun.linearFit(x, y, rngt[i], rngt[i+1])
-            ax.semilogy(x[rngt[i]:rngt[i+1]+1], np.exp(fun.linear(x[rngt[i]:rngt[i+1]+1], a, b)), ls="-", color="black", zorder=+200)
+            ax.semilogy(x[rngt[i]:rngt[i+1]+1], np.exp(fun.linear(x[rngt[i]:rngt[i+1]+1], a, b)), ls="-", color="lightgray", zorder=+200)
+            xHalf = (x[rngt[i]]+x[rngt[i+1]]+1)/2
+            text = "{:03.3f}".format(-b)
+            yHalf = np.exp(fun.linear(xHalf, a, b))*10
+            text = r"$"+text+r"$"
+            ax.text(xHalf,yHalf, text, color="black", ha="center", va="center", size=10, zorder=+400)
+        # print "interpolation" to the rest of lower x values
+        ax.semilogy(np.arange(120,70,-1), np.exp(fun.linear(np.arange(120,70,-1), a, b)), ls=":", color="lightgray", zorder=+300)
         
     return lg1
 
@@ -167,7 +179,6 @@ ax2 = fig.add_axes([0.6,0.6,0.35,0.35])
 legends = annotateBasic(ax2)
 for i,f in enumerate(fluxes):
     os.chdir(workingPath)
-    print(os.system("pwd"))
     if i<-1:
         continue
     temperaturesPlot = []
