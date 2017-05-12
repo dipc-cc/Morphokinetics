2d58cdcfc47f1ed8f9b55e7f18beb86a3010c858
jalberdi004 <jalberdi004@pc-345>
2017-05-12 11:43:38 +0200

    Added a reaction test. For the moment, simulated time is not checked because it is wrong.

--- test/basic/CatalysisSimulationTest.java
+++ test/basic/CatalysisSimulationTest.java
@@ -69,7 +69,7 @@ public class CatalysisSimulationTest {
   
   @Test
   public void testCatalysisDiffusion() {
-    AbstractSimulation.printHeader("Catalysis test");
+    AbstractSimulation.printHeader("Catalysis diffusion test");
     Parser parser = new Parser();
     parser.readFile(TestHelper.getBaseDir() + "/test/input/CatalysisDiffusionParameters");
     parser.print();
@@ -91,7 +91,7 @@ public class CatalysisSimulationTest {
   
   @Test
   public void testCatalysisDesorption() {
-    AbstractSimulation.printHeader("Catalysis test");
+    AbstractSimulation.printHeader("Catalysis desorption test");
     Parser parser = new Parser();
     parser.readFile(TestHelper.getBaseDir() + "/test/input/CatalysisDesorptionParameters");
     parser.print();
@@ -113,7 +113,7 @@ public class CatalysisSimulationTest {
   
   @Test
   public void testCatalysisAdsorptionDesorptionDiffusion() {
-    AbstractSimulation.printHeader("Catalysis test");
+    AbstractSimulation.printHeader("Catalysis adsorption desorption diffusion test");
     Parser parser = new Parser();
     parser.readFile(TestHelper.getBaseDir() + "/test/input/CatalysisAddParameters");
     parser.print();
@@ -132,6 +132,29 @@ public class CatalysisSimulationTest {
     }
     assertEquals(0.2175328892004462, simulatedTime, 0.0);
   }
+  
+  @Test
+  public void testCatalysisReaction() {
+    AbstractSimulation.printHeader("Catalysis test");
+    Parser parser = new Parser();
+    parser.readFile(TestHelper.getBaseDir() + "/test/input/CatalysisReactionParameters");
+    parser.print();
+
+    doCatalysisTest(parser);
+    
+    Restart restart = new Restart(TestHelper.getBaseDir() + "/test/references/");
+    float[][] ref0 = null;
+    try {
+      ref0 = restart.readSurfaceBinary2D("CatalysisReactionSurface000.mko");
+    } catch (FileNotFoundException ex) {
+      Logger.getLogger(AgSimulationTest.class.getName()).log(Level.SEVERE, null, ex);
+    }    
+    for (int i = 0; i < ref0.length; i++) {
+      assertArrayEquals(ref0[i], simulatedSurface[i], (float) 0.0001);
+    }
+    //assertEquals(infinity, simulatedTime, 0.0);
+  }
+  
   
   private void doCatalysisTest(Parser parser) {
     AbstractSimulation simulation = new CatalysisSimulation(parser);
