022d215139e503fd57164dc92a54400390f8b259
jalberdi004 <jalberdi004@pc-345>
2017-07-10 15:59:45 +0200

    Always check that the simulation is stationary.

--- src/kineticMonteCarlo/kmcCore/growth/CatalysisKmc.java
+++ src/kineticMonteCarlo/kmcCore/growth/CatalysisKmc.java
@@ -232,7 +232,7 @@ public class CatalysisKmc extends AbstractGrowthKmc {
     }
     simulatedSteps++;
     if (measureDiffusivity && (simulatedSteps + 1) % numStepsEachData == 0) {
-      if (!stationary && ((CatalysisLattice) getLattice()).isStationary(getTime())) {
+      if (((CatalysisLattice) getLattice()).isStationary(getTime()) && !stationary) {
         System.out.println("Stationary state achieved");
         stationary = true;
       }
