9cdece9f8281dffdb77f399217fb8bbc4527ebd8
jalberdi004 <jalberdi004@pc-345>
2017-08-08 10:35:32 +0200

    Added a script to get the TOF for catalysis simulations.

---
+++ scripts/postprocessing/getSimpleTof.py
@@ -0,0 +1,37 @@
+import glob
+import os
+import numpy as np
+import info as inf
+import math
+
+def getSimpleTof():
+    tofFiles = glob.glob("dataTof0*.txt")
+    tofs = 0
+    for t in tofFiles:
+        data = np.loadtxt(t)
+        if len(data) > 0:
+            try:
+                tofs += data[-1,0] # last simulated time, assuming 1000 atoms are created
+            except IndexError:
+                continue
+    tof = 1000/ (tofs / len(tofFiles))
+    return tof
+        
+
+    
+
+workingPath = os.getcwd()
+for f in inf.getTemperatures():
+    os.chdir(str(f)+"/results")
+    runFolder = glob.glob("*/");
+    os.chdir(runFolder[0])
+    os.getcwd()
+    tof = 0
+    try:
+        tof = getSimpleTof()
+    except ZeroDivisionError:
+        tof = 0
+    except IndexError:
+        pass
+    print(f,tof)
+    os.chdir(workingPath)
