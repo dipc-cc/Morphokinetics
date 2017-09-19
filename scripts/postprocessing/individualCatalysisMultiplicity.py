40cb43477c1c0fb55362fb8479fc80a42568652f
Joseba <joseba.alberdi@ehu.eus>
2017-09-19 11:10:06 +0200

    Using merged functions.

--- scripts/postprocessing/individualCatalysisMultiplicity.py
+++ scripts/postprocessing/individualCatalysisMultiplicity.py
@@ -15,86 +15,39 @@ import multiplicitiesInfo as mi
 ##########################################################
 
 temperatures = inf.getTemperatures("float")
+maxRanges = len(temperatures)
 kb = 8.6173324e-5
 p = inf.getInputParameters(glob.glob("*/output*")[0])
 maxCo2 = int(p.nCo2/10)
-maxAlfa = 4
-labelAlfa = ["$CO^B+O^B$","$CO^B+O^C$","$CO^C+O^B$","$CO^C+O^C$"]
-energies = e.catalysisEnergies(p)
-tempMavg = []
-tempOavg = []
-tempRavg = []
+total = False
+if total:
+    maxAlfa = 20
+    p.maxA = 20
+    energies = e.catalysisEnergiesTotal(p)
+else:
+    maxAlfa = 4
+    energies = e.catalysisEnergies(p)
+labelAlfa = [r"$CO^B+O^B\rightarrow CO_2$",r"$CO^B+O^C\rightarrow CO_2$",r"$CO^C+O^B\rightarrow CO_2$",r"$CO^C+O^C\rightarrow CO_2$", #Reaction
+             r"$V\rightarrow CO$",r"$V\rightarrow O$", # Adsorption
+             r"$CO^B\rightarrow V$",r"$CO^C\rightarrow V$", # Desorption CO
+             r"$O^B+O^B\rightarrow V^B+V^B$",r"$O^B+O^C\rightarrow V^B+V^C$",r"$O^C+O^B\rightarrow V^C+V^B$",r"$O^C+O^C\rightarrow V^C+V^C$", # Desorption O
+             r"$CO^B\rightarrow CO^B$",r"$CO^B\rightarrow CO^C$",r"$CO^C\rightarrow CO^B$",r"$CO^C\rightarrow CO^C$",  # Diffusion CO
+             r"$O^B\rightarrow O^B$",r"$O^B\rightarrow O^C$",r"$O^C\rightarrow O^B$",r"$O^C\rightarrow O^C$"] # Diffusion O
 
 workingPath = os.getcwd()
-for t in temperatures:
-    print(t)
-    os.chdir(workingPath)
-    try:
-        os.chdir(str(t)+"/results")
-        runFolder = glob.glob("*/");
-        runFolder.sort()
-        os.chdir(runFolder[-1])
-    except FileNotFoundError:
-        continue
-    tmp1, tmp2, tmp3 = mi.computeMavgAndOmegaOverRuns()
-    tempMavg.append(tmp1)
-    tempOavg.append(tmp2)
-    tempRavg.append(tmp3)
-   
-os.chdir(workingPath) 
-tempMavg = np.array(tempMavg)
-tempOavg = np.array(tempOavg)
-tempRavg = np.array(tempRavg)
+tempMavg, tempOavg, tempRavg = mi.getMavgAndOmega(temperatures,workingPath,total)
+os.chdir(workingPath)
 
 print(np.shape(tempMavg))
 sp=False
 if len(sys.argv) > 1:
     sp = sys.argv[1] == "p"
 
-tempOmegaCo2 = []
-tempEaMCo2 = []
-tempEaCo2 = []
-tempEafCo2 = []
-rngt = e.defineRangesCatalysis(p.calc, p.rLib, temperatures) #list([0, 3])
 
-maxRanges = len(temperatures)
-for co2 in range(0,maxCo2): # created co2: 10,20,30...1000
-    showPlot = sp and float(co2+(maxCo2/10)+1) % float(maxCo2/10) == 0
-    if float(co2+(maxCo2/10)+1) % float(maxCo2/10) == 0:
-        print(co2)
-    x = 1/kb/temperatures
-    y = tempRavg
-    if showPlot:
-        fig, axarr = plt.subplots(3, sharex=True, figsize=(5,6))
-        fig.subplots_adjust(right=0.7, hspace=0.1)
-    else:
-        axarr = np.zeros(3)
-    # N_h
-    tempEaCo2.append(mp.localAvgAndPlotLinear(x, y[:,co2], axarr[0], -1, showPlot, co2))
-    tempOmega = np.zeros((maxAlfa,maxRanges))
-    tempEaM = []
-    
-    for i in range(0,maxAlfa): # alfa
-        y = np.sum(tempMavg[:,co2,i:i+1], axis=1)
-        tempEaM.append(mp.localAvgAndPlotLinear(x, y, axarr[1], i, showPlot, co2))
-        if showPlot:
-            y = np.sum(tempOavg[:,co2,i:i+1], axis=1)
-            mp.plotOmegas(x, y, axarr[-1], i, tempOmega[i], rngt, labelAlfa)
-        tempOmega[i] = list(tempOavg[:, co2, i:i+1])
-    
-    tempOmegaCo2.append(tempOmega)
-    tempEaMCo2.append(tempEaM)
-    if showPlot:
-        plt.savefig("plot"+str(co2)+".svg", bbox_inches='tight')
-        plt.close()
-
-tempOmegaCo2 = np.array(tempOmegaCo2) # [co2, type (alfa), temperature range]
-tempEaCo2 = -np.array(tempEaCo2) # [co2, temperature range]
-tempEaMCo2 = np.array(tempEaMCo2) # [co2, type (alfa), temperature range]
-tempEaRCo2 = np.zeros(np.shape(tempEaMCo2))
+tempOmegaCo2, tempEaCo2, tempEaMCo2, tempEaRCo2 = mi.getEaMandEaR(p,temperatures,labelAlfa,sp,tempMavg,tempOavg,tempRavg)
 for alfa in range(0,maxAlfa):
     tempEaRCo2[:,alfa,:] = energies[alfa]
-tempEaRCo2 += e.getEaCorrections(temperatures)[0:4]
+tempEaRCo2 += e.getEaCorrections(temperatures)[0:maxAlfa]
 
 fig, axarr = plt.subplots(1, maxRanges, sharey=True, figsize=(maxRanges,4))
 fig.subplots_adjust(wspace=0.1)
@@ -160,7 +113,7 @@ if (omegas):
         myLabels.append(labelAlfa[i])
     myLabels.append("Rel. err.")
     plt.figlegend(myLegends, myLabels, loc=(0.68,0.15), prop={'size':11})
-    plt.savefig("multiplicitiesOmegasP.svg", bbox_inches='tight')
+    plt.savefig("multiplicitiesOmegasP.png", bbox_inches='tight')
 
 figR, ax = plt.subplots(1, figsize=(5,4))
 ax.plot(x, tgt, label="target", color="red")
@@ -173,6 +126,6 @@ for i in range(0,maxAlfa):
 # ax2.plot(x, err, label="Relative error")
 # ax2.set_ylim(0,1)
 ax.plot(x, abs(np.array(tgt)-np.array(rct)), label="Absolute error")
-ax.legend(loc="best", prop={'size':6})
+ax.legend(loc=(1.10,0.0), prop={'size':6})
 #ax.set_yscale("log")
 plt.savefig("multiplicitiesResume.svg", bbox_inches='tight')
--- scripts/postprocessing/totalCatalysisMultiplicity.py
+++ scripts/postprocessing/totalCatalysisMultiplicity.py
@@ -15,91 +15,39 @@ import multiplicitiesInfo as mi
 ##########################################################
 
 temperatures = inf.getTemperatures("float")
+maxRanges = len(temperatures)
 kb = 8.6173324e-5
 p = inf.getInputParameters(glob.glob("*/output*")[0])
 maxCo2 = int(p.nCo2/10)
-maxAlfa = 20 # I think is correct
+total = True
+if total:
+    maxAlfa = 20
+    p.maxA = 20
+    energies = e.catalysisEnergiesTotal(p)
+else:
+    maxAlfa = 4
+    energies = e.catalysisEnergies(p)
 labelAlfa = [r"$CO^B+O^B\rightarrow CO_2$",r"$CO^B+O^C\rightarrow CO_2$",r"$CO^C+O^B\rightarrow CO_2$",r"$CO^C+O^C\rightarrow CO_2$", #Reaction
              r"$V\rightarrow CO$",r"$V\rightarrow O$", # Adsorption
              r"$CO^B\rightarrow V$",r"$CO^C\rightarrow V$", # Desorption CO
              r"$O^B+O^B\rightarrow V^B+V^B$",r"$O^B+O^C\rightarrow V^B+V^C$",r"$O^C+O^B\rightarrow V^C+V^B$",r"$O^C+O^C\rightarrow V^C+V^C$", # Desorption O
              r"$CO^B\rightarrow CO^B$",r"$CO^B\rightarrow CO^C$",r"$CO^C\rightarrow CO^B$",r"$CO^C\rightarrow CO^C$",  # Diffusion CO
              r"$O^B\rightarrow O^B$",r"$O^B\rightarrow O^C$",r"$O^C\rightarrow O^B$",r"$O^C\rightarrow O^C$"] # Diffusion O
-energies = e.catalysisEnergiesTotal(p)
-tempMavg = []
-tempOavg = []
-tempRavg = []
 
 workingPath = os.getcwd()
-for t in temperatures:
-    print(t)
-    os.chdir(workingPath)
-    try:
-        os.chdir(str(t)+"/results")
-        runFolder = glob.glob("*/");
-        runFolder.sort()
-        os.chdir(runFolder[-1])
-    except FileNotFoundError:
-        continue
-    tmp1, tmp2, tmp3 = mi.computeMavgAndOmegaOverRuns(total=True)
-    tempMavg.append(tmp1)
-    tempOavg.append(tmp2)
-    tempRavg.append(tmp3)
-   
-os.chdir(workingPath) 
-tempMavg = np.array(tempMavg)
-tempOavg = np.array(tempOavg)
-tempRavg = np.array(tempRavg)
+tempMavg, tempOavg, tempRavg = mi.getMavgAndOmega(temperatures,workingPath,total)
+os.chdir(workingPath)
 
 print(np.shape(tempMavg))
 sp=False
 if len(sys.argv) > 1:
     sp = sys.argv[1] == "p"
 
-tempOmegaCo2 = []
-tempEaMCo2 = []
-tempEaCo2 = []
-tempEafCo2 = []
-rngt = e.defineRangesCatalysis(p.calc, p.rLib, temperatures) #list([0, 3])
 
-maxRanges = len(temperatures)
-for co2 in range(0,maxCo2): # created co2: 10,20,30...1000
-    showPlot = sp and float(co2+(maxCo2/10)+1) % float(maxCo2/10) == 0
-    if float(co2+(maxCo2/10)+1) % float(maxCo2/10) == 0:
-        print(co2)
-    x = 1/kb/temperatures
-    y = tempRavg
-    if showPlot:
-        fig, axarr = plt.subplots(3, sharex=True, figsize=(5,6))
-        fig.subplots_adjust(right=0.7, hspace=0.1)
-    else:
-        axarr = np.zeros(3)
-    # N_h
-    tempEaCo2.append(mp.localAvgAndPlotLinear(x, y[:,co2], axarr[0], -1, showPlot, co2))
-    tempOmega = np.zeros((maxAlfa,maxRanges))
-    tempEaM = []
-    
-    for i in range(0,maxAlfa): # alfa
-        y = np.sum(tempMavg[:,co2,i:i+1], axis=1)
-        tempEaM.append(mp.localAvgAndPlotLinear(x, y, axarr[1], i, showPlot, co2))
-        if showPlot:
-            y = np.sum(tempOavg[:,co2,i:i+1], axis=1)
-            mp.plotOmegas(x, y, axarr[-1], i, tempOmega[i], rngt, labelAlfa)
-        tempOmega[i] = list(tempOavg[:, co2, i:i+1])
-    
-    tempOmegaCo2.append(tempOmega)
-    tempEaMCo2.append(tempEaM)
-    if showPlot:
-        plt.savefig("plot"+str(co2)+".svg", bbox_inches='tight')
-        plt.close()
-
-tempOmegaCo2 = np.array(tempOmegaCo2) # [co2, type (alfa), temperature range]
-tempEaCo2 = -np.array(tempEaCo2) # [co2, temperature range]
-tempEaMCo2 = np.array(tempEaMCo2) # [co2, type (alfa), temperature range]
-tempEaRCo2 = np.zeros(np.shape(tempEaMCo2))
+tempOmegaCo2, tempEaCo2, tempEaMCo2, tempEaRCo2 = mi.getEaMandEaR(p,temperatures,labelAlfa,sp,tempMavg,tempOavg,tempRavg)
 for alfa in range(0,maxAlfa):
     tempEaRCo2[:,alfa,:] = energies[alfa]
-tempEaRCo2 += e.getEaCorrections(temperatures)
+tempEaRCo2 += e.getEaCorrections(temperatures)[0:maxAlfa]
 
 fig, axarr = plt.subplots(1, maxRanges, sharey=True, figsize=(maxRanges,4))
 fig.subplots_adjust(wspace=0.1)
