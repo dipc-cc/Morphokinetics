cf60378feef4069227c54d0416bf18f48eb15d93
Joseba <joseba.alberdi@ehu.eus>
2017-11-09 17:29:26 -0500

    Added concerted processes skeleton

---
+++ src/kineticMonteCarlo/process/ConcertedProcess.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kineticMonteCarlo.process;
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez
+ */
+public class ConcertedProcess {
+  
+  public static final byte ADSORB = 0;
+  public static final byte SINGLE = 1;
+  public static final byte CONCERTED = 2;
+  private double rate;
+}
