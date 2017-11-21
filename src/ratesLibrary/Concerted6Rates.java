cc4d020db39a4a89e9f16cb50bea10bb367ad1bb
Joseba <joseba.alberdi@ehu.eus>
2017-11-21 17:06:27 -0500

    Cu/Ni (111) real rates from Shree Ram

--- src/ratesLibrary/Concerted6Rates.java
+++ src/ratesLibrary/Concerted6Rates.java
@@ -24,63 +24,58 @@ public class Concerted6Rates implements IRates {
   public Concerted6Rates(float temperature) {
     this.temperature = temperature;
     diffusionMl = 10;
-    
-    double e0 = 0.10;
-    double e1 = 0.25;
-    double e2 = 0.33;
-    double e3 = 0.42;
+ 
     double eImpossible = 9999999;
-    double eHigh = 2.4;
-    
+     
     prefactor = 1e13;
     
     energies = new double[7][7];
-    energies[0][0] = e0;
-    energies[0][1] = e0;
-    energies[0][2] = e0;
-    energies[0][3] = e0;
+    energies[0][0] = 0.052;
+    energies[0][1] = 0.040;
+    energies[0][2] = 0.007;
+    energies[0][3] = 0.205;
     energies[0][4] = eImpossible;
     energies[0][5] = eImpossible;
     energies[0][6] = eImpossible;
     
-    energies[1][0] = 1.50;
-    energies[1][1] = e1;
-    energies[1][2] = e1;
-    energies[1][3] = e1;
-    energies[1][4] = e1;
+    energies[1][0] = 0.428;
+    energies[1][1] = 0.038;
+    energies[1][2] = 0.016;
+    energies[1][3] = 0.062;
+    energies[1][4] = 0.417;
     energies[1][5] = eImpossible;
     energies[1][6] = eImpossible;
     
-    energies[2][0] = 1.58;
-    energies[2][1] = e2;
-    energies[2][2] = e2;
-    energies[2][3] = e2;
-    energies[2][4] = e2;
-    energies[2][5] = e2;
+    energies[2][0] = 0.716;
+    energies[2][1] = 0.361;
+    energies[2][2] = 0.448;
+    energies[2][3] = 0.351;
+    energies[2][4] = 0.507;
+    energies[2][5] = 0.032;
     energies[2][6] = eImpossible;
     
-    energies[3][0] = 2.0;
-    energies[3][1] = 0.75;
-    energies[3][2] = 0.75;
-    energies[3][3] = e3;
-    energies[3][4] = e3;
-    energies[3][5] = e3;
+    energies[3][0] = 1.0;
+    energies[3][1] = 0.845;
+    energies[3][2] = 0.014;
+    energies[3][3] = 0.008;
+    energies[3][4] = 0.863;
+    energies[3][5] = 0.034;
     energies[3][6] = eImpossible;
     
     energies[4][0] = eImpossible;
-    energies[4][1] = eHigh;
-    energies[4][2] = eHigh;
-    energies[4][3] = eHigh;
-    energies[4][4] = eHigh;
-    energies[4][5] = eHigh;
+    energies[4][1] = 0.413;
+    energies[4][2] = 0.110;
+    energies[4][3] = 0.083;
+    energies[4][4] = 0.319;
+    energies[4][5] = 0.080;
     energies[4][6] = eImpossible;
     
     energies[5][0] = eImpossible;
     energies[5][1] = eImpossible;
-    energies[5][2] = eHigh;
-    energies[5][3] = eHigh;
-    energies[5][4] = eHigh;
-    energies[5][5] = eHigh;
+    energies[5][2] = 0.872;
+    energies[5][3] = 0.561;
+    energies[5][4] = 1.182;
+    energies[5][5] = 0.029;
     energies[5][6] = eImpossible;
     
     energies[6][0] = eImpossible;
