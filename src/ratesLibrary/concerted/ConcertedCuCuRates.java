3bc4947ee9fcdf4a10e163f6a1c567f6aa1e613d
Joseba <joseba.alberdi@ehu.eus>
2017-12-07 15:57:29 -0500

    Added two more rate collections

---
+++ src/ratesLibrary/concerted/ConcertedCuCuRates.java
@@ -0,0 +1,124 @@
+package ratesLibrary.concerted;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez, Shree Ram Acharya
+ */
+public class ConcertedCuCuRates extends AbstractConcertedRates {
+  
+  public ConcertedCuCuRates(float temperature) {
+    super(temperature);
+    double eImpossible = 9999999;
+    double[][] energies = new double[12][16];
+    for (int i = 0; i < 12; i++) {
+      for (int j = 0; j < 16; j++) {
+        energies[i][j] = eImpossible;
+      }
+    }
+    // From  type 0
+    energies[0][0] = 0.029; // to type 0
+    energies[0][1] = 0.409; // to type 1
+    energies[0][2] = 0.72; // to type 2, subtype 0
+    energies[0][5] = 1.0; // to type 3, subtype 0
+    // From  type 1
+    energies[1][0] = 0.451; // to type 0
+    energies[1][1] = 0.021; // to type 1
+    energies[1][2] = 0.348; // to type 2, subtype 0
+    energies[1][14] = 0.843; // to type 2, subtype 1, detach
+    // From  type 2, subtype 0
+    energies[2][0] = 0.77; // to type 0
+    energies[2][1] = 0.359; // to type 1
+    energies[2][12] = 0.687; // to type 1, detach
+    energies[2][2] = 0.273; // to type 2, subtype 0
+    energies[2][3] = 0.285; // to type 2, subtype 1
+    energies[2][4] = 0.189; // to type 2, subtype 2
+    energies[2][5] = 0.213; // to type 3, subtype 0
+    energies[2][15] = 0.488; // to type 3, subtype 0, detach
+    energies[2][6] = 0.201; // to type 3, subtype 1
+    energies[2][8] = 0.562; // to type 4, subtype 0
+    // From  type 2, subtype 1
+    energies[3][0] = 0.817; // to type 0
+    energies[3][1] = 0.405; // to type 1
+    energies[3][12] = 0.646; // to type 1, detach
+    energies[3][2] = 0.338; // to type 2, subtype 0
+    energies[3][13] = 0.299; // to type 2, subtype 0, detach
+    energies[3][4] = 0.232; // to type 2, subtype 2
+    energies[3][5] = 0.278; // to type 3, subtype 0
+    energies[3][8] = 0.217; // to type 4, subtype 0
+    energies[3][9] = 0.684; // to type 4, subtype 1
+    energies[3][10] = 0.245; // to type 4, subtype 2
+    energies[3][11] = 0.245; // to type 5
+    // From  type 2, subtype 2
+    energies[4][1] = 0.726; // to type 1
+    energies[4][2] = 0.816; // to type 2, subtype 0
+    energies[4][3] = 0.746; // to type 2, subtype 1
+    // From  type 3, subtype 0
+    energies[5][0] = 1.072; // to type 0
+    energies[5][1] = 0.684; // to type 1
+    energies[5][12] = 0.905; // to type 1, detach
+    energies[5][2] = 0.56; // to type 2, subtype 0
+    energies[5][13] = 0.564; // to type 2, subtype 0, detach
+    energies[5][3] = 0.58; // to type 2, subtype 1
+    energies[5][4] = 0.444; // to type 2, subtype 2
+    energies[5][5] = 0.482; // to type 3, subtype 0
+    energies[5][15] = 0.492; // to type 3, subtype 0, detach
+    energies[5][6] = 0.434; // to type 3, subtype 1
+    energies[5][8] = 0.398; // to type 4, subtype 0
+    // From  type 3, subtype 1
+    energies[6][1] = 0.731; // to type 1
+    energies[6][2] = 0.52; // to type 2, subtype 0
+    energies[6][4] = 0.327; // to type 2, subtype 2
+    energies[6][5] = 0.396; // to type 3, subtype 0
+    energies[6][6] = 0.244; // to type 3, subtype 1
+    energies[6][7] = 0.699; // to type 3, subtype 2
+    energies[6][8] = 0.586; // to type 4, subtype 0
+    energies[6][9] = 0.72; // to type 4, subtype 1
+    // From  type 3, subtype 2
+    energies[7][3] = 0.919; // to type 2, subtype 1
+    energies[7][6] = 0.777; // to type 3, subtype 1
+    energies[7][9] = 0.946; // to type 4, subtype 1
+    energies[7][10] = 0.668; // to type 4, subtype 2
+    energies[7][11] = 0.988; // to type 5
+    // From  type 4, subtype 0
+    energies[8][1] = 0.986; // to type 1
+    energies[8][2] = 0.777; // to type 2, subtype 0
+    energies[8][5] = 0.648; // to type 3, subtype 0
+    energies[8][6] = 0.559; // to type 3, subtype 1
+    energies[8][8] = 0.506; // to type 4, subtype 0
+    // From  type 4, subtype 1
+    energies[9][3] = 0.946; // to type 2, subtype 1
+    energies[9][6] = 1.0; // to type 3, subtype 1
+    energies[9][8] = 0.824; // to type 4, subtype 0
+    energies[9][9] = 1.02; // to type 4, subtype 1
+    energies[9][10] = 0.757; // to type 4, subtype 2
+    // From  type 4, subtype 2
+    energies[10][3] = 0.978; // to type 2, subtype 1
+    energies[10][6] = 0.774; // to type 3, subtype 1
+    energies[10][7] = 0.988; // to type 3, subtype 2
+    // From  type 5
+    energies[11][3] = 1.127; // to type 2, subtype 1
+    energies[11][15] = 0.983; // to type 3, subtype 0, detach
+    energies[11][6] = 1.09; // to type 3, subtype 1
+    energies[11][10] = 0.914; // to type 4, subtype 2
+    energies[11][11] = 0.814; // to type 5
+    setEnergies(energies);
+    
+    double[] concertedEnergies = new double[9]; // up to 8 atom islands
+    concertedEnergies[0] = eImpossible; 
+    concertedEnergies[1] = eImpossible; 
+    concertedEnergies[2] = 0.060; // dimer
+    concertedEnergies[3] = 0.160; 
+    concertedEnergies[4] = 0.180; 
+    concertedEnergies[5] = 0.223; 
+    concertedEnergies[6] = 0.300; 
+    concertedEnergies[7] = 0.400; 
+    concertedEnergies[8] = 0.370; 
+    setConcertedEnergies(concertedEnergies);
+  }
+}
---
+++ src/ratesLibrary/concerted/ConcertedNiNiRates.java
@@ -0,0 +1,144 @@
+package ratesLibrary.concerted;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez, Shree Ram Acharya
+ */
+public class ConcertedNiNiRates extends AbstractConcertedRates {
+  
+  public ConcertedNiNiRates(float temperature) {
+    super(temperature);
+    double eImpossible = 9999999;
+    double[][] energies = new double[12][16];
+    for (int i = 0; i < 12; i++) {
+      for (int j = 0; j < 16; j++) {
+        energies[i][j] = eImpossible;
+      }
+    }
+    // From  type 0
+    energies[0][0] = 0.059; // to type 0
+    energies[0][1] = 0.448; // to type 1
+    energies[0][2] = 0.803; // to type 2, subtype 0
+    energies[0][3] = 0.824; // to type 2, subtype 1
+    energies[0][5] = 1.125; // to type 3, subtype 0
+    // From  type 1
+    energies[1][0] = 0.54; // to type 0
+    energies[1][1] = 0.044; // to type 1
+    energies[1][12] = 0.355; // to type 1, detach
+    energies[1][2] = 0.401; // to type 2, subtype 0
+    energies[1][13] = 0.375; // to type 2, subtype 0, detach
+    energies[1][3] = 0.443; // to type 2, subtype 1
+    energies[1][14] = 1.11; // to type 2, subtype 1, detach
+    energies[1][4] = 0.434; // to type 2, subtype 2
+    energies[1][5] = 0.434; // to type 3, subtype 0
+    energies[1][15] = 0.607; // to type 3, subtype 0, detach
+    energies[1][6] = 0.723; // to type 3, subtype 1
+    energies[1][8] = 1.03; // to type 4, subtype 0
+    // From  type 2, subtype 0
+    energies[2][0] = 0.904; // to type 0
+    energies[2][1] = 0.441; // to type 1
+    energies[2][2] = 0.362; // to type 2, subtype 0
+    energies[2][3] = 0.37; // to type 2, subtype 1
+    energies[2][14] = 0.616; // to type 2, subtype 1, detach
+    energies[2][4] = 0.278; // to type 2, subtype 2
+    energies[2][5] = 0.476; // to type 3, subtype 0
+    energies[2][15] = 0.616; // to type 3, subtype 0, detach
+    energies[2][6] = 0.278; // to type 3, subtype 1
+    energies[2][8] = 0.642; // to type 4, subtype 0
+    // From  type 2, subtype 1
+    energies[3][0] = 0.952; // to type 0
+    energies[3][1] = 0.486; // to type 1
+    energies[3][12] = 0.794; // to type 1, detach
+    energies[3][2] = 0.418; // to type 2, subtype 0
+    energies[3][13] = 0.339; // to type 2, subtype 0, detach
+    energies[3][3] = 0.415; // to type 2, subtype 1
+    energies[3][14] = 0.314; // to type 2, subtype 1, detach
+    energies[3][4] = 0.327; // to type 2, subtype 2
+    energies[3][5] = 0.38; // to type 3, subtype 0
+    energies[3][6] = 0.323; // to type 3, subtype 1
+    energies[3][7] = 0.799; // to type 3, subtype 2
+    energies[3][8] = 0.312; // to type 4, subtype 0
+    energies[3][9] = 0.999; // to type 4, subtype 1
+    energies[3][10] = 0.63; // to type 4, subtype 2
+    energies[3][11] = 0.63; // to type 5
+    // From  type 2, subtype 2
+    energies[4][1] = 0.306; // to type 1
+    energies[4][2] = 0.306; // to type 2, subtype 0
+    energies[4][3] = 0.301; // to type 2, subtype 1
+    energies[4][4] = 1.095; // to type 2, subtype 2
+    energies[4][5] = 0.197; // to type 3, subtype 0
+    // From  type 3, subtype 0
+    energies[5][0] = 1.229; // to type 0
+    energies[5][1] = 0.791; // to type 1
+    energies[5][12] = 1.07; // to type 1, detach
+    energies[5][2] = 0.69; // to type 2, subtype 0
+    energies[5][13] = 0.659; // to type 2, subtype 0, detach
+    energies[5][3] = 0.702; // to type 2, subtype 1
+    energies[5][14] = 0.607; // to type 2, subtype 1, detach
+    energies[5][4] = 0.565; // to type 2, subtype 2
+    energies[5][5] = 0.633; // to type 3, subtype 0
+    energies[5][15] = 0.612; // to type 3, subtype 0, detach
+    energies[5][6] = 0.559; // to type 3, subtype 1
+    energies[5][8] = 0.535; // to type 4, subtype 0
+    // From  type 3, subtype 1
+    energies[6][1] = 0.829; // to type 1
+    energies[6][2] = 0.668; // to type 2, subtype 0
+    energies[6][3] = 0.654; // to type 2, subtype 1
+    energies[6][4] = 0.392; // to type 2, subtype 2
+    energies[6][5] = 0.549; // to type 3, subtype 0
+    energies[6][6] = 0.386; // to type 3, subtype 1
+    energies[6][7] = 1.122; // to type 3, subtype 2
+    // From  type 3, subtype 2
+    energies[7][3] = 1.218; // to type 2, subtype 1
+    energies[7][6] = 1.12; // to type 3, subtype 1
+    energies[7][7] = 1.251; // to type 3, subtype 2
+    energies[7][9] = 1.158; // to type 4, subtype 1
+    energies[7][10] = 1.044; // to type 4, subtype 2
+    energies[7][11] = 1.153; // to type 5
+    // From  type 4, subtype 0
+    energies[8][1] = 1.1; // to type 1
+    energies[8][2] = 0.942; // to type 2, subtype 0
+    energies[8][5] = 0.838; // to type 3, subtype 0
+    energies[8][6] = 0.688; // to type 3, subtype 1
+    energies[8][8] = 0.866; // to type 4, subtype 0
+    // From  type 4, subtype 1
+    energies[9][3] = 1.342; // to type 2, subtype 1
+    energies[9][6] = 1.352; // to type 3, subtype 1
+    energies[9][8] = 1.212; // to type 4, subtype 0
+    energies[9][9] = 1.345; // to type 4, subtype 1
+    energies[9][10] = 1.171; // to type 4, subtype 2
+    // From  type 4, subtype 2
+    energies[10][3] = 1.355; // to type 2, subtype 1
+    energies[10][6] = 1.193; // to type 3, subtype 1
+    energies[10][7] = 1.338; // to type 3, subtype 2
+    energies[10][9] = 1.173; // to type 4, subtype 1
+    energies[10][10] = 1.167; // to type 4, subtype 2
+    // From  type 5
+    energies[11][3] = 1.548; // to type 2, subtype 1
+    energies[11][15] = 1.415; // to type 3, subtype 0, detach
+    energies[11][6] = 1.522; // to type 3, subtype 1
+    energies[11][7] = 1.379; // to type 3, subtype 2
+    energies[11][9] = 1.397; // to type 4, subtype 1
+    energies[11][10] = 1.351; // to type 4, subtype 2
+    energies[11][11] = 1.171; // to type 5
+    setEnergies(energies);
+    
+    double[] concertedEnergies = new double[9]; // up to 8 atom islands
+    concertedEnergies[0] = eImpossible; 
+    concertedEnergies[1] = eImpossible; 
+    concertedEnergies[2] = 0.060; // dimer
+    concertedEnergies[3] = 0.160; 
+    concertedEnergies[4] = 0.180; 
+    concertedEnergies[5] = 0.223; 
+    concertedEnergies[6] = 0.300; 
+    concertedEnergies[7] = 0.400; 
+    concertedEnergies[8] = 0.370; 
+    setConcertedEnergies(concertedEnergies);
+  }
+}
