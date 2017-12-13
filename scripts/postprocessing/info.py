a973db6da4ea3a996cf1abd0ee462bd2933c1521
Joseba <joseba.alberdi@ehu.eus>
2017-12-13 10:48:46 -0500

    Adding concerted rates.

--- scripts/postprocessing/info.py
+++ scripts/postprocessing/info.py
@@ -37,32 +37,43 @@ class fileData:
             "basic": e.basic,
             "graphene": e.graphene,
             "catalysis": e.catalysisEnergies,
+            "concerted": e.concertedEnergies,
         }
         func = calcSwitcher.get(self.calc, lambda: "nothing")
         ratios = e.getRatio(self.calc, self.temp, func(self))
         return ratios
 
-    # For catalysis
     def getRatiosTotal(self):
-        energies = e.catalysisEnergiesTotal(self)
-        ratios = np.zeros(28)
-        ratios[4] = e.computeAdsorptionRate(self,self.prCO, 0)
-        ratios[5] = e.computeAdsorptionRate(self,self.prO2, 1)
-        ratios[6] = e.computeDesorptionRate(self,self.prCO, 0, ratios[4],  energies[6]) #CO^B
-        ratios[7] = e.computeDesorptionRate(self,self.prCO, 0, ratios[4],  energies[7]) #CO^C
-        ratios[8] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5],  energies[8]) #O^B + O^B
-        ratios[9] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5],  energies[9]) #O^B + O^C
-        ratios[10] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5], energies[10]) #O^C + O^B
-        ratios[11] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5], energies[11]) #O^C + O^C
-        ratios[0:4] = e.computeReactionRate(self,energies[0:4])
-        ratios[12:20] = e.computeDiffusionRate(self,energies[12:20])
-        if self.rLib == "farkas":
-            ratios[20:22] = e.computeDesorptionRate(self,self.prCO, 0, ratios[4],  energies[20:22])
-            ratios[22:25] = e.computeReactionRate(self, energies[22:25])
-            ratios[25:28] = e.computeDiffusionRate(self,energies[25:28])
-        return ratios
+        # For catalysis
+        if self.calc == "catalysis":
+            return getRatiosCatalysis(self)
+        else:
+            return getRatiosConcerted(self)
         
+def getRatiosCatalysis(self):
+    energies = e.catalysisEnergiesTotal(self)
+    ratios = np.zeros(28)
+    ratios[4] = e.computeAdsorptionRate(self,self.prCO, 0)
+    ratios[5] = e.computeAdsorptionRate(self,self.prO2, 1)
+    ratios[6] = e.computeDesorptionRate(self,self.prCO, 0, ratios[4],  energies[6]) #CO^B
+    ratios[7] = e.computeDesorptionRate(self,self.prCO, 0, ratios[4],  energies[7]) #CO^C
+    ratios[8] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5],  energies[8]) #O^B + O^B
+    ratios[9] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5],  energies[9]) #O^B + O^C
+    ratios[10] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5], energies[10]) #O^C + O^B
+    ratios[11] = e.computeDesorptionRate(self,self.prO2, 1, ratios[5], energies[11]) #O^C + O^C
+    ratios[0:4] = e.computeReactionRate(self,energies[0:4])
+    ratios[12:20] = e.computeDiffusionRate(self,energies[12:20])
+    if self.rLib == "farkas":
+        ratios[20:22] = e.computeDesorptionRate(self,self.prCO, 0, ratios[4],  energies[20:22])
+        ratios[22:25] = e.computeReactionRate(self, energies[22:25])
+        ratios[25:28] = e.computeDiffusionRate(self,energies[25:28])
+    return ratios
 
+def getRatiosConcerted(self):
+    energies = e.concertedEnergies(self)
+    ratios = np.zeros(192)
+    ratios = e.computeDiffusionRate(self,energies)
+    return ratios
 
 def getFluxes():
     fluxes = glob.glob("flux*")
