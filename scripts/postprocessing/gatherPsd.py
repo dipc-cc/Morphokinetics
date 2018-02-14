64c8cd0d034c2630fd83fbed109f1d492c2f1188
Joseba <joseba.alberdi@ehu.es>
2018-02-14 16:08:04 +0100

    Added an script to gather PSDs for a simulation, with different temperatures

---
+++ scripts/postprocessing/gatherPsd.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2018 J. Alberdi-Rodriguez
+#
+# This file is part of Morphokinetics.
+#
+# Morphokinetics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Morphokinetics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Morphokinetics.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import glob
+import info as inf
+import shutil as sh
+
+temperatures = inf.getTemperatures()
+cov = 10
+surfaces = ' '.join("psd"+str(cov)+str(t)+".png" for t in temperatures)
+workingPath = os.getcwd()
+for i,t in enumerate(temperatures):
+    print(t)
+    os.chdir(workingPath)
+    try:
+        os.chdir(str(t)+"/results")
+        runFolder = glob.glob("*/")
+        runFolder.sort()
+        os.chdir(runFolder[-1])
+        os.chdir("results")
+        sh.copy("psd"+str(cov)+".png","psd"+str(cov)+str(t)+".png")
+        print(os.getcwd())
+    except FileNotFoundError:
+        continue
+    
+    sh.copy("psd"+str(10)+str(t)+".png",workingPath)
+
+os.chdir(workingPath)
+#os.system("montage $(ls *svg | sort -n) allSurfaces.png")
+os.system("montage -tile x1 "+surfaces+" allPsds.png")
+#os.system("convert allSurfaces.png -pointsize 10 -gravity south -annotate 0 "+workingPath+" -resize %70 labelSurfaces.png")
