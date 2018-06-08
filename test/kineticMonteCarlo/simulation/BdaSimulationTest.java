38f18079dddba7ebd11c212acbea55758936624a
Joseba <joseba.alberdi@ehu.eus>
2018-06-08 15:51:01 +0200

    Added a test for BDA simulation

---
+++ test/kineticMonteCarlo/simulation/BdaSimulationTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2018 J. Alberdi-Rodriguez
+ *
+ * This file is part of Morphokinetics.
+ *
+ * Morphokinetics is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Morphokinetics is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Morphokinetics.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package kineticMonteCarlo.simulation;
+
+import basic.Parser;
+import basic.io.Restart;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author J. Alberdi-Rodriguez
+ */
+public class BdaSimulationTest {
+  
+  private float[][] simulatedSurface;
+  private double simulatedTime;
+  private String restartFolder;
+  
+  public BdaSimulationTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+  
+  @Test
+  public void testBdaAlpha() {
+    AbstractSimulation.printHeader("BDA alpha phase, without any rotation");
+    Parser parser = new Parser();
+    parser.readFile(TestHelper.getBaseDir() + "/test/input/Bda.json");
+    parser.print();
+
+    doBdaTest(parser);
+    assertEquals(3.237250605717838E-6, simulatedTime, 0.0);
+    
+    
+    Restart restart = new Restart(TestHelper.getBaseDir() + "/test/references/");
+    int[] sizes = {parser.getCartSizeX(), parser.getCartSizeY()};
+    float[][] ref0 = null;
+    try {
+      ref0 = restart.readSurfaceText2D(2, sizes, "BdaAlphaSurface.txt");
+    } catch (FileNotFoundException ex) {
+      Logger.getLogger(AgSimulationTest.class.getName()).log(Level.SEVERE, null, ex);
+    }    
+    for (int i = 0; i < ref0.length; i++) {
+      assertArrayEquals(ref0[i], simulatedSurface[i], (float) 0.0001);
+    }
+  }
+  
+  private void doBdaTest(Parser parser) {
+    AbstractSimulation simulation = new BdaSimulation(parser);
+
+    simulation.initialiseKmc();
+    simulation.createFrame();
+    simulation.doSimulation();
+    simulation.finishSimulation();
+    restartFolder = simulation.getRestartFolderName();
+
+    simulatedSurface = simulation.getKmc().getSampledSurface((int) (parser.getCartSizeX() * parser.getPsdScale()), (int) (parser.getCartSizeY() * parser.getPsdScale()));
+    simulatedTime = simulation.getSimulatedTime();
+  }
+}
