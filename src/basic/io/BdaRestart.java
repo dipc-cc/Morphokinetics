f78ca76bef7c9347a99b963dd33169db724f2211
Joseba <joseba.alberdi@ehu.eus>
2018-07-30 16:08:34 +0200

    Added a method to write extra output for BDA

--- src/basic/io/BdaRestart.java
+++ src/basic/io/BdaRestart.java
@@ -1,6 +1,12 @@
 package basic.io;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import static java.lang.String.format;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import kineticMonteCarlo.lattice.AbstractLattice;
 
 /*
@@ -28,16 +34,51 @@ import kineticMonteCarlo.lattice.AbstractLattice;
  */
 public class BdaRestart extends Restart {
   
-  private final String folder;
+  private PrintWriter outData;
   
   public BdaRestart(String restartFolder) {
     super(restartFolder);
-    folder = restartFolder;
   }
   
   @Override
   public void writeSvg(int simulationNumber, AbstractLattice lattice) {
-    String fileName = format("%s/surface%03d.svg", folder, simulationNumber);
+    String fileName = format("%s/surface%03d.svg", getFolder(), simulationNumber);
     RestartLow.writeSvgBda(fileName, lattice);
+  }
+  
+  public void writeExtraOutput(double temperature, double time, int[] coverages, long[] steps, long[] production, int[] sizes) {
+    String frmt = "%g";
+    outData.format(frmt, temperature);
+    frmt = "\t%g";
+    outData.format(frmt, time);
+    frmt = "\t%d";
+    for (int i = 0; i < coverages.length; i++) {
+      outData.format(frmt, coverages[i]);
+    }
+    frmt = "\n";
+    outData.format(frmt, "");
+  }
+  
+  public void initBdaRestart(int simulationNumber) {
+    //String folder = getFolder();try {
+    try {
+      String fileName = format("%sdataBda%03d.txt", getFolder(), simulationNumber);
+      outData = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+      outData.println("# File " + fileName);
+    } catch (IOException e) {
+      Logger.getLogger(Restart.class.getName()).log(Level.SEVERE, null, e);
+    }
+  }
+  
+  @Override
+  public void reset() {
+    super.reset();
+    initBdaRestart(getIteration());
+  }
+  
+  @Override
+  public void flushExtra() {
+    super.flushExtra();
+    outData.flush();
   }
 }
