9b2d037502bf804253473528e073f7194afed40b
Joseba <joseba.alberdi@ehu.eus>
2018-10-09 12:19:37 +0200

    Removed number of adsorptions from the total rate

--- scripts/postprocessing/concertedObject/plotManyTotalRates.py
+++ scripts/postprocessing/concertedObject/plotManyTotalRates.py
@@ -2,13 +2,30 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 import pdb
 
 def getIndexFromCov(coverages, cov):
     return coverages.tolist().index(cov)
 
 kb = 8.617332e-5
+latSize = 46000
+
+cm = plt.get_cmap('tab20')
+
+if os.getcwd()[-4:] == "CuNi":
+    color0 = cm(0/20)
+    color1 = cm(1/20)
+    label = "Cu/Ni"
+    marker0 = "o"
+    marker1 = "2"
+else:
+    color0 = cm(2/20)
+    color1 = cm(3/20)
+    label = "Ni/Cu"
+    marker0 = "s"
+    marker1 = "+"
+    
 
 # read data
 totalRateM = np.loadtxt("totalRate.txt")
@@ -16,17 +33,17 @@ totalRateE = np.loadtxt("totalRateEvents.txt")
 temperatures = np.loadtxt("temperatures.txt")
 coverages = np.loadtxt("coverages.txt")
 
+#Remove adsorption events
+for i in coverages:
+    index = getIndexFromCov(coverages, i)
+    totalRateE[index,:] -= i
 # plot it
-cm = plt.get_cmap('tab20')
-markers=["o", "s","D","^","d","h","p"]
 fig, axarr = plt.subplots(1, 1, sharey=True, figsize=(5,4))
 fig.subplots_adjust(top=0.95,left=0.15,right=0.95,bottom=0.15)
-for color,i in enumerate(coverages[-101::10][1:]):
+for i in coverages[-101::10][1:]:
     index = getIndexFromCov(coverages, i)
-    color = cm(0/20)
-    axarr.plot(1/kb/temperatures, totalRateM[index], marker="o", ms=5, ls="", label=r"Cu on Ni: $\sum_\alpha \epsilon^{R}_\alpha$  at "+str(coverages[index])+r"$\theta$", color=color)
-    color = cm(1/20)
-    axarr.plot(1/kb/temperatures, totalRateE[index], ":", marker="2", ms=7, label=r"Cu on NI: $N_e^{R}/L$ at "+str(coverages[index])+r"$\theta$", color=color)
+    axarr.plot(1/kb/temperatures, totalRateM[index], marker=marker0, ms=5, ls="", label=label+r": $\sum_\alpha \epsilon^{R}_\alpha$  at "+str(coverages[index])+r"$\theta$", color=color0)
+    axarr.plot(1/kb/temperatures, totalRateE[index], ":", marker=marker1, ms=7, label=label+r": $N_e^{R}/L$ at "+str(coverages[index])+r"$\theta$", color=color1)
 
     break # exits in the first loop
 
@@ -34,6 +51,6 @@ axarr.annotate(r"$\epsilon^{R}_\alpha=\omega^{R}_\alpha(E^k_\alpha+E^M_\alpha)$"
 axarr.set_yscale("log")
 axarr.set_ylabel("Total rate per site")
 axarr.set_xlabel(r"$1/k_BT$")
-axarr.set_ylim(0.1,1.5e4)
+axarr.set_ylim(1e-2,1.5e4)
 axarr.legend(loc="best", prop={'size':6})
 fig.savefig("TotalRates10.svg")
