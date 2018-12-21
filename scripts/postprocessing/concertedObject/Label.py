bee6bfcd932708ca0b566f4c1f90401ddd71445e
Joseba <joseba.alberdi@ehu.eus>
2018-12-21 13:05:45 +0100

    New codes for the labels

--- scripts/postprocessing/concertedObject/Label.py
+++ scripts/postprocessing/concertedObject/Label.py
@@ -1,21 +1,21 @@
 class Label:
 
     def __init__(self):
-        code = ["0",
-                 "1",
-                 "2,0",
-                 "2,1",
-                 "2,2",
-                 "3,0",
-                 "3,1",
-                 "3,2",
-                 "4,0",
-                 "4,1",
-                 "4,2",
-                 "5"]
+        code = [ "[0,0]",
+                 "[1,0]",
+                 "[2,0]",
+                 "[2,1]",
+                 "[2,2]",
+                 "[3,0]",
+                 "[3,1]",
+                 "[3,2]",
+                 "[4,0]",
+                 "[4,1]",
+                 "[4,2]",
+                 "[5,0]"]
         self.types = []
         for i in range(0,12):
-            self.types.append(r"D_{"+code[i]+r"}")
+            self.types.append(code[i]+r"")
         # detachs
         self.types.append(r"D_{1}'")
         self.types.append(r"D_{2,0}'")
@@ -28,12 +28,12 @@ class Label:
             for j in range(0,16):
                 labelAlfa.append(self.__getLabel(i,j))
         for i in range(0,9):
-            labelAlfa.append(r"$I_{"+str(i)+"}$")
+            labelAlfa.append(r"$\epsilon^R_{I"+str(i)+"}$")
         for i in range(0,4):
-            labelAlfa.append(r"$M_{"+str(i+1)+"}$")
+            labelAlfa.append(r"$\epsilon^R_{C"+str(i+1)+"}$")
         labelAlfa.append(r"Adsorption")
         return labelAlfa
 
     def __getLabel(self, i, j):
-        label = r"$" + self.types[i] + r"\rightarrow " + self.types[j] + r"$"
+        label = r"$\epsilon^R_{D" + self.types[i] + r"\rightarrow " + self.types[j] + r"}$"
         return label
